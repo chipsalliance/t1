4051bc0e1bc9582bd2e86269b06cdaa1335671f3
Qinjun Li <liqinjun1994@gmail.com>
2022-07-08 18:11:26 +0800

    Design uop for VFU, and some parameters
    1. Add notes for VFU, implement its interface.
    2. add VLMax, groupSize, groupSizeBits to VectorParameters.

--- v/src/Lane.scala
+++ v/src/Lane.scala
@@ -5,6 +5,14 @@ import chisel3.util._
 
 class LaneReq(param: VectorParameters) extends Bundle {
   val uop: UInt = UInt(4.W)
+  val w: Bool = Bool()
+  val n: Bool =  Bool()
+  val sew: UInt = UInt(2.W)
+  val src: Vec[UInt] = Vec(3, UInt(param.ELEN.W))
+  val groupIndex: UInt = UInt(param.groupSizeBits.W)
+  val sign: Bool = Bool()
+  val mask: Bool = Bool()
+  val maskDestination: Bool = Bool()
 }
 
 class LaneResp(param: VectorParameters) extends Bundle {
--- v/src/Parameters.scala
+++ v/src/Parameters.scala
@@ -10,6 +10,10 @@ case class VectorParameters(ELEN: Int = 32, VLEN: Int = 128, lane: Int = 4) {
   val addRespWidth: Int = ELEN + 1
   val mulRespWidth: Int = ELEN * 2
   val elenBits: Int = log2Ceil(ELEN)
+  // VLEN * lMulMax / sewMin
+  val VLMax: Int = VLEN
+  val groupSize: Int = VLMax / lane
+  val groupSizeBits: Int = log2Ceil(groupSize)
 
   def lanePopCountParameter: LanePopCountParameter = LanePopCountParameter(ELEN, elenBits)
   def laneFFOParameter: LaneFFOParameter = LaneFFOParameter(ELEN, ELEN)
