aa1b2edd0e3448ab3360ca505eb15d00e5d4b971
SharzyL <me@sharzy.in>
2022-07-21 20:22:50 +0800

    add scaladoc and complementary notes to VFU

--- v/src/LaneMul.scala
+++ v/src/LaneMul.scala
@@ -10,6 +10,7 @@ class LaneMul(param: LaneMulParam) extends Module {
   val src:  Vec[UInt] = IO(Input(Vec(3, UInt(param.inputWidth.W))))
   val resp: Vec[UInt] = IO(Output(Vec(2, UInt(param.respWidth.W))))
 
+  // TODO: mul
   // todo: resp c&s
   resp.head := src.head * src(1) + src.last
   resp.last := 0.U
