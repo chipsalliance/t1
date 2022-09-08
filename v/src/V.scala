f13e40256094836a18b33b7a9d492214075ba839
Qinjun Li <liqinjun1994@gmail.com>
2022-09-08 14:47:52 +0800

    [rtl] add an empty top for elaboration

---
+++ v/src/V.scala
@@ -0,0 +1,25 @@
+package v
+
+import chisel3._
+import chisel3.util._
+
+case class VParam(ELEN: Int = 32) {
+  def laneParam: LaneParameters = LaneParameters(ELEN)
+}
+
+class VReq(param: VParam) extends Bundle {
+  val inst: UInt = UInt(32.W)
+  val src1Data: UInt = UInt(param.ELEN.W)
+}
+
+class VResp(param: VParam) extends  Bundle {
+  val data: UInt = UInt(param.ELEN.W)
+}
+
+class V(param: VParam) extends Module {
+  val req: DecoupledIO[VReq] = IO(Flipped(Decoupled(new VReq(param))))
+  val resp: ValidIO[VResp] = IO(Valid(new VResp(param)))
+  val csrInterface: LaneCsrInterface = IO(Input(new LaneCsrInterface(param.laneParam)))
+  val storeBufferClear: Bool = IO(Input(Bool()))
+  // 后续需要l2的 tile link 接口
+}
