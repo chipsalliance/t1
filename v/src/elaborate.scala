c73f66766f8cd22649005f5df1f12287a1ddbd53
qinjun-li <liqinjun1994@gmial.com>
2022-09-20 11:24:26 +0800

    fix for mshr elaboration.

--- v/src/elaborate.scala
+++ v/src/elaborate.scala
@@ -10,7 +10,7 @@ object elaborate extends App {
     Seq(
       TargetDirAnnotation("./builds/"),
       //EmitAllModulesAnnotation(classOf[VerilogEmitter]),
-      ChiselGeneratorAnnotation(() => new Lane(LaneParameters())),
+      ChiselGeneratorAnnotation(() => new MSHR(LSUParam())),
       RunFirrtlTransformAnnotation(new VerilogEmitter)
     )
   )
