b49d296b2dfc069b1674fdf474546a4883071a70
qinjun-li <liqinjun1994@gmial.com>
2022-10-12 17:12:25 +0800

    fix[cosim]:
    1. Fix the problem of vector instruction PC not changing.
    2. Fix the problem of wrong judgment of instruction type.

--- v/tests/resources/util.h
+++ v/tests/resources/util.h
@@ -6,8 +6,13 @@ inline uint32_t clip(uint32_t binary, int a, int b) { return (binary >> a) & ((1
 
 inline bool is_vector_instr(uint64_t f) {
   uint32_t opcode = clip(f, 0, 6);
+  uint32_t width = clip(f, 12, 14);
 
-  if ((opcode & 0b1011111) == 0b0000111 || opcode == 0b1010111) {
+  auto load_type = opcode == 0b111;
+  auto store_type = opcode == 0b100111;
+  auto v_type = opcode == 0b1010111 && width != 0b111;
+
+  if (load_type || store_type || v_type) {
     return true;
   } else {
     return false;
