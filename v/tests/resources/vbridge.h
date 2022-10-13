566d48e908fa2c3d4e983c01d21eab4af2cafafb
Jiuyang Liu <liu@jiuyang.me>
2022-10-14 01:46:48 +0800

    [cosim] add notes for the cosim refactor
    1. discussed the rtl&spike coroutine design for cosim.
    2. discussed the VCS&verilator code sharing design.

--- v/tests/resources/vbridge.h
+++ v/tests/resources/vbridge.h
@@ -10,6 +10,38 @@
 class VerilatedContext;
 struct VBridgeImpl;
 
+// Vector Instruction Buffer:
+//   diff: record VRF Write, Mem write
+//   drive: instr, csr, Mem load
+//
+// Spike enqueue buffer when not empty.
+// for each cycle, verilator capture signals, and use Vector Instruction Buffer to difftest
+//   after commit, dequeue VIB
+//
+// Verilator with boring -> Verilator with VPI
+// get_signals(&some_struct) {
+//   // access with IO
+//   some_struct->xxx = top.debug.xxx
+//   // access with VPI
+//   vpi_get_value(vh1, &v)
+// }
+//
+// VCS
+// DPI set_signals() {
+//   VIB vib;
+//   // use vib to return signals
+// }
+// each `cycle` will call a callback to triger difftest
+// in difftest, get_signals be implemented with VPI
+// each `cycle`, VCS use DPI to get data
+//
+//
+// void difftest() {
+//   SomeStruct some_struct;
+//   get_singals(&some_struct);
+//   // do difftest
+// }
+
 struct VBridge {
   VBridgeImpl *impl;
 
