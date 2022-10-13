f648a2d2e1cc04cdec514945739489321a1b8067
Jiuyang Liu <liu@jiuyang.me>
2022-10-13 17:43:37 +0800

    [cosim] bore VRF write to Top

--- v/tests/src/Cosim.scala
+++ v/tests/src/Cosim.scala
@@ -1,11 +1,18 @@
 package v.tests
 
+import chisel3._
+import chisel3.aop.Select
+import chisel3.aop.injecting.InjectingAspect
+import chisel3.experimental.{ChiselAnnotation, annotate}
 import chisel3.stage.ChiselGeneratorAnnotation
-import firrtl.{AnnotationSeq, VerilogEmitter}
+import firrtl.annotations.Annotation
 import firrtl.options.TargetDirAnnotation
 import firrtl.stage.{OutputFileAnnotation, RunFirrtlTransformAnnotation}
+import firrtl.transforms.TopWiring.TopWiringTransform
+import firrtl.{AnnotationSeq, VerilogEmitter}
 import os.Path
 import utest._
+import v.{V, VRF}
 
 object Cosim extends TestSuite {
   val tests = Tests {
@@ -20,6 +27,23 @@ object Cosim extends TestSuite {
           Seq(
             TargetDirAnnotation(outputDirectory.toString()),
             ChiselGeneratorAnnotation(generator),
+            RunFirrtlTransformAnnotation(new TopWiringTransform),
+            InjectingAspect(
+              { dut: V =>
+                Select.collectDeep(dut) {
+                  case vrf: VRF => vrf
+                }
+              },
+              { vrf: VRF => {
+                val debug = Wire(chiselTypeOf(vrf.write)).suggestName("_debug")
+                debug := vrf.write
+                annotate(new ChiselAnnotation {
+                  override def toFirrtl: Annotation =
+                    firrtl.transforms.TopWiring.TopWiringAnnotation(debug.toTarget, "verilator_debug_")
+                })
+              }
+              }
+            ),
             RunFirrtlTransformAnnotation(new VerilogEmitter)
           ): AnnotationSeq
         ) { case (annos, stage) => stage.transform(annos) }
