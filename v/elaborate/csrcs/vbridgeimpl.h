c97e5fbb8ac0d98aa11d6dbcd350141eee47c76e
Jiuyang Liu <liu@jiuyang.me>
2022-10-15 16:36:33 +0800

    [cosim] add spike, rtl communication queue.

--- v/elaborate/csrcs/vbridgeimpl.h
+++ v/elaborate/csrcs/vbridgeimpl.h
@@ -1,12 +1,21 @@
 #ifndef V_VBRIDGEIMPL_H
 #define V_VBRIDGEIMPL_H
 
+#include <queue>
+
 #include "VV.h"
 #include "verilated_fst_c.h"
 #include "mmu.h"
 #include "simple_sim.h"
 
 constexpr int numTL = 2;
+
+class SpikeToRTL {
+
+} ;
+class RTLEvent {
+
+} ;
 
 struct TLBank {
     uint32_t data;
@@ -46,6 +55,16 @@ private:
     processor_t proc;
     uint64_t _cycles;
     TLBank banks[numTL];
+    /// to rtl stack
+    /// in the spike thread, spike should detech if this queue is full, if not full, execute until a vector instruction,
+    /// record the behavior of this instruction, and send to str_stack.
+    /// in the RTL thread, the RTL driver will consume from this queue, drive signal based on the queue.
+    std::queue<SpikeToRTL> to_rtl_queue;
+
+    /// from rtl queue
+    /// in the RTL thread, for each RTL cycle, valid signals should be checked, generate events, let testbench be able
+    /// to check the correctness of RTL behavior, benchmark performance signals.
+    std::queue<RTLEvent> from_rtl_queue;
 
     inline void reset();
     inline void rtl_tick();
