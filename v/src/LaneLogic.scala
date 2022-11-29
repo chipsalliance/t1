f544aa538d79d321fc8b82b53012ceca117e6c75
qinjun-li <liqinjun1994@gmial.com>
2022-11-29 16:03:10 +0800

    [rtl] Modify logic-unit according to the instruction decode.

--- v/src/LaneLogic.scala
+++ v/src/LaneLogic.scala
@@ -4,7 +4,8 @@ import chisel3._
 
 class LaneLogicRequest(param: DataPathParam) extends Bundle {
   val src:    Vec[UInt] = Vec(2, UInt(param.dataWidth.W))
-  val opcode: UInt = UInt(3.W)
+  /** n_op ## op_n ## op*/
+  val opcode: UInt = UInt(4.W)
 }
 
 class LaneLogic(param: DataPathParam) extends Module {
@@ -14,8 +15,8 @@ class LaneLogic(param: DataPathParam) extends Module {
   resp := VecInit(req.src.map(_.asBools).transpose.map {
     case Seq(sr0, sr1) =>
       val bitCalculate = Module(new LaneBitLogic)
-      bitCalculate.src := sr0 ## sr1
+      bitCalculate.src := sr0 ## (req.opcode(2) ^ sr1)
       bitCalculate.opcode := req.opcode
-      bitCalculate.resp
+      bitCalculate.resp ^ req.opcode(3)
   }).asUInt
 }
