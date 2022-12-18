cd661f855d601cb526e29dc770f85eaaa7b44c91
Jiuyang Liu <liu@jiuyang.me>
2022-12-18 19:16:06 +0800

    [cosim] prototype DPI

---
+++ tests/elaborate/src/GrandCentral.scala
@@ -0,0 +1,43 @@
+package sifive {
+  package enterprise {
+    package grandcentral {
+
+      import firrtl.annotations._
+
+      case class ReferenceDataTapKey(source: ReferenceTarget, sink: ReferenceTarget)
+
+      case class DataTapsAnnotation(keys: Seq[ReferenceDataTapKey]) extends NoTargetAnnotation with HasSerializationHints {
+        override def typeHints: Seq[Class[_]] = Seq(classOf[ReferenceDataTapKey])
+      }
+    }
+
+  }
+
+}
+
+package tests {
+  package elaborate {
+
+    import chisel3._
+    import chisel3.experimental.ChiselAnnotation
+    import sifive.enterprise.grandcentral._
+    trait TapModule extends RawModule { t =>
+      private val dataTapKeys = scala.collection.mutable.ArrayBuffer[(Data, Data)]()
+      def tap[T <: Data](source: T): T = {
+        val sink = Wire(chiselTypeOf(source))
+        dontTouch(sink)
+        dataTapKeys.append((source, sink))
+        sink
+      }
+      // wait for https://github.com/chipsalliance/chisel3/pull/1943
+      def done(): Unit = {
+        chisel3.experimental.annotate(new ChiselAnnotation {
+          override def toFirrtl = DataTapsAnnotation(dataTapKeys.toSeq.map({ case (source, sink) =>
+            ReferenceDataTapKey(source.toTarget, sink.toTarget)
+          }))
+        })
+      }
+    }
+  }
+
+}
--- tests/elaborate/src/Main.scala
+++ tests/elaborate/src/Main.scala
@@ -7,51 +7,24 @@ import chisel3.stage.ChiselGeneratorAnnotation
 import firrtl.AnnotationSeq
 import firrtl.stage.FirrtlCircuitAnnotation
 import mainargs._
-import v.{LSU, RegFile, V, VRF}
 
 object Main {
-  @main def elaborate(@arg(name = "dir") dir: String) = {
+  @main def elaborate(@arg(name="dir") dir: String) = {
     var topName: String = null
     val annos: AnnotationSeq = Seq(
       new chisel3.stage.phases.Elaborate,
-      new chisel3.stage.phases.AspectPhase,
-      new chisel3.tests.elaborate.Convert,
-      new chisel3.aop.injecting.InjectingPhase,
+      new chisel3.tests.elaborate.Convert
     ).foldLeft(
-      AnnotationSeq(Seq(
-        ChiselGeneratorAnnotation(() => new v.V(v.VParam())),
-        InjectingAspect(
-          { dut: V => Select.collectDeep(dut) { case v: V => v } },
-          { v: V => chisel3.experimental.Trace.traceName(v.instCount) }
-        ),
-        InjectingAspect(
-          { dut: V => Select.collectDeep(dut) { case vrf: VRF => vrf } },
-          { vrf: VRF => chisel3.experimental.Trace.traceName(vrf.write) }
-        ),
-        InjectingAspect(
-          { dut: V => Select.collectDeep(dut) { case lsu: LSU => lsu } },
-          { lsu: LSU =>
-            lsu.writeQueueVec.map(queue => chisel3.experimental.Trace.traceName(queue.io.enq))
-          }
-        ),
-        InjectingAspect(
-          { dut: V => Select.collectDeep(dut) { case lsu: LSU => lsu } },
-          { lsu: LSU =>
-            val reqEnqDBG = RegNext(lsu.reqEnq).suggestName("reqEnq_debug")
-            chisel3.dontTouch(reqEnqDBG)
-            chisel3.experimental.Trace.traceName(reqEnqDBG)
-          }
-        ),
-        InjectingAspect(
-          { dut: V => Select.collectDeep(dut) { case regFile: RegFile => regFile } },
-          { regFile: RegFile => chisel3.experimental.Trace.traceName(regFile.writePort) }
-        ),
-      ))
-    ) { case (anno, phase) => phase.transform(anno) }.flatMap {
+      Seq(
+        ChiselGeneratorAnnotation(() => new TestBench)
+      ): AnnotationSeq
+    )
+    { case (annos, stage) => stage.transform(annos) }.flatMap {
       case FirrtlCircuitAnnotation(circuit) =>
         topName = circuit.main
         os.write(os.Path(dir) / s"$topName.fir", circuit.serialize)
         None
+      case _: chisel3.stage.DesignAnnotation[_] => None
       case a => Some(a)
     }
     os.write(os.Path(dir) / s"$topName.anno.json", firrtl.annotations.JsonProtocol.serialize(annos))
