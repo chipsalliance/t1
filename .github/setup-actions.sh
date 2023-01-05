dc36cfd20a34f40fc76a2bec57c262d1a1f29abd
Liu Xiaoyi <circuitcoder0@gmail.com>
2023-01-05 13:11:07 +0800

    Properly echo in post-build-hooks

--- .github/setup-actions.sh
+++ .github/setup-actions.sh
@@ -5,9 +5,9 @@ set -eu
 set -f # disable globbing
 export IFS=' '
 
->&2 echo "[Post Build Hook] Signing paths" $OUT_PATHS
+echo "Signing paths" $OUT_PATHS
 nix store sign --key-file /etc/nix/cache-key.pem $OUT_PATHS
->&2 echo "[Post Build Hook] Uploading paths" $OUT_PATHS
+echo "Uploading paths" $OUT_PATHS
 nix copy --to 's3://nix?profile=nix-upload&scheme=https&endpoint=minio.inner.fi.c-3.moe&secret-key=/etc/nix/cache-key.pem' $OUT_PATHS
 EOF
 
