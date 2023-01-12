61120d430d05de7d884b913c87bcf2b8fe8f7b85
qinjun-li <liqinjun1994@gmail.com>
2023-01-12 13:10:12 +0800

    [rtl] Handling overflow for average.

--- v/src/LaneAdder.scala
+++ v/src/LaneAdder.scala
@@ -23,7 +23,14 @@ class LaneAdder(param: DataPathParam) extends Module {
   // TODO: adder
   val (s, c) = csa32(req.src.head, req.src(1), req.src.last)
   val addResult: UInt = s +& (c ## false.B)
-  val averageResult: UInt = ((req.sign && addResult(param.dataWidth - 1)) ## (addResult >> 1).asUInt(30, 0)) + Mux1H(
+  /** 因为 average 会右移, 所以需要比[[param.dataWidth]]多一位的结果
+    * 对于有符号的而言,多出来的是符号位扩展
+    * 对于无符号的而言,多出来的是上面加的进位
+    * For vaaddu and vaadd there can be no overflow in the result. For
+    * vasub and vasubu, overflow is ignored and the result wraps around.
+    * todo：vasub and vasubu
+    */
+  val averageResult: UInt = (Mux(req.sign, addResult(param.dataWidth - 1), addResult(param.dataWidth)) ## (addResult >> 1).asUInt(30, 0)) + Mux1H(
     UIntToOH(csr.vxrm),
     Seq(addResult(0), addResult(0) && addResult(1), false.B, addResult(0) && !addResult(1))
   )
