5f4981c45dabc44477d87358237b6460313a1a01
Yanqi Yang <shxxyyq0000@qq.com>
2023-02-08 21:55:23 +0800

    add srt wrapper

--- v/src/LaneDiv.scala
+++ v/src/LaneDiv.scala
@@ -26,9 +26,12 @@ class LaneDiv(param: DataPathParam) extends Module {
   }
 
   val div: SRT = Module(new SRT(32, 32, 32))
-  div.input.bits.divider := srcExtend.head
-  div.input.bits.dividend := srcExtend.head
-  div.input.bits.counter := 8.U
+  val wrapper = Module(new SRTWrapper)
+  wrapper.io.dividendIn := req.bits.src.last
+  wrapper.io.divisorIn := req.bits.src.head
+  div.input.bits.divider := wrapper.io.divisorOut
+  div.input.bits.dividend := wrapper.io.dividendOut
+  div.input.bits.counter := wrapper.io.counter
   div.input.valid := req.valid
   req.ready := div.input.ready
 
@@ -36,4 +39,106 @@ class LaneDiv(param: DataPathParam) extends Module {
 
   resp.valid := div.output.valid
   resp.bits := Mux(remReg, div.output.bits.reminder, div.output.bits.quotient)
+}
+
+class SRTWrapper extends Module{
+  val io = IO(new Bundle{
+    val dividendIn  = Input(UInt(32.W))
+    val divisorIn   = Input(UInt(32.W))
+    val dividendOut = Output(UInt(32.W))
+    val divisorOut  = Output(UInt(32.W))
+    val counter     = Output(UInt(5.W))
+  })
+  val LZC0 = Module(new LZC32)
+  val LZC1 = Module(new LZC32)
+  LZC0.io.a := io.dividendIn
+  LZC1.io.a := io.divisorIn
+
+  // 6-bits , above zero
+  // add one bit for calculate complement
+  val zeroHeadDividend = Wire(UInt(6.W))
+  val zeroHeadDivisor = Wire(UInt(6.W))
+  zeroHeadDividend := ~LZC0.io.z
+  zeroHeadDivisor := ~LZC1.io.z
+
+  // sub = zeroHeadDivider - zeroHeadDividend
+  val sub = Wire(UInt(6.W))
+  sub := addition.prefixadder.koggeStone(-zeroHeadDividend, zeroHeadDivisor, false.B)
+
+  // needComputerWidth: Int = zeroHeadDivider - zeroHeadDividend + 2
+  val needComputerWidth = Wire(UInt(7.W))
+  needComputerWidth := addition.prefixadder.koggeStone(sub, 2.U, false.B)
+
+  // noguard: Boolean = needComputerWidth % radixLog2 == 0
+  val noguard = !needComputerWidth(0)
+
+  // counter: Int = (needComputerWidth + 1) / 2
+  val counter = (addition.prefixadder.koggeStone(needComputerWidth, 1.U, false.B) >> 1).asUInt
+  // leftShiftWidthDividend: Int = zeroHeadDividend - (if (noguard) 0 else 1)
+  val leftShiftWidthDividend = Wire(UInt(6.W))
+  val leftShiftWidthDivisor = Wire(UInt(6.W))
+  leftShiftWidthDividend := Mux(noguard,zeroHeadDividend(4,0),
+    addition.prefixadder.koggeStone(zeroHeadDividend(4,0), "b111111".asUInt, false.B))
+  leftShiftWidthDivisor := zeroHeadDivisor(4,0)
+  io.divisorOut := io.divisorIn << leftShiftWidthDivisor
+  io.dividendOut := io.dividendIn << leftShiftWidthDividend
+  io.counter := counter
+}
+
+class LZC8 extends Module{
+  val io = IO(new Bundle{
+    val a = Input(UInt(8.W))
+    val z = Output(UInt(3.W))
+    val v = Output(UInt(1.W))
+  })
+  val a = io.a
+  val z0 : UInt = (!(a(7) | (!a(6)) & a(5))) & ((a(6) | a(4)) | !(a(3) | (!a(2) & a(1))))
+  val z1 : UInt = !(a(7) | a(6)) & ((a(5) | a(4)) | !(a(3) | a(2)))
+  val z2 : UInt = !(a(7) | a(6)) & !(a(5) | a(4))
+  val _v : UInt = !(!(a(7) | a(6)) & !(a(5) | a(4))) | !(!(a(3) | a(2)) & !(a(1) | a(0)))
+
+  io.z := Cat(~z2,~z1,~z0)
+  io.v := _v
+}
+
+class LZC16 extends Module{
+  val io = IO(new Bundle{
+    val a = Input(UInt(16.W))
+    val z = Output(UInt(4.W))
+    val v = Output(UInt(1.W))
+  })
+  val L0 = Module(new LZC8)
+  val L1 = Module(new LZC8)
+  L1.io.a := io.a(15,8)
+  L0.io.a := io.a(7,0)
+
+  val flag = L1.io.v.asBool
+  val z3 = Mux(flag, 1.U, 0.U)
+  val z2 = Mux(flag, L1.io.z(2), L0.io.z(2))
+  val z1 = Mux(flag, L1.io.z(1), L0.io.z(1))
+  val z0 = Mux(flag, L1.io.z(0), L0.io.z(0))
+
+  io.z := Cat(z3,z2,z1,z0)
+  io.v := L1.io.v | L0.io.v
+}
+class LZC32 extends Module{
+  val io = IO(new Bundle{
+    val a = Input(UInt(32.W))
+    val z = Output(UInt(5.W))
+    val v = Output(UInt(1.W))
+  })
+  val L0 = Module(new LZC16)
+  val L1 = Module(new LZC16)
+  L1.io.a := io.a(31,16)
+  L0.io.a := io.a(15,0)
+
+  val flag = L1.io.v.asBool
+  val z4 = Mux(flag, 1.U, 0.U)
+  val z3 = Mux(flag, L1.io.z(3), L0.io.z(3))
+  val z2 = Mux(flag, L1.io.z(2), L0.io.z(2))
+  val z1 = Mux(flag, L1.io.z(1), L0.io.z(1))
+  val z0 = Mux(flag, L1.io.z(0), L0.io.z(0))
+
+  io.z := Cat(z4,z3,z2,z1,z0)
+  io.v := L1.io.v | L0.io.v
 }
