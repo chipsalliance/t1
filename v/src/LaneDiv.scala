fb78ca22041cfcec9de41667a71c11711d64a98c
Yanqi Yang <shxxyyq0000@qq.com>
2023-02-15 14:24:42 +0800

    update SRT Wrapper for signed division

--- v/src/LaneDiv.scala
+++ v/src/LaneDiv.scala
@@ -26,37 +26,85 @@ class LaneDiv(param: DataPathParam) extends Module {
   }
 
   val wrapper = Module(new SRTWrapper)
-  wrapper.input.bits.dividend := req.bits.src.last
-  wrapper.input.bits.divisor := req.bits.src.head
+  wrapper.input.bits.dividend := req.bits.src.last.asSInt
+  wrapper.input.bits.divisor := req.bits.src.head.asSInt
+  wrapper.input.bits.signIn := req.bits.sign
   wrapper.input.valid := req.valid
+  // todo
+  wrapper.input.bits.index := 0.U
 
   req.ready := wrapper.input.ready
 
   val remReg: Bool = RegEnable(req.bits.rem, false.B, req.fire)
 
   resp.valid := wrapper.output.valid
-  resp.bits := Mux(remReg, wrapper.output.bits.reminder, wrapper.output.bits.quotient)
+  resp.bits := Mux(remReg, wrapper.output.bits.reminder.asUInt, wrapper.output.bits.quotient.asUInt)
 }
 
 class SRTIn extends Bundle {
-  val dividend = UInt(32.W)
-  val divisor = UInt(32.W)
+  val dividend = SInt(32.W)
+  val divisor = SInt(32.W)
+  val signIn = Bool()
+  //index =  UInt(log2Ceil(param.dataPathByteWidth).W)
+  val index = UInt(2.W)
 }
+
+class SRTOut extends Bundle {
+  val reminder = SInt(32.W)
+  val quotient = SInt(32.W)
+  val index = UInt(2.W)
+}
+/** 32-bits Divider for signed and unsigned division based on SRT4
+  *
+  * Input:
+  * dividend and divisor
+  * sign: true for signed input
+  *
+  * Component:
+  * divided by zero detection
+  * bigger divisor detection
+  * leading zero process
+  * sign process
+  */
 class SRTWrapper extends Module{
 
   val input = IO(Flipped(DecoupledIO(new SRTIn)))
-  val output = IO(ValidIO(new SRTOutput(32, 32)))
+  val output = IO(ValidIO(new SRTOut))
 
+  //abs
+  val abs = Module(new Abs(32))
+  abs.io.aIn := input.bits.dividend
+  abs.io.bIn := input.bits.divisor
+  abs.io.signIn := input.bits.signIn
+  val negative = abs.io.aSign ^ abs.io.bSign
+
+  //LZC
   val LZC0 = Module(new LZC32)
   val LZC1 = Module(new LZC32)
-  LZC0.io.a := input.bits.dividend
-  LZC1.io.a := input.bits.divisor
+  LZC0.io.a := abs.io.aOut
+  LZC1.io.a := abs.io.bOut
+
   val srt: SRT = Module(new SRT(32, 32, 32))
 
-  // pre-process
+  /** divided by zero detection */
+  val divideZero = Wire(Bool())
+  divideZero := (input.bits.divisor === 0.S) && input.fire
 
-  // 6-bits , above zero
-  // add one bit for calculate complement
+  /** bigger divisor detection */
+  val dividend = Wire(UInt(33.W))
+  val divisor = Wire(UInt(33.W))
+  val gap = Wire(UInt(34.W))
+  val biggerdivisor = Wire(Bool())
+  dividend := abs.io.aOut
+  divisor := abs.io.bOut
+  gap := addition.prefixadder.koggeStone(divisor, -dividend, false.B)
+  biggerdivisor := gap(33) && !(gap(32,0).orR === false.B) && input.fire
+
+  // bypass
+  val bypassSRT = divideZero || biggerdivisor
+
+  /** Leading Zero component*/
+  // extend one bit for calculation
   val zeroHeadDividend = Wire(UInt(6.W))
   val zeroHeadDivisor = Wire(UInt(6.W))
   zeroHeadDividend := ~LZC0.io.z
@@ -75,25 +123,56 @@ class SRTWrapper extends Module{
   val leftShiftWidthDividend = Wire(UInt(6.W))
   val leftShiftWidthDivisor = Wire(UInt(6.W))
   leftShiftWidthDividend := Mux(noguard,zeroHeadDividend(4,0),
-    addition.prefixadder.koggeStone(zeroHeadDividend(4,0), "b111111".asUInt, false.B))
+    addition.prefixadder.koggeStone(zeroHeadDividend(4,0), "b111111".U, false.B))
   leftShiftWidthDivisor := zeroHeadDivisor(4,0)
 
   // do SRT
-  srt.input.bits.divider := input.bits.divisor << leftShiftWidthDivisor
-  srt.input.bits.dividend := input.bits.dividend << leftShiftWidthDividend
+  srt.input.bits.dividend := abs.io.aOut << leftShiftWidthDividend
+  srt.input.bits.divider := abs.io.bOut << leftShiftWidthDivisor
   srt.input.bits.counter := counter
-  srt.input.valid := input.valid && !(input.bits.divisor === 0.U)
+  // if dividezero or biggerdivisor, bypass SRT
+  srt.input.valid := input.valid && !bypassSRT
+  // copy srt ready to top
   input.ready := srt.input.ready
 
-  // logic
-  val divideZero = Wire(Bool())
-  divideZero := (input.bits.divisor === 0.U) && input.fire
+  // post-process for sign
+  val quotientAbs = Wire(UInt(32.W))
+  val remainderAbs = Wire(UInt(32.W))
+  quotientAbs := srt.output.bits.quotient
+  remainderAbs := srt.output.bits.reminder >> zeroHeadDivisor
 
-  // post-process
-  output.valid := srt.output.valid | divideZero
-  output.bits.quotient := Mux(divideZero,"hffffffff".U(32.W), srt.output.bits.quotient)
-  output.bits.reminder := srt.output.bits.reminder
+  output.bits.index := input.bits.index
+  output.valid := srt.output.valid | bypassSRT
+  // the quotient of division by zero has all bits set, and the remainder of division by zero equals the dividend.
+  output.bits.quotient := Mux(divideZero,"hffffffff".U(32.W),
+    Mux(biggerdivisor, 0.U,
+      Mux(negative, -quotientAbs, quotientAbs))).asSInt
+  output.bits.reminder := Mux(divideZero, dividend(31,0),
+    Mux(biggerdivisor, Mux(abs.io.aSign, -dividend(31,0), dividend(31,0)),
+      Mux(abs.io.aSign, -remainderAbs, remainderAbs))).asSInt
+}
 
+class Abs(n: Int) extends Module {
+  val io = IO(new Bundle() {
+    val aIn = Input(SInt(n.W))
+    val bIn = Input(SInt(n.W))
+    val signIn = Input(Bool())
+    val aOut = Output(UInt(n.W))
+    val bOut = Output(UInt(n.W))
+    val aSign = Output(Bool())
+    val bSign = Output(Bool())
+    //debug
+  })
+  val a = Wire(SInt(n.W))
+  val b = Wire(SInt(n.W))
+  val aSign = io.aIn(n-1)
+  val bSign = io.bIn(n-1)
+  a := io.aIn
+  b := io.bIn
+  io.aOut := Mux(io.signIn, Mux(aSign, -a, a), a).asUInt
+  io.bOut := Mux(io.signIn, Mux(bSign, -b, b), b).asUInt
+  io.aSign := Mux(io.signIn,aSign,false.B)
+  io.bSign := Mux(io.signIn,bSign,false.B)
 }
 
 class LZC8 extends Module{
