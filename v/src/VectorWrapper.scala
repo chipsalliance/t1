7833918f16b0affcb7c276e9e5ebe9521a2c5443
qinjun-li <liqinjun1994@gmail.com>
2023-04-12 14:52:45 +0800

    [rtl] Temporarily ignore the vset instruction sent from the core.

--- v/src/VectorWrapper.scala
+++ v/src/VectorWrapper.scala
@@ -18,11 +18,12 @@ class VectorWrapper(parameter: VParameter) extends Module {
 
   // v主体
   val vector: V = Module(new V(parameter))
-
+  // 先忽视v set 类型的指令
+  val vSetInstruction: Bool = (request.bits.instruction(6, 0) === "0b1010111".U) && (request.bits.instruction(14, 12) === 7.U)
   val instructionQueue: Queue[InstructionQueueBundle] = Module(new Queue(new InstructionQueueBundle(parameter), parameter.instructionQueueSize))
   // queue 入口的连接,csr信息伴随指令走
-  instructionQueue.io.enq.valid := request.valid
-  request.ready := instructionQueue.io.enq.ready
+  instructionQueue.io.enq.valid := request.valid && !vSetInstruction
+  request.ready := instructionQueue.io.enq.ready || vSetInstruction
   instructionQueue.io.enq.bits.instruction := request.bits
   instructionQueue.io.enq.bits.csrInterface := csrInterface
 
