17f3c7378a1392d2d880a0ddf77dccad3fcdb1e7
qinjun-li <liqinjun1994@gmail.com>
2023-06-29 15:00:54 +0800

    [rtl] Maintaining the chaining of the LSU instruction.

--- v/src/V.scala
+++ v/src/V.scala
@@ -1197,6 +1197,7 @@ class V(val parameter: VParameter) extends Module with SerializableModule[VParam
       VecInit(v0ForLane.asBools.grouped(parameter.datapathWidth).toSeq.map(VecInit(_).asUInt))
     }(index)(lane.maskSelect)
     lane.lsuLastReport := lsu.lastReport
+    lane.lsuMaskGroupChange := lsu.lsuMaskGroupChange
     lane.lsuVRFWriteBufferClear := !lsu.vrfWritePort(index).valid && busClear
     lane.maskUnitFlushVrf := lastMaskUnitWrite
 
