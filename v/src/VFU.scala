a689f2b781c1abdf1bdb52774f2bdcd20bf4050f
midnighter95 <shxxyyq0000@qq.com>
2023-07-08 21:32:34 +0800

    add VFU draft

--- v/src/VFU.scala
+++ v/src/VFU.scala
@@ -1,3 +1,5 @@
+package v
+
 import chisel3._
 import chisel3.util._
 import hardfloat._
@@ -13,34 +15,40 @@ import hardfloat._
   * 0001 for div
   * 0010 for rdiv
   * 1000 for sqrt
-  * 1100 for sqrt7
   *
-  * For Compare/Convert
+  *
+  * For Compare
   * 0001 EQ
   * 0000 NQ
   * 0010 LT
   * 0011 LE
   * 0100 GT
   * 0101 GE
+  *
+  * 1000 min
+  * 1100 max
+  *
+  *
+  * none of these 3
+  *
+  * 0001 SGNJ
+  * 0010 SGNJN
+  * 0011 SGNJX
+
+  * 0100 classify
+  * 0101 merge
+  * 0110 sqrt7
+  * 0111 rec7
+  *
   * 1000 to float
   * 1010 to sint
   * 1001 to uint
   * 1110 to sint tr
   * 1101 to uint tr
   *
-  * none of these 3
-  *
-  * 0000 SGNJ
-  * 0001 SGNJN
-  * 0010 SGNJX
-  * 0101 min
-  * 0110 max
-  * 1000 classify
-  * 1001 merge
-  *
-  *
-  * */
-class VFUInput extends Bundle{
+  */
+class VFPUInput extends Bundle{
+  val unsigned = Bool()
   val in0 = UInt(32.W)
   val in1 = UInt(32.W)
   val in2 = UInt(32.W)
@@ -48,48 +56,248 @@ class VFUInput extends Bundle{
   val uop = UInt(4.W)
   val FMA = Bool()
   val DIV = Bool()
-  val CompareConvert = Bool()
+  val Compare = Bool()
   val reduction = Bool()
   val roundingMode = UInt(3.W)
 }
-class VFU extends Module{
-  val req  = IO(Flipped(Decoupled(new VFUInput)))
+
+class VFPU extends Module{
+  val req  = IO(Flipped(Decoupled(new VFPUInput)))
   val resp = IO(Decoupled(new Bundle(){
     val output = UInt(32.W)
+    val exceptionFlags = UInt(5.W)
   }))
-  val uop = req.bits.uop
-  val in2en = req.bits.in2en
 
-  val reverse12 = (req.bits.FMA && (req.bits.uop === 13.U)) || (req.bits.DIV && (req.bits.uop === 2.U))
-  val reverse23 = req.bits.FMA && req.bits.in2en && req.bits.uop(2)
-  val addsub = req.bits.FMA && req.bits.uop(3)
+  val recIn0 = recFNFromFN(8, 24, req.bits.in0)
+  val recIn1 = recFNFromFN(8, 24, req.bits.in1)
+  val recIn2 = recFNFromFN(8, 24, req.bits.in2)
 
-  //FMA
+  val uop = RegEnable(req.bits.uop, 0.U, req.fire)
+  val OtherEn = Wire(Bool())
+  OtherEn := !req.bits.FMA  && !req.bits.DIV && !req.bits.Compare
 
-  val fmaIn0 = Wire(UInt(32.W))
-  val fmaIn1 = Wire(UInt(32.W))
-  val fmaIn2 = Wire(UInt(32.W))
-  fmaIn0 := Mux(reverse12, req.bits.in1, req.bits.in0)
-  /**
-    * uop(3)==1 => addsub + rsub
-    * in2en && (uop(3,2) === 1.U => MAF
+  /** DIV insn lasts muticycles
+    * Not Div insn lasts 1 cycle
+    */
+
+  val DivSqrtSelect = RegEnable(req.bits.DIV    , false.B, req.fire)
+  val FMASelect     = RegEnable(req.bits.FMA    , false.B, req.fire)
+  val CompareSelect = RegEnable(req.bits.Compare, false.B, req.fire)
+  val OtherSelect   = RegEnable(OtherEn         , false.B, req.fire)
+
+  val occupied = RegInit(false.B)
+  val OneCyleValid = RegInit(false.B)
+
+  val VFPUresult = Wire(UInt(32.W))
+  val VFPUflags = Wire(UInt(5.W))
+
+  /** FMA
+    *
+    * addsub(with rsub) rsub mul  maf  rmaf
+    * a  in0               in1  in0  in0  in0
+    * b  1                 1    in1  in1  in2
+    * c  in1               in0  0    in2  in1
     *
     * */
-  fmaIn1 := Mux(uop(3), 1.U, Mux(in2en && (uop(3,2) === 1.U), req.bits.in2, req.bits.in1))
 
+  /** MAF control */
+  val rsub   = req.bits.FMA && uop(3,2) === 3.U
+  val mul    = req.bits.FMA && (!req.bits.in2en) && (uop(3,2) === 0.U)
+  val addsub = req.bits.FMA && req.bits.uop(3)
+  val maf    = req.bits.FMA && req.bits.in2en && (uop(3,2) === 0.U)
+  val rmaf   = req.bits.FMA && req.bits.in2en && (uop(3,2) === 1.U)
 
+  val fmaIn0 = Mux(rsub, recIn1, recIn0)
+  val fmaIn1 = Mux(addsub, 1.U, Mux(rmaf, recIn2, recIn1))
+  val fmaIn2 = Mux(rsub, recIn0,
+    Mux(mul, 0.U,
+      Mux(maf, recIn2,
+        recIn1) ))
 
-  val mulAddRecFN = Module(new MulAddRecFN(5, 11))
+  val mulAddRecFN = Module(new MulAddRecFN(8, 24))
   mulAddRecFN.io.op := req.bits.uop(1,0)
-  mulAddRecFN.io.a := recFNFromFN(5, 11, fmaIn0)
-  mulAddRecFN.io.b := recFNFromFN(5, 11, fmaIn1)
-  mulAddRecFN.io.c := recFNFromFN(5, 11, fmaIn2)
+  mulAddRecFN.io.a := fmaIn0
+  mulAddRecFN.io.b := fmaIn1
+  mulAddRecFN.io.c := fmaIn2
   mulAddRecFN.io.roundingMode := req.bits.roundingMode
   mulAddRecFN.io.detectTininess := false.B
 
+  /** DIV
+    *
+    * 0001 for div
+    * 0010 for rdiv
+    * 1000 for sqrt
+    * 1001 for sqrt7
+    * 1010 for rec7
+    *
+    *   div rdiv  sqrt      rec7   sqrt7phase0  sqrt7phase1
+    * a in0  in1  dontcare  1.U    dontcare      1.U
+    * b in1  in0  in0       in0    in0           result
+    *
+    */
+
+  val div      = req.bits.DIV && (uop === "b0001".U)
+  val rdiv     = req.bits.DIV && (uop === "b0010".U)
+  val sqrt     = req.bits.DIV && (uop === "b1000".U)
+  val sqrt7    = req.bits.DIV && (uop === "b1001".U)
+  val rec7     = req.bits.DIV && (uop === "b1001".U)
+
+  val sqrt7Phase1Next = Wire(Bool())
+  val sqrt7Phase1 = RegNext(sqrt7Phase1Next, false.B)
+  val sqrt7Phase1Valid = RegInit(false.B)
+
+  val divIn0 = Mux(div, recIn0,
+    Mux(rdiv, recIn1,
+      Mux(rec7 || sqrt7Phase1, 1.U, DontCare)))
+  val divIn1 = Mux(div, recIn1,
+    Mux(sqrt7Phase1, Divfnout, recIn0))
+
+  val DivsqrtResult = Wire(UInt(32.W))
+  val divSqrt = Module(new DivSqrtRecFN_small(8, 24,0))
+
+  divSqrt.io.a := divIn0
+  divSqrt.io.b := divIn1
+  divSqrt.io.roundingMode := req.bits.roundingMode
+  divSqrt.io.detectTininess := 0.U
+  divSqrt.io.sqrtOp := uop === "b1000".U
+  divSqrt.io.inValid := (req.valid && req.bits.DIV) || sqrt7Phase1Valid
+
+  val Divfnout = fNFromRecFN(8, 24, divSqrt.io.out)
+  val DivsqrtValid = Mux(sqrt7, divSqrt.io.outValid_sqrt && sqrt7Phase1, divSqrt.io.outValid_div || divSqrt.io.outValid_sqrt)
+  DivsqrtResult := Mux(rec7, Divfnout(23,17),
+    Mux(sqrt7 && sqrt7Phase1, Divfnout(23,17), Divfnout))
+  sqrt7Phase1Next := sqrt7Phase1Valid || (!DivsqrtValid &&  sqrt7Phase1)
+  sqrt7Phase1Valid := sqrt7 && divSqrt.io.outValid_sqrt
+
+  /** Compare
+    *
+    * 0001 EQ
+    * 0000 NQ
+    * 0010 LT
+    * 0011 LE
+    * 0100 GT
+    * 0101 GE
+    *
+    * 1000 min
+    * 1100 max
+    *
+    */
+  val Compare = Module(new CompareRecFN(8, 24))
+  Compare.io.a := recIn0
+  Compare.io.b := recIn1
+  Compare.io.signaling := false.B
+  val CompareResult = Wire(UInt(32.W))
+  val Compareflags = Wire(UInt(5.W))
+  CompareResult := Mux(uop === "b0001".U, Compare.io.eq,
+    Mux(uop === "b0000".U, !Compare.io.eq,
+      Mux(uop === "b0010".U, Compare.io.lt,
+        Mux(uop === "b0011".U, Compare.io.lt || Compare.io.eq,
+          Mux(uop === "b0100".U, Compare.io.gt,
+            Mux(uop === "b0101".U, Compare.io.gt || Compare.io.eq,
+              Mux(uop === "b1000".U , Mux(Compare.io.lt, req.bits.in0, req.bits.in1),
+                Mux(uop === "b1100".U,Mux(Compare.io.gt, req.bits.in0, req.bits.in1),
+                  false.B))))))))// todo false.B for illegal
+  Compareflags := Compare.io.exceptionFlags
 
 
+  /** Other
+    *
+    * uop(3,2) =
+    * 1x => convert
+    * 00 => sgnj
+    * 01 => clasify,merge
+    *
+    *
+    * 0001 SGNJ
+    * 0010 SGNJN
+    * 0011 SGNJX
+    *
+    * 0100 classify
+    * 0101 merge
+    *
+    * 1000 to float
+    * 1010 to sint
+    * 1001 to uint
+    * 1110 to sint tr
+    * 1101 to uint tr
+    *
+    * */
+  val IntToFn = Module(new INToRecFN(32, 8, 24))
+  IntToFn.io.in := req.bits.in0
+  IntToFn.io.signedIn := req.bits.unsigned
+  IntToFn.io.roundingMode := req.bits.roundingMode
+  IntToFn.io.detectTininess := false.B
+
+  /** io.signedOut define output sign
+    *
+    * false for toUInt
+    * true for toInt
+    */
+  val FnToInt = Module(new RecFNToIN(8, 24, 32))
+  val ConvertRtz = (uop(3,2) === 3.U) && req.bits.Compare
+  FnToInt.io.in := recIn0
+  FnToInt.io.roundingMode := Mux(ConvertRtz, "b001".U(3.W), req.bits.roundingMode)
+  FnToInt.io.signedOut := !(uop(3) && uop(0))
+
+  val ConvertResult = Wire(UInt(32.W))
+  val ConvertFlags = Wire(UInt(5.W))
+  ConvertResult := Mux(uop === "b1000".U, fNFromRecFN(8, 24, IntToFn.io.out), FnToInt.io.out)
+  ConvertFlags  := Mux(uop === "b1000".U, IntToFn.io.exceptionFlags, FnToInt.io.intExceptionFlags)
+
+  /** sgnj
+    *
+    * 0001 SGNJ
+    * 0010 SGNJN
+    * 0011 SGNJX
+    */
+  val sgnjresult = Wire(UInt(32.W))
+  val sgnjSign = Mux(OtherEn && uop === 1.U, req.bits.in1(31),
+    Mux(OtherEn && uop === 2.U, !req.bits.in1(31),
+      Mux(OtherEn && uop ===3.U, req.bits.in1(31) ^ req.bits.in0(31), false.B)))
+  sgnjresult := Cat(sgnjSign, req.bits.in0(30,0))
+
+  val OtherResult = Wire(UInt(32.W))
+  OtherResult := Mux(uop(3),ConvertResult,
+    Mux(uop(3,2) === "b00".U, sgnjresult,
+      Mux(uop === "b0100".U, classifyRecFN(8, 24, recFNFromFN(8, 24, req.bits.in0)), 0.U)))
 
 
+  /** stateMachine */
+  /** todo verify it*/
+  switch(occupied) {
+    is(false.B) {
+      occupied := req.fire
+    }
+    is(true.B) {
+      occupied := req.fire || Mux(DivSqrtSelect, !DivsqrtValid, false.B)
+    }
+  }
 
+  switch(OneCyleValid){
+    is(false.B){
+      OneCyleValid := req.fire && !req.bits.DIV
+    }
+    is(true.B){
+      OneCyleValid := false.B
+    }
+  }
+
+  /** Output */
+  req.ready := !occupied
+  resp.valid := DivsqrtValid || OneCyleValid
+
+  VFPUresult := Mux(DivSqrtSelect, DivsqrtResult,
+    Mux(FMASelect, fNFromRecFN(8, 23, mulAddRecFN.io.out),
+      Mux(CompareSelect , CompareResult,
+        Mux(OtherSelect , OtherResult,
+          0.U))))// todo 0.U for illegal
+
+  VFPUflags := Mux(DivSqrtSelect, divSqrt.io.exceptionFlags,
+    Mux(FMASelect, fNFromRecFN(8, 23, mulAddRecFN.io.exceptionFlags),
+      Mux(CompareSelect , Compareflags,
+        Mux(OtherSelect && uop(3), ConvertFlags,
+          0.U))))
+
+  resp.bits.output := VFPUresult
+  resp.bits.exceptionFlags := VFPUflags
 }
