34fcd44ee37909566ccd24f6e37f404bb49a6e23
Yanqi Yang <shxxyyq0000@qq.com>
2023-07-11 17:03:10 +0800

    update Mux for compare

--- v/src/LaneFloat.scala
+++ v/src/LaneFloat.scala
@@ -201,15 +201,15 @@ class LaneFloat(val parameter: LaneFloatParam) extends VFUModule(parameter) with
   val compareResult = Wire(UInt(32.W))
   val compareflags = Wire(UInt(5.W))
 
-  assert(uop === "b0001".U || uop === "b0000".U || uop === "b0010".U || uop === "b0011".U || uop === "b0100".U || uop === "b0101".U || uop === "b1000".U || uop === "b1100".U)
+  assert(!unitSeleOH(2) || (uop === "b0001".U || uop === "b0000".U || uop === "b0010".U || uop === "b0011".U || uop === "b0100".U || uop === "b0101".U || uop === "b1000".U || uop === "b1100".U))
   compareResult := Mux(uop === "b1000".U , Mux(compareModule.io.lt, request.src(0), request.src(1)),
-    Mux(uop === "b0011".U, compareModule.io.lt || compareModule.io.eq,
-      Mux(uop === "b0101".U, compareModule.io.gt || compareModule.io.eq,
-        Mux(uop === "b0010".U, compareModule.io.lt,
-          Mux(uop === "b0100".U, compareModule.io.gt,
-            Mux(uop === "b0000".U, !compareModule.io.eq,
-              Mux(uop === "b0001".U, compareModule.io.eq,
-                Mux(compareModule.io.gt, request.src(0), request.src(1)))))))))
+    Mux(uop === "b1100".U, Mux(compareModule.io.gt, request.src(0), request.src(1)),
+     Mux(uop === "b0011".U, compareModule.io.lt || compareModule.io.eq,
+       Mux(uop === "b0101".U, compareModule.io.gt || compareModule.io.eq,
+         Mux(uop === "b0010".U, compareModule.io.lt,
+           Mux(uop === "b0100".U, compareModule.io.gt,
+             Mux(uop === "b0000".U, !compareModule.io.eq,
+               compareModule.io.eq)))))))
   compareflags := compareModule.io.exceptionFlags
 
 
