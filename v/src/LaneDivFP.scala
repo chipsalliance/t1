26c084cf9e84f39bf96c3befd10d2537d892aaa5
Yanqi Yang <shxxyyq0000@qq.com>
2023-08-27 09:49:41 +0800

    [rtl] switch to new RoundingUnit

--- v/src/LaneDivFP.scala
+++ v/src/LaneDivFP.scala
@@ -118,11 +118,15 @@ class SRTFPWrapper(expWidth: Int, sigWidth: Int) extends Module {
   val rawA_S = rawFloatFromFN(expWidth, sigWidth, input.bits.a)
   val rawB_S = rawFloatFromFN(expWidth, sigWidth, input.bits.b)
 
-  /** Exceptions */
+  // Exceptions
+
+  /** inf/inf and 0/0  => NaN out */
   val notSigNaNIn_invalidExc_S_div =
     (rawA_S.isZero && rawB_S.isZero) || (rawA_S.isInf && rawB_S.isInf)
+  /** -Inf + -normal => NaN out */
   val notSigNaNIn_invalidExc_S_sqrt =
     !rawA_S.isNaN && !rawA_S.isZero && rawA_S.sign
+  /** isSigNaNRawFloat detect signaling NaN */
   val majorExc_S =
     Mux(input.bits.sqrt,
       isSigNaNRawFloat(rawA_S) || notSigNaNIn_invalidExc_S_sqrt,
@@ -130,20 +134,25 @@ class SRTFPWrapper(expWidth: Int, sigWidth: Int) extends Module {
         notSigNaNIn_invalidExc_S_div ||
         (!rawA_S.isNaN && !rawA_S.isInf && rawB_S.isZero)
     )
+
+  /** all cases result in NaN output */
   val isNaN_S =
     Mux(input.bits.sqrt,
       rawA_S.isNaN || notSigNaNIn_invalidExc_S_sqrt,
       rawA_S.isNaN || rawB_S.isNaN || notSigNaNIn_invalidExc_S_div
     )
-  val isInf_S =  Mux(input.bits.sqrt, rawA_S.isInf, rawA_S.isInf || rawB_S.isZero)
+  val isInf_S = Mux(input.bits.sqrt, rawA_S.isInf, rawA_S.isInf || rawB_S.isZero)
   val isZero_S = Mux(input.bits.sqrt, rawA_S.isZero, rawA_S.isZero || rawB_S.isInf)
 
   val majorExc_Z = RegEnable(majorExc_S, false.B, input.fire)
-  val isNaN_Z    = RegEnable(isNaN_S, false.B, input.fire)
-  val isInf_Z    = RegEnable(isInf_S, false.B, input.fire)
-  val isZero_Z   = RegEnable(isZero_S, false.B, input.fire)
+  val isNaN_Z = RegEnable(isNaN_S, false.B, input.fire)
+  val isInf_Z = RegEnable(isInf_S, false.B, input.fire)
+  val isZero_Z = RegEnable(isZero_S, false.B, input.fire)
 
+  /** invalid operation flag */
   val invalidExec = majorExc_Z && isNaN_Z
+
+  /** DivideByZero flag */
   val infinitExec = majorExc_Z && !isNaN_Z
 
   val specialCaseA_S = rawA_S.isNaN || rawA_S.isInf || rawA_S.isZero
@@ -156,18 +165,36 @@ class SRTFPWrapper(expWidth: Int, sigWidth: Int) extends Module {
   val fastValid = RegInit(false.B)
   fastValid := specialCase_S && input.fire && fractEn
 
-  // needNorm for div
-  val needNormNext = input.bits.b(sigWidth - 2, 0) > input.bits.a(sigWidth - 2, 0)
-  val needNorm = RegEnable(needNormNext, input.fire)
+
 
   // sign
   val signNext = Mux(input.bits.sqrt, false.B, rawA_S.sign ^ rawB_S.sign)
   val signReg = RegEnable(signNext, input.fire)
 
   // sqrt input
-  val adjustedExp = Cat(rawA_S.sExp(expWidth - 1), rawA_S.sExp(expWidth - 1, 0))
-  val sqrtExpIsEven = input.bits.a(sigWidth - 1)
-  val sqrtFractIn = Mux(sqrtExpIsEven, Cat("b0".U(1.W), rawA_S.sig(sigWidth - 1, 0), 0.U(1.W)),
+
+  /** construct expForSqrt
+    *
+    * sExp first 2 bits
+    * 00 -> 10 (subnormal)
+    * 01 -> 11 (true exp negative)
+    * 10 -> 00 (true exp positive)
+    *
+    */
+  val expfirst2 = UIntToOH(rawA_S.sExp(expWidth, expWidth - 1))
+  /** @todo expfirst2(3) never happens */
+  val expstart = Mux1H(
+    Seq(
+      expfirst2(0) -> "b10".U,
+      expfirst2(1) -> "b11".U,
+      expfirst2(2) -> "b00".U,
+      expfirst2(3) -> "b00".U
+    )
+  )
+
+  val expForSqrt = Cat(expstart, rawA_S.sExp(expWidth - 2, 0)) >> 1
+  val sqrtExpIsOdd = !rawA_S.sExp(0)
+  val sqrtFractIn = Mux(sqrtExpIsOdd, Cat("b0".U(1.W), rawA_S.sig(sigWidth - 1, 0), 0.U(1.W)),
     Cat(rawA_S.sig(sigWidth - 1, 0), 0.U(2.W)))
 
   val SqrtModule = Module(new SquareRoot(2, 2, sigWidth + 2, sigWidth + 2))
@@ -287,12 +314,13 @@ class SRTFPWrapper(expWidth: Int, sigWidth: Int) extends Module {
   intResult := Mux(remReg, intRemainder, intQuotient)
 
   // -------------------- FP result collect -----------------------------------------
+  val needRightShift = !divModule.output.bits.quotient(27)
   val sigToRound_sqrt = SqrtModule.output.bits.result(24, 2)
   val rbits_sqrt = SqrtModule.output.bits.result(1) ## (!SqrtModule.output.bits.zeroRemainder || SqrtModule.output.bits.result(0))
 
-  val sigToRound_div = Mux(needNorm, divModule.output.bits.quotient(calWidth - 3, calWidth - sigWidth - 1),
+  val sigToRound_div = Mux(needRightShift, divModule.output.bits.quotient(calWidth - 3, calWidth - sigWidth - 1),
     divModule.output.bits.quotient(calWidth - 2, calWidth - sigWidth))
-  val rbits_div = Mux(needNorm, divModule.output.bits.quotient(calWidth - sigWidth - 2) ## 1.U(1.W),
+  val rbits_div = Mux(needRightShift, divModule.output.bits.quotient(calWidth - sigWidth - 2) ## 1.U(1.W),
     divModule.output.bits.quotient(calWidth - sigWidth - 1) ## 1.U(1.W))
 
 
@@ -301,17 +329,28 @@ class SRTFPWrapper(expWidth: Int, sigWidth: Int) extends Module {
   val rbitsToRound = Mux(opSqrtReg, rbits_sqrt, rbits_div)
 
   // exp logic
-  val expStoreNext = Wire(UInt(expWidth.W))
-  val expToRound   = Wire(UInt(expWidth.W))
+  val expStoreNext, expToRound = Wire(UInt((expWidth + 2).W))
+
+  /** expStore
+    *
+    * output is 10bits SInt
+    *
+    * for sqrt
+    * expForSqrt(7,0) effective is 8bits, MSB is sign
+    * extends 2 sign bit in MSB
+    * expStoreNext = 10bits
+    *
+    * todo define it format, important
+    */
   expStoreNext := Mux(input.bits.sqrt,
-    Cat(rawA_S.sExp(expWidth - 1), rawA_S.sExp(expWidth - 1, 0))(expWidth, 1),
-    input.bits.a(fpWidth - 1, sigWidth - 1) - input.bits.b(fpWidth - 1, sigWidth - 1))
-  val expStore = RegEnable(expStoreNext, 0.U(expWidth.W), input.fire)
-  expToRound := Mux(opSqrtReg, expStore, expStore - needNorm)
+    Cat(expForSqrt(7), expForSqrt(7), expForSqrt(7, 0)),
+    (rawA_S.sExp - rawB_S.sExp).asUInt)
+  val expStore = RegEnable(expStoreNext, 0.U((expWidth + 2).W), input.fire)
+  expToRound := Mux(opSqrtReg, expStore, expStore - needRightShift)
 
   val roundresult = RoundingUnit(
     signReg,
-    expToRound,
+    expToRound.asSInt,
     sigToRound,
     rbitsToRound,
     rmReg,
