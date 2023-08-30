db19347bd83166fda292805ac1c925c65856d02c
sequencer <sequencer@users.noreply.github.com>
2023-08-30 14:55:21 +0000

    [nix] bump testcase

--- nix/rvv-testcase-unwrapped.nix
+++ nix/rvv-testcase-unwrapped.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "latest";
   src = fetchurl {
     url = "https://github.com/sequencer/vector/releases/download/${version}/rvv-testcase.tar.gz";
-    sha256 = "sha256-hitMGx2ZZH/qzXZtPpgd9p7IgviDDzqska4jhSShsZk=";
+    sha256 = "sha256-Dbs22BnNaX+EEeWH+sEy4e7LIUv2pxW89cEsHW0Op2Q=";
   };
   dontUnpack = true;
   installPhase = ''
