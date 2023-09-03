77a63c7abcd88078c99001c38a4b71e10bafbb3e
sequencer <sequencer@users.noreply.github.com>
2023-09-03 16:49:28 +0000

    [nix] bump testcase to version 2023-09-03+25d3212

--- nix/rvv-testcase-unwrapped.nix
+++ nix/rvv-testcase-unwrapped.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   name = "rvv-testcase-unwrapped";
-  version = "latest";
+  version = "2023-09-03+25d3212";
   src = fetchurl {
     url = "https://github.com/chipsalliance/t1/releases/download/${version}/rvv-testcase.tar.gz";
-    sha256 = "sha256-Dbs22BnNaX+EEeWH+sEy4e7LIUv2pxW89cEsHW0Op2Q=";
+    sha256 = "sha256-zqDs+blxLrpDTVaRR9GFwu4t8E39T52rsc2eQhXLgpc=";
   };
   dontUnpack = true;
   installPhase = ''
