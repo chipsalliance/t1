5846f344dcd121d3bdedfdd90c5c4da788f717e6
Avimitin <dev@avimit.in>
2023-09-12 19:07:33 +0800

    [emulator] replace jsoncpp to nlohmann/json
    Signed-off-by: Avimitin <dev@avimit.in>

--- emulator/src/rtl_config.cc
+++ emulator/src/rtl_config.cc
@@ -1,24 +1,26 @@
 #include <fstream>
 #include <bit>
+#include <cstdint>
 
-#include <json/json.h>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 #include "rtl_config.h"
 
 RTLConfig::RTLConfig(const char *json_file_name) {
-  Json::Value root;
-  std::ifstream(json_file_name) >> root;
+  std::ifstream json_file(json_file_name);
+  json root = json::parse(json_file);
   const auto &para = root["design"]["parameter"];
-  x_len = para["xLen"].asUInt64();
-  v_len = para["vLen"].asUInt64();
+  para["xLen"].get_to(x_len);
+  para["vLen"].get_to(v_len);
   v_len_in_bytes = v_len / 8;
-  datapath_width = para["datapathWidth"].asUInt64();
+  para["datapathWidth"].get_to(datapath_width);
   datapath_width_in_bytes = datapath_width / 8;
   datapath_width_log2 = std::__bit_width(datapath_width_in_bytes) - 1;
-  lane_number = para["laneNumber"].asUInt64();
-  physical_address_width = para["physicalAddressWidth"].asUInt64();
-  chaining_size = para["chainingSize"].asUInt64();
-  vrf_write_queue_size = para["vrfWriteQueueSize"].asUInt64();
+  para["laneNumber"].get_to(lane_number);
+  para["physicalAddressWidth"].get_to(physical_address_width);
+  para["chainingSize"].get_to(chaining_size);
+  para["vrfWriteQueueSize"].get_to(vrf_write_queue_size);
 }
 
 size_t lsu_idx_default = 255;
