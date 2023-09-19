bfaea1b563dd1216ad291aedcf93812da2d07b34
Yanqi Yang <shxxyyq0000@qq.com>
2023-09-19 14:50:13 +0800

    [rtl] opt multiplier64 with vectorAdder64

--- v/src/VFU/Multiplier16.scala
+++ v/src/VFU/Multiplier16.scala
@@ -1,14 +1,14 @@
 package VFU
 
-import addition.prefixadder.common.BrentKungSum
 import chisel3._
 import chisel3.util._
 
 class Multiplier16 extends Module{
   val a = IO(Input(UInt(16.W)))
   val b = IO(Input(UInt(16.W)))
-  val z = IO(Output(UInt(32.W)))
   val sew = IO(Input(UInt(2.W)))
+  val outCarry = IO(Output(UInt(32.W)))
+  val outSum   = IO(Output(UInt(32.W)))
 
   val a0Vec = a(7, 0).asBools
   val a1Vec = a(15, 8).asBools
@@ -37,18 +37,12 @@ class Multiplier16 extends Module{
   val a1x1: Seq[UInt] = a1Vec.zipWithIndex.map {
     case (a, i) => make8BitsPartialProduct((a, i), b(15, 8))
   }
-
   /** output effect width = 16 */
   def compress82(in: Seq[UInt]): (UInt, UInt) = {
     val layer0 = csa42(12)(VecInit(in.dropRight(4)))
-    val layer1 = csa42(12)(VecInit(in(4)>>4, in(5)(12, 4), in(6)(13, 4), in(7)(14, 4)))
+    val layer1 = csa42(12)(VecInit(in(4)>>4, in(5)>>4, in(6)>>4, in(7)>>4))
     val layerOut = csa42(16)(VecInit(layer0._1 << 1, layer0._2, layer1._1 << 5, layer1._2 << 4))
     ((layerOut._1(14, 0) << 1).asUInt, layerOut._2(15, 0))
-  }
-
-  def add82(in: Seq[UInt]): UInt = {
-    val compress = compress82(in)
-    compress._1.asUInt + compress._2.asUInt
   }
 
   val ax00 = compress82(a0x0)
@@ -58,36 +52,41 @@ class Multiplier16 extends Module{
   /** 16bits << 16 */
   val ax11 = compress82(a1x1)
 
-  val ax00result = addition.prefixadder.apply(BrentKungSum)(ax00._1(15,0) , ax00._2(15,0), false.B)
-  val ax11result = addition.prefixadder.apply(BrentKungSum)(ax11._1(15,0) , ax11._2(15,0), false.B)
+  val axSeq: Seq[(UInt, UInt)] = Seq(ax00,ax01,ax10,ax11)
+  def merge16(in: Seq[(UInt, UInt)]): (UInt, UInt) = {
+    val layer00 = csa42(24)(VecInit(
+      in(0)._1,
+      in(0)._2,
+      in(1)._1<<8,
+      in(1)._2<<8))
+    val layer01 = csa42(24)(VecInit(
+      in(2)._1,
+      in(2)._2,
+      in(3)._1 << 8,
+      in(3)._2 << 8))
+    val layer1 = csa42(32)(VecInit(
+      layer00._1(22,0)<<1,
+      layer00._2,
+      layer01._1(22,0)<<9,
+      layer01._2<<8))
+    ((layer1._1(30,0)<<1).asUInt, layer1._2(31,0))
+  }
 
-  val merge = csa42(16)(
-    VecInit(
-      ax10._1,
-      ax10._2,
-      ax01._1,
-      ax01._2))
-  val result16 = csa42(32)(
-    VecInit(
-      ax00result,
-      merge._1<<9,
-      merge._2<<8,
-      ax11result<<16))
-  val output16 = addition.prefixadder.apply(BrentKungSum)((result16._1<<1)(31,0).asUInt, result16._2(31,0))
+  val result16 = merge16(axSeq)
 
-  val output8 = ax11result(15,0) ## ax00result(15,0)
-
-  z := Mux(sew(0), output8, output16)
+  outCarry := Mux(sew(0), ax11._1(15,0) ## ax00._1(15,0), result16._1)
+  outSum   := Mux(sew(0), ax11._2(15,0) ## ax00._2(15,0), result16._2)
 }
 
 object Multiplier16 {
   def apply(a: UInt,
-             b: UInt, sew:UInt) = {
+            b: UInt,
+            sew: UInt) = {
     val mul16 = Module(new Multiplier16)
     // This need synthesis tool to do constant propagation
     mul16.a := a
     mul16.b := b
     mul16.sew := sew
-    mul16.z
+    Seq(mul16.outCarry, mul16.outSum)
   }
 }
--- v/src/VFU/Multiplier32.scala
+++ v/src/VFU/Multiplier32.scala
@@ -1,6 +1,5 @@
 package VFU
 
-import addition.prefixadder.common.BrentKungSum
 import chisel3._
 import chisel3.util._
 
@@ -14,9 +13,6 @@ class Multiplier32 extends Module{
   val a1Vec = a(31, 16).asBools
 
   val sewFor16 = Mux(sew(0), 1.U(2.W), 2.U(2.W))
-  val result00 = Multiplier16(a(15,0), b(15,0), sewFor16)
-  /** shift 32 */
-  val result11 = Multiplier16(a(31,16),b(31,16), sewFor16)
 
   def make16BitsPartialProduct(a: Tuple2[Bool, Int], in: UInt): UInt = { // Seq[(weight, value)]
     val exist = Mux(a._1, in, 0.U(16.W))
@@ -27,7 +23,6 @@ class Multiplier32 extends Module{
     doShift
   }
 
-  /** shift 16 */
   val a1x0: Seq[UInt] = a1Vec.zipWithIndex.map {
     case (a, i) => make16BitsPartialProduct((a, i), b(15, 0))
   }
@@ -37,7 +32,7 @@ class Multiplier32 extends Module{
   }
 
   /** output effect width = 32 */
-  def compress16_2(in: Seq[UInt]): (UInt, UInt) = {
+  def compress16_2(in: Seq[UInt]): Seq[UInt] = {
     val layer00 = csa42(20)(VecInit(in.dropRight(12)))
     /** 4 */
     val layer01 = csa42(20)(VecInit(in(4)>>4,  in(5)>>4,  in(6)>>4,  in(7)>>4))
@@ -49,27 +44,30 @@ class Multiplier32 extends Module{
     val layer10 = csa42(24)(VecInit(layer00._1 << 1, layer00._2, layer01._1 << 5, layer01._2 << 4))
     val layer11 = csa42(24)(VecInit(layer02._1 << 1, layer02._2, layer03._1 << 5, layer03._2 << 4))
     val layer2  = csa42(32)(VecInit(layer10._1 << 1, layer10._2, layer11._1 << 9, layer11._2 << 8))
-    ((layer2._1(31, 0) << 1).asUInt, layer2._2(31, 0))
+    Seq((layer2._1(30, 0) << 1).asUInt, layer2._2(31, 0))
   }
 
+  /** shift 16 */
   val ax01 = compress16_2(a0x1)
   val ax10 = compress16_2(a1x0)
 
-  val merge = csa42(32)(
-    VecInit(
-      ax10._1,
-      ax10._2,
-      ax01._1,
-      ax01._2))
+  val ax00: Seq[UInt] = Multiplier16(a(15,0) , b(15,0) , sewFor16)
+  /** shift 32 */
+  val ax11: Seq[UInt] = Multiplier16(a(31,16), b(31,16), sewFor16)
 
-  val result32 = csa42(64)(
-    VecInit(
-      result00,
-      (merge._1 << 17).asUInt,
-      (merge._2 << 16).asUInt,
-      result11 << 32))
-  val output32 = addition.prefixadder.apply(BrentKungSum)((result32._1 << 1).asUInt(63,0), result32._2(63,0))
-  val output8And16 = result11 ## result00
+  def compress8_2(in: Seq[UInt]): (UInt, UInt) = {
+    val layer00 = csa42(48)(VecInit(in(0), in(1), in(2) << 16, in(3) << 16))
+    val layer01 = csa42(48)(VecInit(in(4), in(5), in(6) << 16, in(7) << 16))
+    val layer1 = csa42(64)(VecInit(layer00._1 << 1, layer00._2, layer01._1 << 17, layer01._2 << 16))
+    ((layer1._1(62, 0) << 1).asUInt, layer1._2(63, 0))
+  }
 
-  z := Mux(sew(2), output32, output8And16)
+  val mergeSplit32: (UInt, UInt) = compress8_2(ax00 ++ ax01 ++ ax10 ++ ax11)
+
+  val sumForAdder   = Mux(sew(2), mergeSplit32._1, ax11(0) ## ax00(0))
+  val carryForAdder = Mux(sew(2), mergeSplit32._2, ax11(1) ## ax00(1))
+
+  val result = vectorAdder64(sumForAdder, carryForAdder, sew ## 0.U(1.W))
+
+  z := result
 }
