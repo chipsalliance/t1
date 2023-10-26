1f6bf0b79682d88dc3e95c24c9567a584d6fd9dd
Avimitin <dev@avimit.in>
2023-10-26 18:10:56 +0800

    [emulator] improve emulator logging performance
    - allow disable logging functionality
    - display cycle data for all log entry
    - use global logger to avoid construct/deconstruct thousands of logger instance
    
    Signed-off-by: Avimitin <dev@avimit.in>

--- emulator/src/spdlog-ext.cc
+++ emulator/src/spdlog-ext.cc
@@ -8,106 +8,8 @@
 #include <sstream>
 #include <string>
 
-#include <spdlog/async.h>
-#include <spdlog/common.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_sinks.h>
-#include <spdlog/spdlog.h>
-
 #include "spdlog-ext.h"
-
-/**
- * Get environment variable by the given `env` key or fallback to a default
- * value.
- */
-std::string getenv_or(const char *env, std::string fallback) {
-  char *env_var = std::getenv(env);
-  if (env_var && strlen(env_var) > 0) {
-    return std::string{env_var};
-  }
-
-  return fallback;
-}
-
-/**
- * Get environment variable and split them into std::vector by the given
- * `delimiter`
- */
-std::set<std::string> get_set_from_env(const char *env, const char delimiter) {
-  std::set<std::string> set;
-
-  auto raw = getenv_or(env, "");
-  if (raw.empty()) {
-    return set;
-  }
-
-  std::stringstream ss(raw);
-  std::string token;
-  // Use `delimiter` instead of '\n' to split element in string.
-  while (std::getline(ss, token, delimiter)) {
-    set.insert(token);
-  }
-
-  return set;
-}
-
-/**
- * Filter logging message by module type. Filter should be set by environment
- * `EMULATOR_BLACKLIST_MODULE` and `EMULATOR_WHITELIST_MODULE`. Module should be
- * separate by column `,`. When you set both env, only blacklist will have
- * effect.
- */
-class ConsoleSink : public spdlog::sinks::base_sink<std::mutex> {
-private:
-  std::set<std::string> blacklist;
-  std::set<std::string> whitelist;
-
-  bool is_expected_module(std::string &module) {
-    if (!blacklist.empty() && blacklist.find(module) != blacklist.end()) {
-      return false;
-    }
-    if (!whitelist.empty() && whitelist.find(module) == whitelist.end()) {
-      return false;
-    }
-
-    return true;
-  }
-
-public:
-  ConsoleSink()
-      : blacklist{get_set_from_env("EMULATOR_BLACKLIST_MODULE", ',')},
-        whitelist{get_set_from_env("EMULATOR_WHITELIST_MODULE", ',')} {}
-
-protected:
-  void sink_it_(const spdlog::details::log_msg &msg) override {
-    auto data = std::string(msg.payload.data(), msg.payload.size());
-    // Don't touch error message
-    if (msg.level == spdlog::level::info) {
-      std::string module_name;
-      try {
-        json payload = json::parse(data);
-        payload["name"].get_to(module_name);
-      } catch (const json::parse_error &ex) {
-        throw std::runtime_error(
-            fmt::format("Fail to convert msg {} to json: {}", data, ex.what()));
-      } catch (const json::type_error &ex) {
-        throw std::runtime_error(
-            fmt::format("Fail to get field name from: {}", data));
-      }
-
-      if (!is_expected_module(module_name)) {
-        return;
-      }
-    }
-
-    spdlog::memory_buf_t formatted;
-    spdlog::sinks::base_sink<std::mutex>::formatter_->format(msg, formatted);
-    // stdout will be captured by mill, so we need to print them into stderr
-    std::cerr << fmt::to_string(formatted);
-  }
-
-  void flush_() override { std::cerr << std::flush; }
-};
+#include "vbridge_impl.h"
 
 /**
  * Create two logging sink, one for creating JSON format logging file, another
@@ -117,36 +19,11 @@ protected:
 void setup_logger() {
   std::string log_path = getenv_or("EMULATOR_LOG_PATH", "emulator-log.json");
 
-  // Pre-process the file so that after program exit, it can be a valid JSON
-  // array.
-  spdlog::file_event_handlers f_handlers;
-  f_handlers.after_open = [](spdlog::filename_t _, std::FILE *fstream) {
-    fputs("[\n", fstream);
-  };
-  f_handlers.after_close = [](spdlog::filename_t log_path) {
-    std::ifstream old_log_file(log_path);
-    std::ofstream temp_file("temp-log.json");
-    std::string line;
-    while (std::getline(old_log_file, line)) {
-      temp_file << line;
-    }
-    // Drop the last ','
-    temp_file.seekp(-1, std::ios::cur);
-    temp_file.write("]\n", 2);
-    temp_file.close();
-    old_log_file.close();
-    std::filesystem::rename("temp-log.json", log_path);
-  };
-  auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-      log_path, /*truncate=*/true, f_handlers);
-  // Basic logging information, other data are nested in the `log` attribute
-  // set.
-  file_sink->set_pattern(
-      "{ \"timestamp\": \"%E\", \"level\": \"%l\", \"log\": %v },");
+  auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_path, /*truncate=*/true);
+  file_sink->set_pattern("%v");
 
   auto console_sink = std::make_shared<ConsoleSink>();
-  // %T: "23:55:59" (We don't need other information.)
-  console_sink->set_pattern("%T %v");
+  console_sink->set_pattern("- %v");
 
   // One thread with 8192 queue size
   spdlog::init_thread_pool(8192, 1);
@@ -168,4 +45,8 @@ void setup_logger() {
   spdlog::set_default_logger(logger);
 
   return;
+}
+
+uint64_t JsonLogger::get_cycle() {
+  return vbridge_impl_instance.get_t();
 }
--- emulator/src/spdlog-ext.h
+++ emulator/src/spdlog-ext.h
@@ -1,10 +1,220 @@
 #pragma once
 
+#include <set>
 #include <iostream>
-#include <nlohmann/json.hpp>
 #include <optional>
+
 #include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/common.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/spdlog.h>
+
+#include <nlohmann/json.hpp>
 using json = nlohmann::json;
+
+/**
+ * Get environment variable by the given `env` key or fallback to a default
+ * value.
+ */
+inline std::string getenv_or(const char *env, std::string fallback) {
+  char *env_var = std::getenv(env);
+  if (env_var && strlen(env_var) > 0) {
+    return std::string{env_var};
+  }
+
+  return fallback;
+}
+
+/**
+ * Get environment variable and split them into std::vector by the given
+ * `delimiter`
+ */
+inline std::set<std::string> get_set_from_env(const char *env, const char delimiter) {
+  std::set<std::string> set;
+
+  auto raw = getenv_or(env, "");
+  if (raw.empty()) {
+    return set;
+  }
+
+  std::stringstream ss(raw);
+  std::string token;
+  // Use `delimiter` instead of '\n' to split element in string.
+  while (std::getline(ss, token, delimiter)) {
+    set.insert(token);
+  }
+
+  return set;
+}
+
+/**
+ * Filter logging message by module type. Filter should be set by environment
+ * `EMULATOR_BLACKLIST_MODULE` and `EMULATOR_WHITELIST_MODULE`. Module should be
+ * separate by column `,`. When you set both env, only blacklist will have
+ * effect.
+ */
+class ConsoleSink : public spdlog::sinks::base_sink<std::mutex> {
+private:
+  std::set<std::string> whitelist;
+  bool enable_sink;
+
+  bool is_expected_module(std::string &module) {
+    if (!whitelist.empty() && whitelist.find(module) == whitelist.end()) {
+      return false;
+    }
+
+    return true;
+  }
+
+public:
+  ConsoleSink() {
+    auto no_log = getenv_or("EMULATOR_NO_LOG", "false");
+    enable_sink = (no_log == "false");
+
+    whitelist = get_set_from_env("EMULATOR_WHITELIST_MODULE", ',');
+    whitelist.insert("DPIInitCosim");
+    whitelist.insert("SpikeStep");
+    whitelist.insert("SimulationExit");
+  }
+
+protected:
+  void sink_it_(const spdlog::details::log_msg &msg) override {
+    if (!enable_sink) { return; };
+
+    auto data = std::string(msg.payload.data(), msg.payload.size());
+    // Don't touch error message
+    if (msg.level == spdlog::level::info) {
+      std::string module_name;
+      try {
+        json payload = json::parse(data);
+        payload["name"].get_to(module_name);
+      } catch (const json::parse_error &ex) {
+        throw std::runtime_error(
+            fmt::format("Fail to convert msg {} to json: {}", data, ex.what()));
+      } catch (const json::type_error &ex) {
+        throw std::runtime_error(
+            fmt::format("Fail to get field name from: {}", data));
+      }
+
+      if (!is_expected_module(module_name)) {
+        return;
+      }
+    }
+
+    spdlog::memory_buf_t formatted;
+    spdlog::sinks::base_sink<std::mutex>::formatter_->format(msg, formatted);
+    // stdout will be captured by mill, so we need to print them into stderr
+    std::cerr << fmt::to_string(formatted);
+  }
+
+  void flush_() override { std::cerr << std::flush; }
+};
+
+enum class LogType {
+  Info,
+  Warn,
+  Trace,
+  Fatal,
+};
+
+class JsonLogger {
+private:
+  json internal;
+  bool do_logging;
+
+  inline std::string dump() {
+    std::string ret;
+    try {
+      ret = internal.dump();
+    } catch (json::type_error &ex) {
+      throw std::runtime_error(fmt::format("JSON error for log '{}': {}",
+                                           internal["name"], ex.what()));
+    }
+    return ret;
+  }
+
+  uint64_t get_cycle();
+
+  template <typename... Arg>
+  inline void try_log(LogType log_type, std::optional<fmt::format_string<Arg...>> fmt, Arg &&...args) {
+    if (!do_logging) return;
+
+    if (fmt.has_value()) {
+      auto msg = fmt::format(fmt.value(), args...);
+      internal["message"] = msg;
+    }
+
+    internal["cycle"] = get_cycle();
+
+    switch(log_type) {
+      case LogType::Info : spdlog::info("{}", this->dump()); break;
+      case LogType::Warn : spdlog::warn("{}", this->dump()); break;
+      case LogType::Trace : spdlog::trace("{}", this->dump()); break;
+      case LogType::Fatal : {
+        spdlog::critical("{}", this->dump());
+        spdlog::shutdown();
+
+        throw std::runtime_error(internal["message"]);
+      }
+    }
+
+  }
+
+public:
+  JsonLogger() {
+    auto no_log = getenv_or("EMULATOR_NO_LOG", "false");
+    do_logging = (no_log == "false");
+  }
+  ~JsonLogger() = default;
+
+  JsonLogger operator()(const char*n) {
+    if (!do_logging) return *this;
+
+    internal.clear();
+    internal["name"] = n;
+    return *this;
+  }
+
+  template <typename T> JsonLogger &with(const char *key, T value) {
+    if (!do_logging) return *this;
+
+    internal["data"][key] = value;
+    return *this;
+  }
+
+  // Overload the index operator
+  json &operator[](const char *key) { return internal["info"][key]; };
+
+  inline void info() {
+    try_log(LogType::Info, std::nullopt);
+  }
+  inline void trace() {
+    try_log(LogType::Trace, std::nullopt);
+  }
+
+  template <typename... Arg>
+  inline void info(fmt::format_string<Arg...> fmt, Arg &&...args) {
+    try_log(LogType::Info, fmt, args...);
+  }
+
+  template <typename... Arg>
+  inline void warn(fmt::format_string<Arg...> fmt, Arg &&...args) {
+    try_log(LogType::Warn, fmt, args...);
+  }
+
+  template <typename... Arg>
+  inline void trace(fmt::format_string<Arg...> fmt, Arg &&...args) {
+    try_log(LogType::Trace, fmt, args...);
+  };
+
+  template <typename... Arg>
+  inline void fatal(fmt::format_string<Arg...> fmt, Arg &&...args) {
+    try_log(LogType::Fatal, fmt, args...);
+  };
+ };
+
 
 // Exported symbols
 #define FATAL(context)                                                         \
@@ -32,72 +242,4 @@ using json = nlohmann::json;
 
 void setup_logger();
 
-class Log {
-private:
-  json internal;
-
-  inline std::string dump() {
-    std::string ret;
-    try {
-      ret = internal.dump(/*indent=*/2);
-    } catch (json::type_error &ex) {
-      throw std::runtime_error(fmt::format("JSON error for log '{}': {}",
-                                           internal["name"], ex.what()));
-    }
-    return ret;
-  }
-
-public:
-  Log(const char *n) { internal["name"] = n; }
-  ~Log() = default;
-
-  template <typename T> Log &with(const char *key, T value) {
-    internal["data"][key] = value;
-    return *this;
-  }
-
-  // Overload the index operator
-  json &operator[](const char *key) { return internal["info"][key]; };
-
-  inline void info() { spdlog::info("{}", this->dump()); }
-  inline void trace() { spdlog::trace("{}", this->dump()); }
-
-  template <typename... Arg>
-  inline void info(fmt::format_string<Arg...> fmt, Arg &&...args) {
-    auto msg = fmt::format(fmt, args...);
-    internal["message"] = msg;
-    spdlog::info("{}", this->dump());
-  }
-
-  template <typename... Arg>
-  inline void warn(fmt::format_string<Arg...> fmt, Arg &&...args) {
-    auto msg = fmt::format(fmt, args...);
-    internal["message"] = msg;
-    spdlog::warn("{}", this->dump());
-  }
-
-  template <typename... Arg>
-  inline void critical(fmt::format_string<Arg...> fmt, Arg &&...args) {
-    auto msg = fmt::format(fmt, args...);
-    internal["message"] = msg;
-    spdlog::critical("{}", this->dump());
-  }
-
-  template <typename... Arg>
-  inline void trace(fmt::format_string<Arg...> fmt, Arg &&...args) {
-    auto msg = fmt::format(fmt, args...);
-    internal["message"] = msg;
-    spdlog::trace("{}", this->dump());
-  };
-
-  template <typename... Arg>
-  inline void fatal(fmt::format_string<Arg...> fmt, Arg &&...args) {
-    auto msg = fmt::format(fmt, args...);
-    internal["message"] = msg;
-
-    spdlog::critical("{}", this->dump());
-    spdlog::shutdown();
-
-    throw std::runtime_error(msg);
-  };
-};
+inline JsonLogger Log;
