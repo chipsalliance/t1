7379737ef31e7efcca2a14d3b30ff3fb2ea26c94
qinjun-li <liqinjun1994@gmail.com>
2023-10-31 18:28:01 +0800

    [test] Change smoke.asm length to test the performance of accessing vrf.

--- tests/asm/smoke.asm
+++ tests/asm/smoke.asm
@@ -1,23 +1,31 @@
 .global test
 test:
     addi x1, x1, 1
-    vsetivli x0, 16, e8, m1, ta, ma
+    li a0, 0x400
+    li a1, 0xc3
+    li a2, 0x10
+    vsetvl x0, a0, a1
     lui x30, 1
     auipc x31, 0
 
 add_test:
-    vadd.vi v0, v1, 10
-    vadd.vv v2, v1, v1
-    vadd.vi v1, v1, 7, v0.t
-    vadd.vx v1, v1, x1
+    vadd.vi v0, v0, 10
+    vadd.vi v16, v8, 11, v0.t
+    vadd.vv v8, v16, v0
+    vadd.vi v16, v8, 7, v0.t
+    vadd.vx v8, v16, x1
 
 chaining_test:
-    vadd.vx v1, v1, x1
-    vxor.vi v7, v5, 7
-    vsll.vi v1, v1, 1
+    vadd.vx v24, v16, x1
+    vxor.vi v8, v24, 7
+    vsll.vi v16, v8, 1
 
 ld_test:
-    vle8.v v4, (x30)
+    vle8.v v16, (x30)
+
+loop:
+    addi a2, a2, -1
+    bnez a2, add_test
 
 exit:
     # Write msimend to exit simulation.
