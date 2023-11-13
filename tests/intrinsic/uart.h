103225258e031714720cb2ec3c0a561f8662ba3d
Yangyu Chen <cyy@cyyself.name>
2023-11-13 16:54:36 +0800

    [build system] add UART testcase
    Signed-off-by: Yangyu Chen <cyy@cyyself.name>

---
+++ tests/intrinsic/uart.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdint.h>
+
+struct uartlite_regs {
+    volatile unsigned int rx_fifo;
+    volatile unsigned int tx_fifo;
+    volatile unsigned int status;
+    volatile unsigned int control;
+};
+
+struct uartlite_regs *const ttyUL0 = (struct uartlite_regs *)0x60000000;
+
+void uart_put_c(const char c) {
+  while (ttyUL0->status & (1<<3) /* transmit FIFO full */);
+  ttyUL0->tx_fifo = c;
+}
+
+void uart_put_s(const char *s) {
+  while (*s) {
+    uart_put_c(*s++);
+  }
+}
+
+void uart_put_hex_d(uint64_t x) {
+    for (int i=15;i>=0;i--) {
+        uint64_t res = (x >> (i * 4)) & 0xf;
+        uart_put_c(res >= 10 ? 'a' + res - 10 : '0' + res);
+    }
+}
---
+++ tests/intrinsic/uarttest.c
@@ -0,0 +1,8 @@
+#include "uart.h"
+
+void test() {
+    uart_put_s("Test Begin from UART!\n");
+    uart_put_hex_d(0xdeadbeef);
+    uart_put_c('\n');
+    uart_put_s("Test End from UART!\n");
+}
