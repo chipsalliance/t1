ecd020c4c509a96d26469a9a0201396170aecca1
qinjun-li <liqinjun1994@gmail.com>
2023-11-14 15:12:43 +0800

    [rtl] fix multi-cycle instruction control.

--- v/src/laneStage/Distributor.scala
+++ v/src/laneStage/Distributor.scala
@@ -19,6 +19,7 @@ class Distributor[T <: SlotRequestToVFU, B <: VFUResponseToSlot](enqueue: T, deq
   val responseToSlot: ValidIO[VFUResponseToSlot] = IO(Valid(dequeue))
 
   val requestReg: ValidIO[SlotRequestToVFU] = RegInit(0.U.asTypeOf(Valid(enqueue)))
+  val sendRequestValid: Bool = RegInit(false.B)
   val responseData: UInt = RegInit(0.U(enqueue.src.head.getWidth.W))
   val executeIndex = RegInit(0.U(2.W))
 
@@ -28,6 +29,10 @@ class Distributor[T <: SlotRequestToVFU, B <: VFUResponseToSlot](enqueue: T, deq
 
   when(requestFromSlot.fire ^ responseToSlot.fire) {
     requestReg.valid := requestFromSlot.fire
+  }
+  val lastRequestFire = Wire(Bool())
+  when(requestFromSlot.fire || lastRequestFire) {
+    sendRequestValid := requestFromSlot.fire
   }
 
   // enqueue fire
@@ -75,7 +80,7 @@ class Distributor[T <: SlotRequestToVFU, B <: VFUResponseToSlot](enqueue: T, deq
   /** the bit-level mask of current execution. */
   val bitMaskForExecution: UInt = FillInterleaved(8, byteMaskForExecution)
 
-  def CollapseOperand(data: UInt, enable: Bool = true.B, sign: Bool = false.B): UInt = {
+  def CollapseOperand(data: UInt, sign: Bool = false.B): UInt = {
     val dataMasked: UInt = data & bitMaskForExecution
     // when sew = 0
     val collapse0 = Seq.tabulate(4)(i => dataMasked(8 * i + 7, 8 * i)).reduce(_ | _)
@@ -92,7 +97,7 @@ class Distributor[T <: SlotRequestToVFU, B <: VFUResponseToSlot](enqueue: T, deq
   }
   val signSeq: Seq[Bool] = Seq(requestReg.bits.sign0, requestReg.bits.sign, false.B, false.B)
 
-  requestToVfu.valid := requestReg.valid
+  requestToVfu.valid := sendRequestValid
   requestToVfu.bits := requestReg.bits
   requestToVfu.bits.src := VecInit(
     requestReg.bits.src.zip(signSeq).map{ case (d, s) => CollapseOperand(d, s) }
@@ -103,15 +108,16 @@ class Distributor[T <: SlotRequestToVFU, B <: VFUResponseToSlot](enqueue: T, deq
   val isLastRequest: Bool = Mux1H(vSew1H, Seq(!remainder.orR, !remainder(1, 0).orR, true.B))
   val writeIndex: UInt = Wire(UInt(4.W))
   val isLastResponse: Bool = Wire(Bool())
+  lastRequestFire := isLastRequest && requestToVfu.fire
   writeIndex := executeIndex
   isLastResponse := isLastRequest
   if (multiCycle) {
-    val executeQueue = Module(new Queue(UInt(5.W), 4))
+    val executeQueue = Module(new Queue(UInt(3.W), 4))
     executeQueue.io.enq.bits := isLastRequest ## executeIndex
     executeQueue.io.enq.valid := requestToVfu.fire
     executeQueue.io.deq.ready := responseFromVfu.fire
-    writeIndex := executeQueue.io.deq.bits(3, 0)
-    isLastResponse := executeQueue.io.deq.bits(4)
+    writeIndex := executeQueue.io.deq.bits(1, 0)
+    isLastResponse := executeQueue.io.deq.bits(2) && responseFromVfu.valid && executeQueue.io.deq.valid
   }
 
   // update result
