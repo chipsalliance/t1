5d993e79293b7020947ccc61180a99ed67687b5f
Avimitin <dev@avimit.in>
2023-11-17 02:27:20 +0800

    [ci] fix unfinish change to bump testcase script
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/t1/testcases/rvv-testcases-prebuilt.nix
+++ nix/t1/testcases/rvv-testcases-prebuilt.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "rvv-testcases-prebuilt";
   version = "2023-09-03+25d3212";
   src = fetchurl {
-    url = "https://github.com/chipsalliance/t1/releases/download/${version}/rvv-testcase.tar.gz";
+    url = "https://github.com/chipsalliance/t1/releases/download/${version}/rvv-testcases.tar.gz";
     sha256 = "sha256-zqDs+blxLrpDTVaRR9GFwu4t8E39T52rsc2eQhXLgpc=";
   };
   dontUnpack = true;
