289acb42cb73e45af377fe0b76a6c9f46f7ddacd
Avimitin <dev@avimit.in>
2023-11-17 14:22:17 +0800

    [nix] fix unexpected file ordering produce in firtool
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/t1/elaborate.nix
+++ nix/t1/elaborate.nix
@@ -19,5 +19,13 @@ stdenvNoCC.mkDerivation {
 
     mfcArgs="$(jq -r '.mfcArgs[]' < "${elaborate-config}")"
     firtool $out/*.fir --annotation-file $out/*.anno.json -o $out $mfcArgs
+
+    # Fix file ordering difference introduced in some unknown breaking change between firtool 1.50 -> 1.58
+    # In the previous working version, all files starting with './' should be placed on top of the filelist.f.
+    # However in the latest version, they were placed at the bottom, which breaks the verilator.
+    # Here is an disgusting workaround to make it work. But we need to fix this issue at firtool side.
+    grep '^\./' $out/filelist.f > prefixed.f
+    grep -v '^\./' $out/filelist.f > not-prefixed.f
+    cat prefixed.f not-prefixed.f > $out/filelist.f
   '';
 }
