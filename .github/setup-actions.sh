3b3c988bb6ffdc27e4f671a7faf8155a080dd78b
SharzyL <me@sharzy.in>
2023-11-29 02:43:58 +0800

    [ci] simplify nix_config, disable StrictHostKeyChecking

--- .github/setup-actions.sh
+++ .github/setup-actions.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-eval $(ssh-agent -s)
-echo "${ROCKETCHIP_NIX_REMOTE_SSH_PRIVKEY}" |  ssh-add -
-mkdir -p ~/.ssh
-ssh-keyscan -H ${ROCKETCHIP_NIX_REMOTE_HOST} > ~/.ssh/known_hosts
-
 mkdir -p /etc/nix
+
 cat > /etc/nix/upload-to-cache.sh << EOF
 #!/bin/sh
 set -eu
@@ -15,14 +11,6 @@ set -f # disable globbing
 export IFS=' '
 
 echo "Post-build hook invoked at \$USER (\$(whoami))" | tee -a /tmp/nix-post-build-hook.log
-
-if ! command -v nix; then
-  echo "Nix installing. Exit" | tee -a /tmp/nix-post-build-hook.log
-  exit 0
-fi
-
-# export AWS_ACCESS_KEY_ID="${AWS_ID}"
-# export AWS_SECRET_ACCESS_KEY="${AWS_SECRET}"
 
 echo "Uploading paths" \$OUT_PATHS | tee -a /tmp/nix-post-build-hook.log
 nix copy --to 's3://nix?profile=nix-upload&scheme=https&endpoint=${CACHE_DOMAIN}&secret-key=/etc/nix/cache-key.pem' \$OUT_PATHS 2>&1 | tee -a /tmp/nix-post-build-hook.log
@@ -46,8 +34,28 @@ echo "$ROCKETCHIP_NIX_REMOTE_SSH_PRIVKEY" > /etc/nix/builder-key
 echo "Builder key file:"
 ls -ll /etc/nix/builder-key
 
+mkdir -p ~runner/.ssh
+cat > ~runner/.ssh/config << EOF
+StrictHostKeyChecking accept-new
+
+Host builder
+  HostName ${ROCKETCHIP_NIX_REMOTE_HOST}
+  IdentitiesOnly yes
+  IdentityFile /etc/nix/builder-key
+  User nix-remote
+EOF
+
+tee -a /etc/nix/nix.conf << EOF
+post-build-hook = /etc/nix/upload-to-cache.sh
+extra-trusted-public-keys = minio.inner.fi.c-3.moe:gDg5SOIH65O0tTV89dUawME5BTmduWWaA7as/cqvevM=
+extra-substituters = https://${CACHE_DOMAIN}/nix
+max-jobs = 0
+builders = ssh-ng://builder x86_64-linux - 32 1 big-parallel
+builders-use-substitutes = true
+EOF
+
 chown runner:runner /etc/nix/cache-key.pem
-chmod 600 /etc/nix/cache-key.pem
+chmod 400 /etc/nix/cache-key.pem
 chown runner:runner /etc/nix/builder-key
-chmod 600 /etc/nix/builder-key
+chmod 400 /etc/nix/builder-key
 chmod +x /etc/nix/upload-to-cache.sh
