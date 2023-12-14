ca7ef8f80709edb15c642c0549c249b8c97c9193
SharzyL <me@sharzy.in>
2023-12-14 11:31:02 +0800

    [build system] bump nixpkgs, use upstream llvm

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -7,31 +7,7 @@ final: prev:
   llvmForDev = final.llvmPackages_14;
 
   # llvmToolsForRV32Clang is used for compiling rv32-clang to kept compatibility with buddy-mlir
-  llvmToolsForRV32Clang = (final.llvmPackages_16.override {
-    gitRelease = {
-      version = "17.0.0";
-      rev = "8f966cedea594d9a91e585e88a80a42c04049e6c";
-      rev-version = "unstable-2023-05-02";
-      sha256 = "sha256-g2cYk3/iyUvmIG0QCQpYmWj4L2H4znx9KbuA5TvIjrc=";
-    };
-    officialRelease = null;
-    buildLlvmTools = final.buildPackages.llvmToolsForRV32Clang;
-  }).tools.extend (lfinal: lprev: {
-    libllvm = lprev.libllvm.overrideAttrs (oldAttrs: {
-      patches = (builtins.filter (p: builtins.baseNameOf p != "gnu-install-dirs.patch") oldAttrs.patches) ++ [
-        ./patches/llvm/gnu-install-dirs.patch
-      ];
-      # Disable llvm tests here, because it wastes too much time.
-      # But if someday someone bumps the new llvm, it is their responsibility to make sure
-      # that these tests can pass successfully.
-      doCheck = false;
-    });
-    libclang = lprev.libclang.overrideAttrs (oldAttrs: {
-      patches = oldAttrs.patches ++ [
-        ./patches/llvm/fix-clang-build.patch
-      ];
-    });
-  });
+  llvmToolsForRV32Clang = final.llvmPackages_17;
 
   espresso = final.callPackage ./pkgs/espresso.nix { };
   libspike = final.callPackage ./pkgs/libspike.nix { };
