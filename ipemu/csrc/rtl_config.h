c1244e6a9b9164c82e6f649e2826b10f57a79550
qinjun-li <liqinjun1994@gmail.com>
2024-01-12 15:45:30 +0800

    [cosim] read memoryBankSize from vParam.

--- ipemu/csrc/rtl_config.h
+++ ipemu/csrc/rtl_config.h
@@ -13,11 +13,11 @@ struct RTLConfig {
   size_t physical_address_width;
   size_t chaining_size;
   size_t vrf_write_queue_size;
+  size_t tl_bank_number;
 
   size_t elen = 32;
   size_t vreg_number = 32;
   size_t mshr_number = 3;
-  size_t tl_bank_number = 2;
 };
 
 extern size_t lsu_idx_default;
