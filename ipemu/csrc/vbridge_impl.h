4d59e7729195b832bc52e358c8861c99f3a5b0c0
Liu Xiaoyi <circuitcoder0@gmail.com>
2024-01-19 03:32:29 +0800

    [cosim] dramsim3 integration [skip ci]

--- ipemu/csrc/vbridge_impl.h
+++ ipemu/csrc/vbridge_impl.h
@@ -21,6 +21,10 @@
 #include <svdpi.h>
 #endif
 
+#ifndef COSIM_NO_DRAMSIM
+#include "dramsim3.h"
+#endif
+
 #include "encoding.h"
 #include "rtl_config.h"
 #include "simple_sim.h"
@@ -35,17 +39,88 @@ struct TLReqRecord {
   size_t size_by_byte;
   reg_t addr;
   uint16_t source;
+
+  bool muxin_read_required; // Read required for partial writes
+  bool muxin_read_sent = false; // Mux-in read consists of at most one transaction
+
+  // For writes, this is the number of bytes written by user.
+  // For reads, this is the number of bytes returned by memory controller
+  // For writes, as soon as the transaction is sent to the controller, the request is resolved, so we don't have to track the number
+  //   of bytes that have been processed by the memory controller
   size_t bytes_received = 0;
-  size_t bytes_sent = 0;
+
+  // This is the number of bytes(or worth-of transaction for reads) sent to the memory controller
+  size_t bytes_committed = 0;
+
+  // For read, number of bytes returned to user
+  size_t bytes_returned = 0;
 
   /// when opType set to nil, it means this record is already sent back
   enum class opType { Nil, Get, PutFullData } op;
-  int remaining_cycles;
 
   TLReqRecord(size_t t, std::vector<uint8_t> data, size_t size_by_byte,
-              reg_t addr, uint16_t source, opType op, int cycles)
+              reg_t addr, uint16_t source, opType op, reg_t burst_size)
       : t(t), data(std::move(data)), size_by_byte(size_by_byte), addr(addr),
-        source(source), op(op), remaining_cycles(cycles){};
+        source(source), op(op) {
+          muxin_read_required = op == opType::PutFullData && size_by_byte < burst_size;
+        };
+  
+  reg_t aligned_addr(reg_t burst_size) const {
+    return (addr / burst_size) * burst_size;
+  }
+
+  bool done_commit() const {
+    if(muxin_read_required) return false;
+    if(bytes_committed < size_by_byte) return false;
+    return true;
+  }
+
+  bool done_return() const {
+    if(muxin_read_required) return false;
+    if(op == opType::PutFullData) return bytes_returned > 0;
+    else return bytes_returned >= size_by_byte;
+  }
+
+  std::optional<std::pair<reg_t, bool>> issue_mem_request(reg_t burst_size) const {
+    if(muxin_read_required) {
+      if(muxin_read_sent) return {};
+      return {{ aligned_addr(burst_size), false }};
+    }
+
+    if(bytes_committed >= size_by_byte) return {};
+    if(op == opType::PutFullData) if(bytes_committed - bytes_received < std::min(burst_size, size_by_byte)) return {};
+
+    return {{ aligned_addr(burst_size) + bytes_committed, op == opType::PutFullData }};
+  }
+
+  void commit_mem_request(reg_t burst_bytes) {
+    if(muxin_read_required) muxin_read_sent = true;
+    else bytes_committed += burst_bytes;
+  }
+
+  void resolve_mem_response(reg_t addr, reg_t burst_bytes) {
+    // TODO: check is valid response
+    if(muxin_read_required) muxin_read_required = false; // Resolve mux-in read
+    else if(op != opType::PutFullData) bytes_received += burst_bytes;
+  }
+
+  // Returns: offset!
+  std::optional<std::pair<reg_t, size_t>> issue_tl_response(size_t tl_bytes) const {
+    if(op == opType::PutFullData) {
+      // Writes need to wait for commit finishes
+      if(!done_commit()) return {};
+      return {{ 0, 0 }}; // Write resolves all together
+    } else {
+      // Reads need to wait for available data
+      auto transfer_size = std::min(tl_bytes, size_by_byte);
+      if(bytes_returned - bytes_received < transfer_size) return {};
+      return {{ bytes_returned, transfer_size }};
+    }
+  }
+
+  void commit_tl_respones(reg_t tl_bytes) {
+    bytes_returned += tl_bytes;
+  }
 };
 
 class VBridgeImpl {
@@ -150,9 +225,15 @@ private:
 
 #endif
 
-  int get_mem_req_cycles() { return 1; };
+#ifndef COSIM_NO_DRAMSIM
+  void dramsim_drive(const int channel_id);
+  void dramsim_resolve(const int channel_id, const reg_t addr);
+  size_t dramsim_burst_size(const int channel_id) const;
+  std::vector<std::pair<dramsim3::MemorySystem, uint64_t>> drams; // (controller, dram tick)
+#endif
 
+  double tck;
   uint64_t last_commit_time = 0;
 };
 
-extern VBridgeImpl vbridge_impl_instance;
+  extern VBridgeImpl vbridge_impl_instance;
