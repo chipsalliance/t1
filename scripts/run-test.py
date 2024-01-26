19053b113b00ce26d2adb9703d5b4d1952b206bb
Avimitin <dev@avimit.in>
2024-01-26 22:01:06 +0800

    [script] increase cosim default timeout
    Signed-off-by: Avimitin <dev@avimit.in>

--- scripts/run-test.py
+++ scripts/run-test.py
@@ -17,11 +17,17 @@ logger.addHandler(ch)
 
 def main():
     parser = ArgumentParser()
-    subparsers = parser.add_subparsers(help="sub-commands help", dest='emu_type', required=True)
+    subparsers = parser.add_subparsers(
+        help="sub-commands help", dest="emu_type", required=True
+    )
 
     # Add sub-commands
-    verilator_args_parser = subparsers.add_parser("ip", help="ip emulator help")  # TODO: rename to ip
-    subsystem_args_parser = subparsers.add_parser("subsystem", help="subsystem emulator help")
+    verilator_args_parser = subparsers.add_parser(
+        "ip", help="ip emulator help"
+    )  # TODO: rename to ip
+    subsystem_args_parser = subparsers.add_parser(
+        "subsystem", help="subsystem emulator help"
+    )
 
     # Register common args
     for subparser in (verilator_args_parser, subsystem_args_parser):
@@ -41,14 +47,17 @@ def main():
             help="path to the subsystem emulator, use nix generated one if unspecified",
         )
         subparser.add_argument(
-            "--cases-dir", help="path to testcases, default to TEST_CASES_DIR environment"
+            "--cases-dir",
+            help="path to testcases, default to TEST_CASES_DIR environment",
         )
         subparser.add_argument(
-            "--use-individual-drv", help="use .#t1.cases.<case_type>.<case_name> instead of .#t1.cases.all",
+            "--use-individual-drv",
+            help="use .#t1.cases.<case_type>.<case_name> instead of .#t1.cases.all",
             action="store_true",
         )
         subparser.add_argument(
-            "--force-x86", help="use cases built in x86, for non-x86 machines not capable of building cases",
+            "--force-x86",
+            help="use cases built in x86, for non-x86 machines not capable of building cases",
             action="store_true",
         )
         subparser.add_argument(
@@ -79,7 +88,7 @@ def main():
         type=float,
     )
     verilator_args_parser.add_argument(
-        "--cosim-timeout", default=100000, help="set cosim timeout"
+        "--cosim-timeout", default=400000, help="set cosim timeout"
     )
     verilator_args_parser.add_argument(
         "--no-logging",
@@ -112,25 +121,41 @@ def main():
 
 
 # Try to search ELF from the given directory
-def load_elf_from_dir(cases_dir, case_name, use_individual_drv, force_x86):
+def load_elf_from_dir(config, cases_dir, case_name, use_individual_drv, force_x86):
     cases_attr_name = "cases-x86" if force_x86 else "cases"
     if cases_dir is None:
         if env_case_dir := os.environ.get("TEST_CASES_DIR"):
             cases_dir = env_case_dir
         else:
-            nix_args = [ "nix", "build", "--no-link", "--print-out-paths", "--no-warn-dirty" ]
+            nix_args = [
+                "nix",
+                "build",
+                "--no-link",
+                "--print-out-paths",
+                "--no-warn-dirty",
+            ]
             if use_individual_drv:
-                split_idx = case_name.rfind('-')
-                case_true_name, case_type = case_name[:split_idx].replace('.', '-'), case_name[split_idx+1:]
-                nix_args.append(f".#t1.{cases_attr_name}.{case_type}.{case_true_name}")
+                split_idx = case_name.rfind("-")
+                case_true_name, case_type = (
+                    case_name[:split_idx].replace(".", "-"),
+                    case_name[split_idx + 1 :],
+                )
+                nix_args.append(
+                    f".#t1.{config}.{cases_attr_name}.{case_type}.{case_true_name}"
+                )
             else:
-                nix_args.append(f".#t1.{cases_attr_name}.all")
+                nix_args.append(f".#t1.{config}.{cases_attr_name}.all")
             logger.info(f'Run "{" ".join(nix_args)}"')
             cases_dir = subprocess.check_output(nix_args).strip().decode("UTF-8")
 
     cases_dir = Path(cases_dir)
+    logger.info(f"Running cases in {cases_dir}")
 
-    case_config_path = cases_dir / f"{case_name}.json" if use_individual_drv else cases_dir / "configs" / f"{case_name}.json"
+    case_config_path = (
+        cases_dir / f"{case_name}.json"
+        if use_individual_drv
+        else cases_dir / "configs" / f"{case_name}.json"
+    )
     assert case_config_path.exists(), f"cannot find case config in {case_config_path}"
     config = json.loads(case_config_path.read_text())
 
@@ -159,67 +184,95 @@ def run_test(args):
     case_elf_path = (
         args.case
         if Path(args.case).exists()
-        else load_elf_from_dir(args.cases_dir, args.case, args.use_individual_drv, args.force_x86)
+        else load_elf_from_dir(
+            args.config,
+            args.cases_dir,
+            args.case,
+            args.use_individual_drv,
+            args.force_x86,
+        )
     )
 
     elaborate_config_path = Path(f"{args.out_dir}/config.json")
     if elaborate_config_path.exists():
         os.remove(elaborate_config_path)
     configgen_args = [
-        "nix", "run", "--no-warn-dirty", ".#t1.configgen",
-        "--", f"{args.config.replace('-', '')}", "-t", f"{args.out_dir}",
+        "nix",
+        "run",
+        "--no-warn-dirty",
+        ".#t1.configgen",
+        "--",
+        f"{args.config.replace('-', '')}",
+        "-t",
+        f"{args.out_dir}",
     ]
     logger.info(f'Run "{" ".join(configgen_args)}"')
     subprocess.Popen(configgen_args).wait()
-    assert elaborate_config_path.exists(), f"cannot find elaborate config in {elaborate_config_path}"
+    assert (
+        elaborate_config_path.exists()
+    ), f"cannot find elaborate config in {elaborate_config_path}"
 
-    target_name = (f"{emu_type}.emu-trace" if args.trace else f"{emu_type}.emu")
+    target_name = f"{emu_type}.emu-trace" if args.trace else f"{emu_type}.emu"
 
     emu_args = None
 
     def optionals(cond, items):
         return items if cond else []
 
-    if emu_type == 'ip':
+    if emu_type == "ip":
         dramsim3_cfg = args.dramsim3_cfg
         tck = 10**3 / args.frequency
-        emu_args = [
-            "--elf", str(case_elf_path),
-            "--wave", str(Path(args.out_dir) / "wave.fst"),
-            "--timeout", str(args.cosim_timeout),
-            "--config", str(elaborate_config_path),
-            "--tck", str(tck),
-            "--perf", str(Path(args.out_dir) / "perf.txt"),
-        ] + optionals(args.no_logging, [
-              "--no-logging"
-            ]) \
-          + optionals(args.no_file_logging,[
-              "--no-file-logging"
-            ]) \
-          + optionals(args.no_console_logging, [
-              "--no-console-logging"
-            ]) \
-          + optionals(args.out_dir, [
-              f"--log-path={str(Path(args.out_dir) / 'emulator.log')}"
-            ]) \
-          + optionals(dramsim3_cfg is not None, [
-              "--dramsim3-result", str(Path(args.out_dir) / "dramsim3-logs"),
-              "--dramsim3-config", dramsim3_cfg,
-            ])
+        emu_args = (
+            [
+                "--elf",
+                str(case_elf_path),
+                "--wave",
+                str(Path(args.out_dir) / "wave.fst"),
+                "--timeout",
+                str(args.cosim_timeout),
+                "--config",
+                str(elaborate_config_path),
+                "--tck",
+                str(tck),
+                "--perf",
+                str(Path(args.out_dir) / "perf.txt"),
+            ]
+            + optionals(args.no_logging, ["--no-logging"])
+            + optionals(args.no_file_logging, ["--no-file-logging"])
+            + optionals(args.no_console_logging, ["--no-console-logging"])
+            + optionals(
+                args.out_dir, [f"--log-path={str(Path(args.out_dir) / 'emulator.log')}"]
+            )
+            + optionals(
+                dramsim3_cfg is not None,
+                [
+                    "--dramsim3-result",
+                    str(Path(args.out_dir) / "dramsim3-logs"),
+                    "--dramsim3-config",
+                    dramsim3_cfg,
+                ],
+            )
+        )
 
-    elif emu_type == 'subsystem':
-        emu_args = [ f"+init_file={case_elf_path}" ]
+    elif emu_type == "subsystem":
+        emu_args = [f"+init_file={case_elf_path}"]
         if args.trace:
             trace_file_path = args.trace_output_file or f"{args.out_dir}/trace.fst"
             emu_args.append(f"+trace_file={trace_file_path}")
 
     else:
-        assert False, f'unknown emutype {emu_type}'
+        assert False, f"unknown emutype {emu_type}"
 
     process_args = (
         [args.emulator_path]
         if args.emulator_path
-        else ["nix", "run", "--no-warn-dirty", f".#t1.{args.config}.{target_name}", "--"]
+        else [
+            "nix",
+            "run",
+            "--no-warn-dirty",
+            f".#t1.{args.config}.{target_name}",
+            "--",
+        ]
     ) + emu_args
 
     logger.info(f'Run "{" ".join(process_args)}"')
