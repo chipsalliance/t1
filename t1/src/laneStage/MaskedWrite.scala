ae631cb5117bb8309959e60184fe8ccaa3e04f25
qinjun-li <liqinjun1994@gmail.com>
2024-01-29 15:04:44 +0800

    [rtl] raw forward in mask write unit.

--- t1/src/laneStage/MaskedWrite.scala
+++ t1/src/laneStage/MaskedWrite.scala
@@ -5,7 +5,7 @@ package org.chipsalliance.t1.rtl.lane
 
 import chisel3._
 import chisel3.util._
-import org.chipsalliance.t1.rtl.{LaneParameter, VRFReadRequest, VRFWriteRequest, indexToOH}
+import org.chipsalliance.t1.rtl.{LaneParameter, VRFReadRequest, VRFWriteRequest, ffo, indexToOH}
 
 class MaskedWrite(parameter: LaneParameter) extends Module {
   val vrfWriteBundle: VRFWriteRequest = new VRFWriteRequest(
@@ -24,18 +24,23 @@ class MaskedWrite(parameter: LaneParameter) extends Module {
    * 3 is for [[source1]] [[source2]] [[source3]]
    */
   val vrfReadResult: UInt = IO(Input(UInt(parameter.datapathWidth.W)))
+  // raw forward
+  val hitWrite: Bool = Wire(Bool())
 
   // 需要这个读端口完全ready
   val readBeforeWrite: Bool = enqueue.fire && !enqueue.bits.mask.andR
-  vrfReadRequest.valid := readBeforeWrite
+  vrfReadRequest.valid := readBeforeWrite && !hitWrite
   vrfReadRequest.bits.vs := enqueue.bits.vd
   vrfReadRequest.bits.readSource := 2.U
   vrfReadRequest.bits.offset := enqueue.bits.offset
   vrfReadRequest.bits.instructionIndex := enqueue.bits.instructionIndex
   // latch data
-  val readNext: Bool = RegNext(readBeforeWrite)
-  val dataReg: UInt = RegEnable(vrfReadResult, 0.U(parameter.datapathWidth.W), readNext)
-  val dataSelect: UInt = Mux(readNext, vrfReadResult, dataReg)
+  val readNext: Bool = RegNext(readBeforeWrite, false.B)
+  val dataFromWrite: Bool = RegNext(hitWrite, false.B)
+  val writeNext: UInt = RegNext(dequeue.bits.data, 0.U)
+  val readDataSelect: UInt = Mux(dataFromWrite, writeNext, vrfReadResult)
+  val dataReg: UInt = RegEnable(readDataSelect, 0.U(parameter.datapathWidth.W), readNext)
+  val latchDataSelect: UInt = Mux(readNext, readDataSelect, dataReg)
 
   val pipeValid: Bool = RegInit(false.B)
   val enqueuePipe: VRFWriteRequest = RegEnable(enqueue.bits, 0.U.asTypeOf(enqueue.bits), enqueue.fire)
@@ -44,7 +49,7 @@ class MaskedWrite(parameter: LaneParameter) extends Module {
   writeQueue.io.enq.valid := pipeValid
   writeQueue.io.enq.bits := enqueuePipe
   val maskFill: UInt = FillInterleaved(8, enqueuePipe.mask)
-  writeQueue.io.enq.bits.data := enqueuePipe.data & maskFill | (dataSelect & (~maskFill))
+  writeQueue.io.enq.bits.data := enqueuePipe.data & maskFill | (latchDataSelect & (~maskFill))
   maskedWrite1H :=
     Mux(writeQueue.io.deq.valid, indexToOH(writeQueue.io.deq.bits.instructionIndex, parameter.chainingSize), 0.U) |
       Mux(pipeValid, indexToOH(enqueuePipe.instructionIndex, parameter.chainingSize), 0.U)
@@ -52,4 +57,7 @@ class MaskedWrite(parameter: LaneParameter) extends Module {
   when(enqueue.fire ^ writeQueue.io.enq.fire) {
     pipeValid := enqueue.fire
   }
+  hitWrite := writeQueue.io.deq.valid &&
+    (writeQueue.io.deq.bits.vd === enqueue.bits.vd) &&
+    (writeQueue.io.deq.bits.offset === enqueue.bits.offset)
 }
