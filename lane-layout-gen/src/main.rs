13fab7a54b277af6af20ce9e7fb597ebcb713262
SharzyL <me@sharzy.in>
2024-02-03 12:09:51 +0800

    [floorplan] add arg --width-limit

--- lane-layout-gen/src/main.rs
+++ lane-layout-gen/src/main.rs
@@ -26,12 +26,16 @@ struct Args {
     #[arg(long)]
     /// if set, will not stop until find a d smaller or equal to that
     search_until_d: Option<i64>,
+
+    #[arg(long)]
+    /// the maximum layout width
+    width_limit: Option<u64>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let mut searcher = LayoutSearcher::new(args.n);
+    let mut searcher = LayoutSearcher::new(args.n, args.width_limit);
     searcher.search(args.iter, args.seed, args.search_until_d);
 
     let layout = searcher.best_layout.unwrap();
@@ -43,7 +47,7 @@ fn main() {
         (k, (v.0 - min_pos.0, v.1 - min_pos.1))
     }).collect();
 
-    eprintln!("find a layout with the max distance: {}", searcher.best_d);
+    eprintln!("found a layout with the max distance: {}", searcher.best_d);
     if args.print_json {
         println!("{}", serde_json::to_string_pretty(&transformed_layout).unwrap());
     } else {
@@ -70,10 +74,13 @@ struct LayoutSearcher {
     best_layout: Option<HashMap<usize, Pos>>,
 
     nodes: Vec<LaneNode>,
+
+    y_limit_min: i64,
+    y_limit_max: i64,
 }
 
 impl LayoutSearcher {
-    fn new(n: u64) -> Self {
+    fn new(n: u64, width_limit: Option<u64>) -> Self {
         let mut nodes: Vec<LaneNode> = Vec::new();
         nodes.reserve(n as usize);
         for idx in 0..n {
@@ -87,7 +94,14 @@ impl LayoutSearcher {
             })
         }
 
-        Self{n, best_d: i64::MAX, best_layout: None, nodes}
+        assert!(width_limit.map_or(true, |x| x > 0u64), "error: width_limit should be non-zero if specified");
+        let y_limit_min = width_limit.map_or(i64::MIN, |l| -(l as i64 / 2));
+        let y_limit_max = width_limit.map_or(i64::MAX, |l| (l as i64 - 1) / 2);
+        assert!(width_limit.is_none() || y_limit_max - y_limit_min + 1 == width_limit.unwrap() as i64);
+        Self{
+            n, best_d: i64::MAX, best_layout: None, nodes,
+            y_limit_min, y_limit_max,
+        }
     }
 
     fn search_once(&self, seed: u64) -> (i64, HashMap<usize, Pos>) {
@@ -120,7 +134,11 @@ impl LayoutSearcher {
                     let span_range_size = (xmax - xmin + 1) * (ymax - ymin + 1);
                     let enlarge = (count > (0.9 * span_range_size as f64).floor() as usize) as i64;  // enlarge by 1 if too full
                     let x_range = Uniform::from(xmin - enlarge..xmax + enlarge + 1);
-                    let y_range = Uniform::from(ymin - enlarge..ymax + enlarge + 1);
+
+                    // handle y limit
+                    let y_max_limited = min(self.y_limit_max, ymax + enlarge);
+                    let y_min_limited = max(self.y_limit_min, ymin - enlarge);
+                    let y_range = Uniform::from(y_min_limited..y_max_limited + 1);
                     loop {
                         let x = rng.sample(x_range);
                         let y = rng.sample(y_range);
@@ -133,7 +151,10 @@ impl LayoutSearcher {
                     let mut best_max_dist = i64::MAX;
                     let mut best_pos: Option<Pos> = None;
                     for x in xmin-1..xmax+2 {
-                        for y in ymin-1..ymax+2 {
+                        // handle y limit
+                        let y_max_limited = min(self.y_limit_max, ymax + 1);
+                        let y_min_limited = max(self.y_limit_min, ymin - 1);
+                        for y in y_min_limited..y_max_limited+1 {
                             let candidate_pos = (x, y);
                             if !occupied.contains_key(&candidate_pos) {
                                 let max_dist_to_neighbors: i64 = placed_neighbors_pos.iter()
