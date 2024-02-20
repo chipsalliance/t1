20d461c104ff72dae0875f243d284f5f24885ed7
Yanqi Yang <shxxyyq0000@qq.com>
2024-02-20 16:34:56 +0800

    [rtl] optimize fmin/fmax logic

--- t1/src/LaneFloat.scala
+++ t1/src/LaneFloat.scala
@@ -164,8 +164,8 @@ class LaneFloat(val parameter: LaneFloatParam) extends VFUModule(parameter) with
   val differentZeros = compareModule.io.eq && (request.src(1)(31) ^ request.src(0)(31))
 
   assert(!unitSeleOH(2) || (uop === "b0001".U || uop === "b0000".U || uop === "b0010".U || uop === "b0011".U || uop === "b0100".U || uop === "b0101".U || uop === "b1000".U || uop === "b1100".U))
-  compareResult := Mux(uop === "b1000".U , Mux(hasNaN, compareNaN, Mux(compareModule.io.lt, request.src(1), Mux(differentZeros, "h80000000".U,request.src(0)))),
-    Mux(uop === "b1100".U, Mux(hasNaN, compareNaN, Mux(compareModule.io.gt, request.src(1), Mux(differentZeros, 0.U(32.W), request.src(0)))),
+  compareResult := Mux(uop === BitPat("b1?00") && hasNaN ,compareNaN,
+    Mux(uop === BitPat("b1?00"), Mux((!uop(2) && compareModule.io.lt) || (uop(2) && compareModule.io.gt) || (differentZeros && (uop(2) ^ request.src(1)(31))), request.src(1), request.src(0)),
      Mux(uop === "b0011".U, compareModule.io.lt || compareModule.io.eq,
        Mux(uop === "b0101".U, compareModule.io.gt || compareModule.io.eq,
          Mux(uop === "b0010".U, compareModule.io.lt,
