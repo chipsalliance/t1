1eac3fbc27072cce1e7f374d2df1cef8ab068a7e
Jiuyang Liu <liu@jiuyang.me>
2024-03-16 23:07:33 +0800

    [rtl] fix subsystem rtl, for rocket-chip/t1-dev

--- subsystem/src/Subsystem.scala
+++ subsystem/src/Subsystem.scala
@@ -5,19 +5,17 @@ package org.chipsalliance.t1.subsystem
 
 import chisel3._
 import chisel3.experimental.{SerializableModuleGenerator, UnlocatableSourceInfo}
-import chisel3.util.BitPat
 import chisel3.util.experimental.BitSet
-import freechips.rocketchip.amba.axi4.{AXI4IdIndexer, AXI4SlaveNode, AXI4SlaveParameters, AXI4SlavePortParameters, AXI4UserYanker, AXI4Xbar}
-import freechips.rocketchip.devices.debug.{DebugModuleKey, TLDebugModule}
+import chisel3.util.{BitPat, Counter}
+import freechips.rocketchip.devices.debug.DebugModuleKey
 import freechips.rocketchip.devices.tilelink._
 import freechips.rocketchip.diplomacy._
-import freechips.rocketchip.prci.{ClockBundle, ClockDomain, ClockGroup, ClockGroupAggregator, ClockGroupSourceNode, ClockGroupSourceParameters, ClockSourceNode, ClockSourceParameters, FixedClockBroadcast, FixedClockBroadcastNode, NoResetCrossing, SimpleClockGroupSource}
+import freechips.rocketchip.prci.{ClockSourceNode, ClockSourceParameters, FixedClockBroadcast, NoResetCrossing}
 import freechips.rocketchip.rocket._
-import freechips.rocketchip.subsystem.CoherenceManagerWrapper.CoherenceManagerInstantiationFn
 import freechips.rocketchip.subsystem._
 import freechips.rocketchip.tile.{MaxHartIdBits, XLen}
-import freechips.rocketchip.tilelink.{HasTLBusParams, RegionReplicator, ReplicatedRegion, TLBroadcast, TLBusWrapper, TLBusWrapperConnection, TLBusWrapperInstantiationLike, TLBusWrapperTopology, TLCacheCork, TLClientNode, TLEdge, TLFilter, TLInwardNode, TLManagerNode, TLMasterParameters, TLMasterPortParameters, TLOutwardNode, TLSlaveParameters, TLSlavePortParameters, TLSourceShrinker, TLToAXI4, TLWidthWidget, TLXbar}
-import freechips.rocketchip.util.{DontTouch, Location, RecordMap}
+import freechips.rocketchip.tilelink.{BroadcastFilter, HasTLBusParams, TLBusWrapper, TLBusWrapperConnection, TLBusWrapperInstantiationLike, TLBusWrapperTopology, TLEdge, TLFIFOFixer, TLFragmenter, TLInwardNode, TLManagerNode, TLOutwardNode, TLSlaveParameters, TLSlavePortParameters, TLWidthWidget, TLXbar}
+import freechips.rocketchip.util.Location
 import org.chipsalliance.cde.config._
 import org.chipsalliance.t1.rocketcore.{T1CrossingParams, T1TileAttachParams, T1TileParams}
 import org.chipsalliance.t1.rockettile.BuildT1
@@ -31,7 +29,7 @@ case class ScalarMasterBusParameters(beatBytes: Int, blockBytes: Int)
     with TLBusWrapperInstantiationLike {
   def dtsFrequency: Option[BigInt] = None
   override def instantiate(
-    context: HasTileLinkLocations,
+    context: HasTileLinkLocations with HasPRCILocations with LazyModule,
     loc:     Location[TLBusWrapper]
   )(
     implicit p: Parameters
@@ -55,16 +53,18 @@ case class ScalarControlBusParameters(beatBytes: Int, blockBytes: Int)
     with TLBusWrapperInstantiationLike {
   def dtsFrequency: Option[BigInt] = None
   def instantiate(
-    context: HasTileLinkLocations,
+    context: HasTileLinkLocations with HasPRCILocations with LazyModule,
     loc:     Location[TLBusWrapper]
   )(
     implicit p: Parameters
   ): TLBusWrapper = {
     val scalarControl = LazyModule(new TLBusWrapper(this, "ScalarControl") {
-      private val xbar = LazyModule(new TLXbar())
-      val inwardNode:  TLInwardNode = xbar.node
-      val outwardNode: TLOutwardNode = xbar.node
-      def busView:     TLEdge = xbar.node.edges.in.head
+      private val xbar = TLXbar()
+      private val fixer = LazyModule(new TLFIFOFixer(TLFIFOFixer.all)).node
+      val node = xbar :*= fixer
+      val inwardNode:  TLInwardNode = node
+      val outwardNode: TLOutwardNode = node
+      def busView:     TLEdge = fixer.edges.in.head
 
       override def prefixNode:     Option[BundleBridgeNode[UInt]] = None
       override def builtInDevices: BuiltInDevices = BuiltInDevices.none
@@ -79,7 +79,7 @@ case class VectorMasterBusParameters(beatBytes: Int, blockBytes: Int)
     with TLBusWrapperInstantiationLike {
   def dtsFrequency: Option[BigInt] = None
   def instantiate(
-    context: HasTileLinkLocations,
+    context: HasTileLinkLocations with HasPRCILocations with LazyModule,
     loc:     Location[TLBusWrapper]
   )(
     implicit p: Parameters
@@ -111,9 +111,7 @@ class T1SubsystemConfig(t1Generator: SerializableModuleGenerator[T1, T1Parameter
         case DebugModuleKey                             => None
         case HasTilesExternalResetVectorKey             => true
         case TLManagerViewpointLocated(T1Subsystem)     => ScalarMasterBus
-        // Don't Drive clock implicitly from IO, we create clock sources and attach our own clock node:
-        // This will pave the road to Chisel Clock Domain API
-        case SubsystemDriveClockGroupsFromIO => false
+        case BroadcastKey                               => BroadcastParams(nTrackers = 4, bufferless = true, controlAddress = None, filterFactory = BroadcastFilter.factory)
         case TilesLocated(T1Subsystem)       =>
           // Attach one core and
           List(
@@ -135,7 +133,7 @@ class T1SubsystemConfig(t1Generator: SerializableModuleGenerator[T1, T1Parameter
                   DCacheParams(
                     // TODO: align with ScalarMasterBus.beatBits
                     rowBits = 64,
-                    nSets = 256,
+                    nSets = 64,
                     nWays = 1,
                     nTLBSets = 1,
                     nTLBWays = 4,
@@ -179,29 +177,54 @@ class T1SubsystemConfig(t1Generator: SerializableModuleGenerator[T1, T1Parameter
               )
             )
           )
+          // at 12M
+        case PLICKey       => Some(PLICParams(baseAddress = 0xc00000, maxPriorities = 3, intStages = 0, maxHarts = 1))
+        case PLICAttachKey => PLICAttachParams(slaveWhere = ScalarControlBus)
+        case CLINTKey =>
+          // at 8M
+          Some(
+            CLINTParams(
+              baseAddress = 0x800000,
+              intStages = 0
+            )
+          )
+        case CLINTAttachKey => CLINTAttachParams(slaveWhere = ScalarControlBus)
         case TLNetworkTopologyLocated(T1Subsystem) =>
           List(
             new TLBusWrapperTopology(
               instantiations = List(
-                (ScalarMasterBus, ScalarMasterBusParameters(beatBytes = 8, blockBytes = 64)),
-                (ScalarControlBus, ScalarControlBusParameters(beatBytes = 8, blockBytes = 64)),
+                (ScalarMasterBus, ScalarMasterBusParameters(beatBytes = 8, blockBytes = 32)),
+                (ScalarControlBus, ScalarControlBusParameters(beatBytes = 8, blockBytes = 32)),
                 // beatBytes, blockBytes should be constraint to DLEN/VLEN:
                 // beatBytes = DLEN, blockBytes = DLEN
-                (VectorMasterBus, VectorMasterBusParameters(beatBytes = t1Generator.parameter.datapathWidth / 8, blockBytes = t1Generator.parameter.dLen / 8)),
-                (COH, CoherenceManagerWrapperParams(blockBytes = 64, beatBytes = 8, 1, COH.name)(CoherenceManagerWrapper.broadcastManagerFn("broadcast", T1Subsystem, ScalarControlBus)))
+                (
+                  VectorMasterBus,
+                  VectorMasterBusParameters(
+                    beatBytes = t1Generator.parameter.datapathWidth / 8,
+                    blockBytes = t1Generator.parameter.dLen / 8
+                  )
+                ),
+                (
+                  COH,
+                  CoherenceManagerWrapperParams(blockBytes = site(CacheBlockBytes), beatBytes = 8, 1, COH.name)(
+                    CoherenceManagerWrapper.broadcastManagerFn("broadcast", T1Subsystem, ScalarControlBus)
+                  )
+                )
               ),
               connections = Seq(
                 (
-                  ScalarMasterBus, ScalarControlBus,
+                  ScalarMasterBus,
+                  ScalarControlBus,
                   TLBusWrapperConnection.crossTo(
                     xType = NoCrossing,
                     driveClockFromMaster = Some(true),
                     nodeBinding = BIND_STAR,
                     flipRendering = false
-                  ),
+                  )
                 ),
                 (
-                  ScalarMasterBus, COH,
+                  ScalarMasterBus,
+                  COH,
                   TLBusWrapperConnection.crossTo(
                     xType = NoCrossing,
                     driveClockFromMaster = Some(true),
@@ -214,7 +237,7 @@ class T1SubsystemConfig(t1Generator: SerializableModuleGenerator[T1, T1Parameter
           )
       })
         .orElse(new WithClockGateModel("./dependencies/rocket-chip/src/vsrc/EICG_wrapper.v"))
-        .orElse(new WithCacheBlockBytes(16))
+        .orElse(new WithCacheBlockBytes(32))
         // SoC
         .orElse(new WithoutTLMonitors)
         // 1 MHz
@@ -226,8 +249,6 @@ class T1Subsystem(implicit p: Parameters)
     extends BareSubsystem
     // TODO: Remove [[HasDTS]] in the following PRs
     with HasDTS
-    // implement ibus, clock domains
-    with HasConfigurablePRCILocations
     // Provides [[locateTLBusWrapper]] and [[HasPRCILocations]]
     with Attachable
     // use [[TLNetworkTopologyLocated]] for configuration bus
@@ -242,87 +263,180 @@ class T1Subsystem(implicit p: Parameters)
     with HasHierarchicalElementsRootContext
     // Attach Tile to clockdomains
     with HasHierarchicalElements {
+  lazy val ibus = LazyModule(new InterruptBusWrapper)
   lazy val module = new T1SubsystemModuleImp(this)
   val t1Parameter: T1Parameter = p(org.chipsalliance.t1.subsystem.T1Generator).parameter
 
   override lazy val location:       HierarchicalLocation = T1Subsystem
   override lazy val busContextName: String = "t1subsystem"
 
+  // 512M-1G
+  val scalarMemoryRanges = BitSet(BitPat("b001?????????????????????????????"))
+  require(
+    t1Parameter.lsuParameters.banks.filter(_.accessScalar).map(_.region).reduce(_ union _).cover(scalarMemoryRanges),
+    "Vector accessing scalar should include scalarMemory"
+  )
+
+  // 256M-512M
+  val mmioRanges = BitSet(BitPat("b0001????????????????????????????"))
+
   // ClockDomains:
-  val clockSource = ClockGroupSourceNode(Seq(ClockGroupSourceParameters()))
-  // Expose clock to IO.
-  allClockGroupsNode := ClockGroupAggregator() := clockSource
+  val clockSource = ClockSourceNode(Seq(ClockSourceParameters()))
 
-  // First clock is ScalarMaster Clock, it will drive ScalarControlClock.
-  viewpointBus.clockGroupNode := allClockGroupsNode
-  // TODO: should it located at control bus?
-  ibus.clockNode := viewpointBus.fixedClockNode
-  // Second is VectorMasterBus clock, it will drive T1.
-  tlBusWrapperLocationMap(VectorMasterBus).clockGroupNode := viewpointBus.clockGroupNode
+  val clockBroadcaster = FixedClockBroadcast() := clockSource
 
-  lazy val clintOpt = None
-  lazy val clintDomainOpt = None
-  lazy val plicOpt = None
-  lazy val plicDomainOpt = None
+  // Three clocks need to special take care about: Interrupt, ScalarBus, VectorBus.
+  ibus.clockNode :*= clockBroadcaster
+  tlBusWrapperLocationMap(ScalarMasterBus).clockNode :*= clockBroadcaster
+  tlBusWrapperLocationMap(VectorMasterBus).clockNode :*= clockBroadcaster
+
   lazy val debugOpt = None
 
-  private def bitsetToAddressSet(bitset: BitSet): Seq[AddressSet] = bitset.terms.map((bp: BitPat) => AddressSet(bp.value, bp.mask ^ ((1 << bp.width) - 1))).toSeq.sorted
-  val vectorMemoryNodes = t1Parameter.lsuParameters.banks.map(bank => TLManagerNode(Seq(TLSlavePortParameters.v1(
-    Seq(TLSlaveParameters.v1(
-      address            = bank.region.terms.map(bitsetToAddressSet).toSeq.flatten,
-      resources          = Nil,
-      regionType         = RegionType.UNCACHED,
-      executable         = true,
-      supportsGet        = TransferSizes(1, tlBusWrapperLocationMap(VectorMasterBus).blockBytes),
-      supportsPutPartial = TransferSizes(1, tlBusWrapperLocationMap(VectorMasterBus).blockBytes),
-      supportsPutFull    = TransferSizes(1, tlBusWrapperLocationMap(VectorMasterBus).blockBytes),
-      supportsArithmetic = TransferSizes.none,
-      supportsLogical    = TransferSizes.none,
-      fifoId             = Some(0))), // requests are handled in order
-    // align with datapath size
-    beatBytes  = tlBusWrapperLocationMap(ScalarMasterBus).beatBytes,
-    minLatency = 1
-  ))))
+  private def bitsetToAddressSet(bitset: BitSet): Seq[AddressSet] =
+    bitset.terms.map((bp: BitPat) => AddressSet(bp.value, bp.mask ^ ((BigInt(1) << bp.width) - 1))).toSeq.sorted
+  private def addressSetToBitSet(addresssSet: AddressSet, width: Int): BitSet = BitPat(
+    "b" + Seq
+      .tabulate(width)(idx =>
+        if (addresssSet.mask.testBit(idx)) "?" else if (addresssSet.base.testBit(idx)) "1" else "0"
+      )
+      .mkString("")
+  )
 
-  vectorMemoryNodes.foreach(vectorMemoryNode => tlBusWrapperLocationMap(VectorMasterBus).coupleTo("hbmVectorPort")(vectorMemoryNode := _))
+  val vectorMemoryNodes = t1Parameter.lsuParameters.banks.filter(!_.accessScalar).map(bank =>
+    TLManagerNode(
+      Seq(
+        TLSlavePortParameters.v1(
+          Seq(
+            TLSlaveParameters.v1(
+              address = bank.region.terms.map(bitsetToAddressSet).toSeq.flatten,
+              resources = Nil,
+              regionType = RegionType.UNCACHED,
+              executable = true,
+              supportsGet = TransferSizes(1, tlBusWrapperLocationMap(VectorMasterBus).blockBytes),
+              supportsPutPartial = TransferSizes(1, tlBusWrapperLocationMap(VectorMasterBus).blockBytes),
+              supportsPutFull = TransferSizes(1, tlBusWrapperLocationMap(VectorMasterBus).blockBytes),
+              supportsArithmetic = TransferSizes.none,
+              supportsLogical = TransferSizes.none,
+              fifoId = Some(0)
+            )
+          ), // requests are handled in order
+          // align with datapath size
+          beatBytes = tlBusWrapperLocationMap(ScalarMasterBus).beatBytes,
+          minLatency = 1
+        )
+      )
+    )
+  )
 
-  val scalarMemoryNode = TLManagerNode(Seq(TLSlavePortParameters.v1(
-    Seq(TLSlaveParameters.v1(
-      address            = bitsetToAddressSet(t1Parameter.lsuParameters.banks.map(_.region).reduce {(l: BitSet, r: BitSet) => l.union(r)}),
-      resources          = Nil,
-      regionType         = RegionType.UNCACHED,
-      executable         = true,
-      supportsGet        = TransferSizes(1, tlBusWrapperLocationMap(ScalarMasterBus).blockBytes),
-      supportsPutPartial = TransferSizes(1, tlBusWrapperLocationMap(ScalarMasterBus).blockBytes),
-      supportsPutFull    = TransferSizes(1, tlBusWrapperLocationMap(ScalarMasterBus).blockBytes),
-      supportsArithmetic = TransferSizes.none,
-      supportsLogical    = TransferSizes.none,
-      fifoId             = Some(0))), // requests are handled in order
-    // align with datapath size
-    beatBytes  = tlBusWrapperLocationMap(ScalarMasterBus).beatBytes,
-    minLatency = 1
-  )))
+  vectorMemoryNodes.foreach(vectorMemoryNode =>
+    tlBusWrapperLocationMap(VectorMasterBus).coupleTo("VectorPort")(vectorMemoryNode := _)
+  )
 
-  tlBusWrapperLocationMap(COH).coupleTo("ScalarTileLinkPort")(
-    scalarMemoryNode
-      := TLWidthWidget(tlBusWrapperLocationMap(COH).beatBytes)
+  // T1 is an accelerator. it only have one scalar outwards bank
+  val scalarMemoryNode = TLManagerNode(
+    Seq(
+      TLSlavePortParameters.v1(
+        Seq(
+          TLSlaveParameters.v1(
+            // all vector bank that supports MMU should observe the scalar banks
+            address = bitsetToAddressSet(scalarMemoryRanges),
+            resources = Nil,
+            regionType = RegionType.UNCACHED,
+            executable = true,
+            supportsGet = TransferSizes(1, tlBusWrapperLocationMap(ScalarMasterBus).blockBytes),
+            supportsPutPartial = TransferSizes(1, tlBusWrapperLocationMap(ScalarMasterBus).blockBytes),
+            supportsPutFull = TransferSizes(1, tlBusWrapperLocationMap(ScalarMasterBus).blockBytes),
+            supportsArithmetic = TransferSizes.none,
+            supportsLogical = TransferSizes.none,
+            fifoId = Some(0)
+          )
+        ), // requests are handled in order
+        // align with datapath size
+        beatBytes = tlBusWrapperLocationMap(ScalarMasterBus).beatBytes,
+        minLatency = 1
+      )
+    )
+  )
+
+  val scalarMemoryXBar = TLXbar()
+
+  scalarMemoryNode :=* scalarMemoryXBar
+
+  tlBusWrapperLocationMap(VectorMasterBus).coupleTo("VectorToScalarPort")(
+    scalarMemoryXBar
+      :=* TLWidthWidget(tlBusWrapperLocationMap(COH).beatBytes)
       := _
   )
 
-  val scalarPort = InModuleBody { scalarMemoryNode.makeIOs() }
-  val vectorPorts = InModuleBody { vectorMemoryNodes.zipWithIndex.map{case (n, i ) => n.makeIOs()(ValName(s"vectorChannel$i")) } }
-  val clocks = InModuleBody {
-    // Only take the first clock for now, I'll purge the clock group.
-    val elements: Seq[(String, ClockBundle)] = clockSource.out.flatMap(_._1.member.elements).take(1)
-    val io = IO(Flipped(RecordMap(elements.map { case (name, data) => name -> data.cloneType }: _*)))
-    elements.foreach { case (name, data) => io(name).foreach { data := _ } }
-    io
+  tlBusWrapperLocationMap(COH).coupleTo("ScalarPort")(
+    scalarMemoryXBar
+      :=* TLWidthWidget(tlBusWrapperLocationMap(COH).beatBytes)
+      := _
+  )
+
+  val mmioNode = TLManagerNode(
+    Seq(
+      TLSlavePortParameters.v1(
+        Seq(
+          TLSlaveParameters.v1(bitsetToAddressSet(mmioRanges),
+            resources = Nil,
+            regionType = RegionType.GET_EFFECTS,
+            executable = false,
+            supportsGet = TransferSizes(1, viewpointBus.blockBytes),
+            supportsPutFull = TransferSizes(1, viewpointBus.blockBytes),
+            supportsPutPartial = TransferSizes(1, viewpointBus.blockBytes),
+            fifoId = Some(0)
+          )
+        ),
+        beatBytes = tlBusWrapperLocationMap(ScalarMasterBus).beatBytes,
+        minLatency = 1
+      )
+    )
+  )
+
+  tlBusWrapperLocationMap(ScalarControlBus).coupleTo(s"ScalarMMIOPort") {
+    mmioNode := _
   }
+
+  // MMIOs
+  lazy val (plicOpt, plicDomainOpt) = p(PLICKey).map { param =>
+    val tlbus = locateTLBusWrapper(p(PLICAttachKey).slaveWhere)
+    val plicDomainWrapper = tlbus.generateSynchronousDomain.suggestName("clint_domain")
+    val plic = plicDomainWrapper { LazyModule(new TLPLIC(param, tlbus.beatBytes)) }
+    plicDomainWrapper { plic.node := tlbus.coupleTo("plic") { TLFragmenter(tlbus) := _ } }
+    plicDomainWrapper { plic.intnode :=* ibus.toPLIC }
+    (plic, plicDomainWrapper)
+  }.unzip
+
+  lazy val (clintOpt, clintDomainOpt) = p(CLINTKey).map { params =>
+    val tlbus = locateTLBusWrapper(p(CLINTAttachKey).slaveWhere)
+    val clintDomainWrapper = tlbus.generateSynchronousDomain.suggestName("clint_domain")
+    val clint = clintDomainWrapper { LazyModule(new CLINT(params, tlbus.beatBytes)) }
+    clintDomainWrapper {
+      clint.node := tlbus.coupleTo("clint") { TLFragmenter(tlbus) := _ }
+      InModuleBody {
+        // TODO: make tick configurable
+        clint.module.io.rtcTick := Counter(true.B, 10000)._2
+      }
+    }
+    (clint, clintDomainWrapper)
+  }.unzip
+
+  // IOs
+  val scalarPort = InModuleBody { scalarMemoryNode.makeIOs() }
+  val mmioPort = InModuleBody { mmioNode.makeIOs() }
+  val vectorPorts = InModuleBody {
+    vectorMemoryNodes.zipWithIndex.map { case (n, i) => n.makeIOs()(ValName(s"vectorChannel$i")) }
+  }
+  val clock = InModuleBody{ clockSource.makeIOs() }
 }
 
 class T1SubsystemModuleImp[+L <: T1Subsystem](_outer: L)
     extends BareSubsystemModuleImp(_outer)
     with HasHierarchicalElementsRootContextModuleImp {
+  childClock := outer.clock.head.clock
+  childReset := outer.clock.head.reset.asBool
+
   lazy val outer = _outer
   // IntSyncCrossingSource requires implcit clock
   override def provideImplicitClockToLazyChildren: Boolean = true
