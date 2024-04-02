81c495f5804c386282b69c21d6b730298b194b26
Avimitin <dev@avimit.in>
2024-04-03 02:30:09 +0800

    [build system] remove default t1package.jar target
    Use t1package.assembly to get correct dependencies bundled fat jar
    package.
    
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/t1/t1.nix
+++ nix/t1/t1.nix
@@ -77,7 +77,6 @@ let
       mill -i '__.assembly'
 
       mill -i t1package.sourceJar
-      mill -i t1package.jar
     '';
 
     installPhase = ''
@@ -85,13 +84,15 @@ let
 
       strip-nondeterminism out/elaborator/assembly.dest/out.jar
       strip-nondeterminism out/configgen/assembly.dest/out.jar
+      strip-nondeterminism out/t1package/assembly.dest/out.jar
+      strip-nondeterminism out/t1package/sourceJar.dest/out.jar
 
       mv out/configgen/assembly.dest/out.jar $out/share/java/configgen.jar
       mv out/elaborator/assembly.dest/out.jar $out/share/java/elaborator.jar
 
       mkdir -p $t1package/share/java
       mv out/t1package/sourceJar.dest/out.jar $t1package/share/java/t1package-sources.jar
-      mv out/t1package/jar.dest/out.jar $t1package/share/java/t1package.jar
+      mv out/t1package/assembly.dest/out.jar $t1package/share/java/t1package.jar
 
       mkdir -p $configgen/bin $elaborator/bin
       makeWrapper ${jdk21}/bin/java $configgen/bin/configgen --add-flags "-jar $out/share/java/configgen.jar"
