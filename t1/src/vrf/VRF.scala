4a5e4b563203eba628a1d05f632ba2fbaaff54cd
qinjun-li <liqinjun1994@gmail.com>
2024-05-09 15:02:29 +0800

    [rtl] Handle chaining check with different relative positions of vd and vsx.

--- t1/src/vrf/VRF.scala
+++ t1/src/vrf/VRF.scala
@@ -528,10 +528,34 @@ class VRF(val parameter: VRFParam) extends Module with SerializableModule[VRFPar
       val sameInst = check.instructionIndex === record.bits.instIndex
 
       // this element in record not execute
-      val notHitMask = (checkOH & record.bits.elementMask) === 0.U
-      val waw: Bool = record.bits.vd.valid && check.vd(4, 3) === record.bits.vd.bits(4, 3) && notHitMask
-      val war1: Bool = record.bits.vs1.valid && check.vd(4, 3) === record.bits.vs1.bits(4, 3) && notHitMask
-      val war2: Bool = check.vd(4, 3) === record.bits.vs2(4, 3) && notHitMask
+      val notHitMaskVd = (checkOH & record.bits.elementMask) === 0.U
+      val waw: Bool = record.bits.vd.valid && check.vd(4, 3) === record.bits.vd.bits(4, 3) && notHitMaskVd
+      // inst eg: vadd v0, v1, v1 (lmul = 1)
+      // We only recorded vd-related masks.
+      // 0 base: 11111111111111xx eg vs = 0 off=2
+      // As above, using vd as the perspective,
+      // we will access the lowest two elements of the register group where vd is located.
+      // But from the perspective of vs1:
+      // 1 base: 111111111111xx11 eg vs = 1 off=2
+      // Apparently. Our mask has shifted
+      // 0 base => 1 base << (1 * off)
+      // we need vd%8 base => vs1%8 base => vd base mask << (vs1 - vd) * off
+      // => vd base mask >> 8 * off << (8 + vs1 - vd) * off
+      // => vd base mask << (8 + vs1 - vd) * off >> 8 * off
+      val vs1Mask = (((-1.S(parameter.elementSize.W)).asUInt ## record.bits.elementMask) <<
+        ((8.U + record.bits.vs1.bits(2, 0) - record.bits.vd.bits(2, 0)) << parameter.vrfOffsetBits).asUInt).asUInt(
+        2 * 8 * parameter.singleGroupSize - 1,
+        8 * parameter.singleGroupSize
+      )
+      val notHitVs1 = (checkOH & vs1Mask) === 0.U
+      val war1: Bool = record.bits.vs1.valid && check.vd(4, 3) === record.bits.vs1.bits(4, 3) && notHitVs1
+      val vs2Mask = (((-1.S(parameter.elementSize.W)).asUInt ## record.bits.elementMask) <<
+        ((8.U + record.bits.vs2(2, 0) - record.bits.vd.bits(2, 0)) << parameter.vrfOffsetBits).asUInt).asUInt(
+        2 * 8 * parameter.singleGroupSize - 1,
+        8 * parameter.singleGroupSize
+      )
+      val notHitVs2 = (checkOH & vs2Mask) === 0.U
+      val war2: Bool = check.vd(4, 3) === record.bits.vs2(4, 3) && notHitVs2
       !((!older && (waw || war1 || war2)) && !sameInst && record.valid)
     }.reduce(_ && _)
   }
