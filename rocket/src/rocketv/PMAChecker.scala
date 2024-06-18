5330d392be97897f346eccf5a1b1d448cc9c56a7
Jiuyang Liu <liu@jiuyang.me>
2024-06-18 12:13:32 +0800

    [rocket] migrate PMAChecker

--- rocket/src/rocketv/PMAChecker.scala
+++ rocket/src/rocketv/PMAChecker.scala
@@ -10,10 +10,36 @@ import chisel3.experimental.hierarchy.Instance
 
 case class PMACheckerParameter() extends SerializableModuleParameter
 class PMACheckerInterface(parameter: PMACheckerParameter) extends Bundle {
-
+  val paddr = Input(UInt())
+  val resp = Output(new Bundle {
+    val cacheable = Bool()
+    val r = Bool()
+    val w = Bool()
+    val pp = Bool()
+    val al = Bool()
+    val aa = Bool()
+    val x = Bool()
+    val eff = Bool()
+  })
 }
 
 class PMAChecker(val parameter: PMACheckerParameter)
   extends FixedIORawModule(new PMACheckerInterface(parameter))
     with SerializableModule[PMACheckerParameter] {
+  // main memory
+  io.resp.cacheable := false.B
+  // read
+  io.resp.r := false.B
+  // write
+  io.resp.w := false.B
+  // put partial
+  io.resp.pp := false.B
+  // logic
+  io.resp.al := false.B
+  // arithmetic
+  io.resp.aa := false.B
+  // exe
+  io.resp.x := false.B
+  // has side effects
+  io.resp.eff := false.B
 }
