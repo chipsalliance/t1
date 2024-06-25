d31eadba88cfa70ac57a305358a08affbc281f31
Jiuyang Liu <liu@jiuyang.me>
2024-06-25 15:38:55 +0800

    [rocket] migrate MulDiv

--- rocket/src/rocketv/MulDiv.scala
+++ rocket/src/rocketv/MulDiv.scala
@@ -7,13 +7,203 @@ package org.chipsalliance.rocketv
 import chisel3._
 import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
 import chisel3.experimental.hierarchy.Instance
+import chisel3.util.{BitPat, Cat, Decoupled, Enum, Fill, Log2, Pipe, Valid, log2Ceil, log2Floor}
 
-case class MulDivParameter() extends SerializableModuleParameter
+case class MulDivParameter() extends SerializableModuleParameter {
+  val latency: Int = ???
+  val width: Int = ???
+  val nXpr: Int = ???
+  val uopWidth: Int = 4
+
+  val divUnroll: Int = ???
+  val divEarlyOut: Boolean = ???
+  val divEarlyOutGranularity: Int = ???
+  val mulUnroll: Int = ???
+  val mulEarlyOut: Boolean = ???
+
+  def FN_MUL = 1.U(4.W)
+  def FN_MULH = 2.U(4.W)
+  def FN_MULHU = 3.U(4.W)
+  def FN_MULHSU = 4.U(4.W)
+  def FN_DIV = 4.U(4.W)
+  def FN_REM = 5.U(4.W)
+  def FN_DIVU = 6.U(4.W)
+  def FN_REMU = 7.U(4.W)
+  def DW_32 = false.B
+}
 class MulDivInterface(parameter: MulDivParameter) extends Bundle {
-
+  val req = Flipped(Decoupled(new MultiplierReq(parameter.width, log2Ceil(parameter.nXpr), parameter.uopWidth)))
+  val kill = Input(Bool())
+  val resp = Decoupled(new MultiplierResp(parameter.width, log2Ceil(parameter.nXpr)))
 }
 
 class MulDiv(val parameter: MulDivParameter)
   extends FixedIORawModule(new MulDivInterface(parameter))
     with SerializableModule[MulDivParameter] {
+  object cfg {
+    val divUnroll = parameter.divUnroll
+    val divEarlyOut = parameter.divEarlyOut
+    val divEarlyOutGranularity = parameter.divEarlyOutGranularity
+    val mulUnroll = parameter.mulUnroll
+    val mulEarlyOut = parameter.mulEarlyOut
+  }
+
+  def N = BitPat.N()
+  def Y = BitPat.N()
+  def X = BitPat.dontCare(1)
+
+
+  private def minDivLatency = Option.when(cfg.divUnroll > 0)(if (cfg.divEarlyOut) 3 else 1 + w / cfg.divUnroll)
+  private def minMulLatency = Option.when(cfg.mulUnroll > 0)(if (cfg.mulEarlyOut) 2 else w / cfg.mulUnroll)
+  def minLatency: Int = (minDivLatency ++ minMulLatency).min
+  val w = io.req.bits.in1.getWidth
+  val mulw = if (cfg.mulUnroll == 0) w else (w + cfg.mulUnroll - 1) / cfg.mulUnroll * cfg.mulUnroll
+  val fastMulW = if (cfg.mulUnroll == 0) false else w / 2 > cfg.mulUnroll && w % (2 * cfg.mulUnroll) == 0
+
+  val s_ready :: s_neg_inputs :: s_mul :: s_div :: s_dummy :: s_neg_output :: s_done_mul :: s_done_div :: Nil = Enum(8)
+  val state = RegInit(s_ready)
+
+  val req = Reg(chiselTypeOf(io.req.bits))
+  val count = Reg(
+    UInt(
+      log2Ceil(
+        (Option.when(cfg.divUnroll != 0)(w / cfg.divUnroll + 1).toSeq ++
+          Option.when(cfg.mulUnroll != 0)(mulw / cfg.mulUnroll)).reduce(_ max _)
+      ).W
+    )
+  )
+  val neg_out = Reg(Bool())
+  val isHi = Reg(Bool())
+  val resHi = Reg(Bool())
+  val divisor = Reg(Bits((w + 1).W)) // div only needs w bits
+  val remainder = Reg(Bits((2 * mulw + 2).W)) // div only needs 2*w+1 bits
+
+  val mulDecode = List(
+    parameter.FN_MUL -> List(Y, N, X, X),
+    parameter.FN_MULH -> List(Y, Y, Y, Y),
+    parameter.FN_MULHU -> List(Y, Y, N, N),
+    parameter.FN_MULHSU -> List(Y, Y, Y, N)
+  )
+  val divDecode = List(
+    parameter.FN_DIV -> List(N, N, Y, Y),
+    parameter.FN_REM -> List(N, Y, Y, Y),
+    parameter.FN_DIVU -> List(N, N, N, N),
+    parameter.FN_REMU -> List(N, Y, N, N)
+  )
+  val cmdMul :: cmdHi :: lhsSigned :: rhsSigned :: Nil =
+    DecodeLogic(
+      io.req.bits.fn,
+      List(X, X, X, X),
+      (if (cfg.divUnroll != 0) divDecode else Nil) ++ (if (cfg.mulUnroll != 0) mulDecode else Nil)
+    ).map(_.asBool)
+
+  require(w == 32 || w == 64)
+  def halfWidth(req: MultiplierReq) = (w > 32).B && req.dw === parameter.DW_32
+
+  def sext(x: Bits, halfW: Bool, signed: Bool) = {
+    val sign = signed && Mux(halfW, x(w / 2 - 1), x(w - 1))
+    val hi = Mux(halfW, Fill(w / 2, sign), x(w - 1, w / 2))
+    (Cat(hi, x(w / 2 - 1, 0)), sign)
+  }
+  val (lhs_in, lhs_sign) = sext(io.req.bits.in1, halfWidth(io.req.bits), lhsSigned)
+  val (rhs_in, rhs_sign) = sext(io.req.bits.in2, halfWidth(io.req.bits), rhsSigned)
+
+  val subtractor = remainder(2 * w, w) - divisor
+  val result = Mux(resHi, remainder(2 * w, w + 1), remainder(w - 1, 0))
+  val negated_remainder = -result
+
+  if (cfg.divUnroll != 0) when(state === s_neg_inputs) {
+    when(remainder(w - 1)) {
+      remainder := negated_remainder
+    }
+    when(divisor(w - 1)) {
+      divisor := subtractor
+    }
+    state := s_div
+  }
+  if (cfg.divUnroll != 0) when(state === s_neg_output) {
+    remainder := negated_remainder
+    state := s_done_div
+    resHi := false.B
+  }
+  if (cfg.mulUnroll != 0) when(state === s_mul) {
+    val mulReg = Cat(remainder(2 * mulw + 1, w + 1), remainder(w - 1, 0))
+    val mplierSign = remainder(w)
+    val mplier = mulReg(mulw - 1, 0)
+    val accum = mulReg(2 * mulw, mulw).asSInt
+    val mpcand = divisor.asSInt
+    val prod = Cat(mplierSign, mplier(cfg.mulUnroll - 1, 0)).asSInt * mpcand + accum
+    val nextMulReg = Cat(prod, mplier(mulw - 1, cfg.mulUnroll))
+    val nextMplierSign = count === (mulw / cfg.mulUnroll - 2).U && neg_out
+
+    val eOutMask = ((BigInt(-1) << mulw).S >> (count * cfg.mulUnroll.U)(log2Ceil(mulw) - 1, 0))(mulw - 1, 0)
+    val eOut = (cfg.mulEarlyOut).B && count =/= (mulw / cfg.mulUnroll - 1).U && count =/= 0.U &&
+      !isHi && (mplier & ~eOutMask) === 0.U
+    val eOutRes = (mulReg >> (mulw.U - count * cfg.mulUnroll.U)(log2Ceil(mulw) - 1, 0))
+    val nextMulReg1 = Cat(nextMulReg(2 * mulw, mulw), Mux(eOut, eOutRes, nextMulReg)(mulw - 1, 0))
+    remainder := Cat(nextMulReg1 >> w, nextMplierSign, nextMulReg1(w - 1, 0))
+
+    count := count + 1.U
+    when(eOut || count === (mulw / cfg.mulUnroll - 1).U) {
+      state := s_done_mul
+      resHi := isHi
+    }
+  }
+  if (cfg.divUnroll != 0) when(state === s_div) {
+    val unrolls = ((0 until cfg.divUnroll)
+      .scanLeft(remainder)) {
+      case (rem, i) =>
+        // the special case for iteration 0 is to save HW, not for correctness
+        val difference = if (i == 0) subtractor else rem(2 * w, w) - divisor(w - 1, 0)
+        val less = difference(w)
+        Cat(Mux(less, rem(2 * w - 1, w), difference(w - 1, 0)), rem(w - 1, 0), !less)
+    }
+      .tail
+
+    remainder := unrolls.last
+    when(count === (w / cfg.divUnroll).U) {
+      state := Mux(neg_out, s_neg_output, s_done_div)
+      resHi := isHi
+      if (w % cfg.divUnroll < cfg.divUnroll - 1)
+        remainder := unrolls(w % cfg.divUnroll)
+    }
+    count := count + 1.U
+
+    val divby0 = count === 0.U && !subtractor(w)
+    if (cfg.divEarlyOut) {
+      val align = 1 << log2Floor(cfg.divUnroll.max(cfg.divEarlyOutGranularity))
+      val alignMask = ~((align - 1).U(log2Ceil(w).W))
+      val divisorMSB = Log2(divisor(w - 1, 0), w) & alignMask
+      val dividendMSB = Log2(remainder(w - 1, 0), w) | ~alignMask
+      val eOutPos = ~(dividendMSB - divisorMSB)
+      val eOut = count === 0.U && !divby0 && eOutPos >= align.U
+      when(eOut) {
+        remainder := remainder(w - 1, 0) << eOutPos
+        count := eOutPos >> log2Floor(cfg.divUnroll)
+      }
+    }
+    when(divby0 && !isHi) { neg_out := false.B }
+  }
+  when(io.resp.fire || io.kill) {
+    state := s_ready
+  }
+  when(io.req.fire) {
+    state := Mux(cmdMul, s_mul, Mux(lhs_sign || rhs_sign, s_neg_inputs, s_div))
+    isHi := cmdHi
+    resHi := false.B
+    count := (if (fastMulW) Mux[UInt](cmdMul && halfWidth(io.req.bits), (w / cfg.mulUnroll / 2).U, 0.U) else 0.U)
+    neg_out := Mux(cmdHi, lhs_sign, lhs_sign =/= rhs_sign)
+    divisor := Cat(rhs_sign, rhs_in)
+    remainder := lhs_in
+    req := io.req.bits
+  }
+
+  val outMul = (state & (s_done_mul ^ s_done_div)) === (s_done_mul & ~s_done_div)
+  val loOut = Mux(fastMulW.B && halfWidth(req) && outMul, result(w - 1, w / 2), result(w / 2 - 1, 0))
+  val hiOut = Mux(halfWidth(req), Fill(w / 2, loOut(w / 2 - 1)), result(w - 1, w / 2))
+  io.resp.bits.tag := req.tag
+
+  io.resp.bits.data := Cat(hiOut, loOut)
+  io.resp.valid := (state === s_done_mul || state === s_done_div)
+  io.req.ready := state === s_ready
 }
