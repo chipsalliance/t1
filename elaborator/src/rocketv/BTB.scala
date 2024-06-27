2b8a3b2dbb077e1d831b7af33c090d9d6bc721ed
Jiuyang Liu <liu@jiuyang.me>
2024-06-27 16:30:02 +0800

    [rocketv] add elaborator for BTB
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.BTB config --useAsyncReset true --fetchBytes 16 --vaddrBits 34 --entries 28 --nMatchBits 14 --nPages 6 --nRAS 6 --cacheBlockBytes 64 --iCacheSet 64 --useCompressed true --updatesOutOfOrder false --bht-nEntries 512 --bht-counterLength 1 --bht-historyLength 8 --bht-historyBits 3
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.BTB design --parameter ./BTB.json --run-firtool

---
+++ elaborator/src/rocketv/BTB.scala
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{BHTParameter, BTB, BTBParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object BTB extends Elaborator {
+  @main
+  case class BHTParameterMain(
+    @arg(name = "nEntries") nEntries:           Int,
+    @arg(name = "counterLength") counterLength: Int,
+    @arg(name = "historyLength") historyLength: Int,
+    @arg(name = "historyBits") historyBits:     Int) {
+    def convert: BHTParameter = BHTParameter(
+      nEntries,
+      counterLength,
+      historyLength,
+      historyBits
+    )
+  }
+  implicit def BHTParameterMainParser: ParserForClass[BHTParameterMain] = ParserForClass[BHTParameterMain]
+
+  @main
+  case class BTBParameterMain(
+    @arg(name = "useAsyncReset") useAsyncReset:         Boolean,
+    @arg(name = "fetchBytes") fetchBytes:               Int,
+    @arg(name = "vaddrBits") vaddrBits:                 Int,
+    @arg(name = "entries") entries:                     Int,
+    @arg(name = "nMatchBits") nMatchBits:               Int,
+    @arg(name = "nPages") nPages:                       Int,
+    @arg(name = "nRAS") nRAS:                           Int,
+    @arg(name = "cacheBlockBytes") cacheBlockBytes:     Int,
+    @arg(name = "iCacheSet") iCacheSet:                 Int,
+    @arg(name = "useCompressed") useCompressed:         Boolean,
+    @arg(name = "updatesOutOfOrder") updatesOutOfOrder: Boolean,
+    @arg(name = "bht-nEntries") nEntries:               Option[Int],
+    @arg(name = "bht-counterLength") counterLength:     Option[Int],
+    @arg(name = "bht-historyLength") historyLength:     Option[Int],
+    @arg(name = "bht-historyBits") historyBits:         Option[Int]) {
+    def convert: BTBParameter = BTBParameter(
+      useAsyncReset,
+      fetchBytes,
+      vaddrBits,
+      entries,
+      nMatchBits,
+      nPages,
+      nRAS,
+      cacheBlockBytes,
+      iCacheSet,
+      useCompressed,
+      updatesOutOfOrder,
+      (nEntries
+        .lazyZip(counterLength)
+        .lazyZip(historyLength)
+        .lazyZip(historyBits))
+        .map {
+          case (nEntries, counterLength, historyLength, historyBits) =>
+            BHTParameter(nEntries, counterLength, historyLength, historyBits)
+        }
+        .headOption
+    )
+  }
+
+  implicit def BTBParameterMainParser: ParserForClass[BTBParameterMain] = ParserForClass[BTBParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: BTBParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[BTB, BTBParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
