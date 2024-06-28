bb1eb36185dff291aeac1f878c81c847a2daa79e
qinjun-li <liqinjun1994@gmail.com>
2024-06-28 13:44:14 +0800

    [rocketv] add elaborator for ICache
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.ICache config --vaddrBits 32 --paddrBits 32
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.ICache design --parameter ./ICache.json --run-firtool

---
+++ elaborator/src/rocketv/ICache.scala
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{ICache, ICacheParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object ICache extends Elaborator {
+  @main
+  case class ICacheParameterMain(
+    @arg(name = "useAsyncReset") useAsyncReset: Boolean,
+    @arg(name = "prefetch") prefetch:           Boolean,
+    @arg(name = "nSets") nSets:                 Int,
+    @arg(name = "nWays") nWays:                 Int,
+    @arg(name = "blockBytes") blockBytes:       Int,
+    @arg(name = "usingVM") usingVM:             Boolean,
+    @arg(name = "vaddrBits") vaddrBits:         Int,
+    @arg(name = "paddrBits") paddrBits:         Int) {
+    def convert: ICacheParameter = ICacheParameter(
+      useAsyncReset,
+      prefetch,
+      nSets,
+      nWays,
+      blockBytes,
+      usingVM,
+      vaddrBits,
+      paddrBits
+    )
+  }
+
+  implicit def ICacheParameterMainParser: ParserForClass[ICacheParameterMain] = ParserForClass[ICacheParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: ICacheParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[ICache, ICacheParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
