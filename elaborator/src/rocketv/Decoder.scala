ed1ffc70c7d1c4fe6675d36db2a52c01814afe5d
Jiuyang Liu <liu@jiuyang.me>
2024-06-29 15:00:03 +0800

    [rocketv] add elaborator for Decoder
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.Decoder config --pipelinedMul false --fenceIFlushDCache false --instructionSets rv32_i --instructionSets rv_v
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.Decoder design --parameter ./Decoder.json --run-firtool

---
+++ elaborator/src/rocketv/Decoder.scala
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{Decoder, DecoderParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object Decoder extends Elaborator {
+  @main
+  case class DecoderParameterMain(
+    @arg(name = "instructionSets") instructionSets:     Set[String],
+    @arg(name = "pipelinedMul") pipelinedMul:           Boolean,
+    @arg(name = "fenceIFlushDCache") fenceIFlushDCache: Boolean) {
+    def convert: DecoderParameter = DecoderParameter(
+      instructionSets,
+      pipelinedMul,
+      fenceIFlushDCache
+    )
+  }
+
+  implicit def DecoderParameterMainParser: ParserForClass[DecoderParameterMain] = ParserForClass[DecoderParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: DecoderParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[Decoder, DecoderParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
