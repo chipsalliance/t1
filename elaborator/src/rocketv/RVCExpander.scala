045beeffa1cc88cec688b560b27551ce739cdfdd
Jiuyang Liu <liu@jiuyang.me>
2024-06-29 23:10:32 +0800

    [rocketv] add elaborator for RVCExpander
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.RVCExpander config --xLen 32 --usingCompressed true
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.RVCExpander design --parameter ./RVCExpander.json --run-firtool

---
+++ elaborator/src/rocketv/RVCExpander.scala
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{RVCExpander, RVCExpanderParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object RVCExpander extends Elaborator {
+  @main
+  case class RVCExpanderParameterMain(
+    @arg(name = "xLen") xLen:                       Int,
+    @arg(name = "usingCompressed") usingCompressed: Boolean) {
+    def convert: RVCExpanderParameter = RVCExpanderParameter(
+      xLen,
+      usingCompressed
+    )
+  }
+
+  implicit def RVCExpanderParameterMainParser: ParserForClass[RVCExpanderParameterMain] =
+    ParserForClass[RVCExpanderParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: RVCExpanderParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[RVCExpander, RVCExpanderParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
