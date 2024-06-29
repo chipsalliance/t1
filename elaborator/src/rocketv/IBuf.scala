5a28953d4c13a3df5e120b537c9718914db205a4
Jiuyang Liu <liu@jiuyang.me>
2024-06-30 00:56:44 +0800

    [rocketv] add elaborator for IBuf
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.IBuf design --useAsyncReset true --xLen 32 --usingCompressed true --vaddrBits 32 --entries 4 --vaddrBitsExtended 32
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.IBuf design --parameter ./IBuf.json --run-firtool

---
+++ elaborator/src/rocketv/IBuf.scala
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{IBuf, IBufParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object IBuf extends Elaborator {
+  @main
+  case class IBufParameterMain(
+    @arg(name = "useAsyncReset") useAsyncReset:         Boolean,
+    @arg(name = "xLen") xLen:                           Int,
+    @arg(name = "usingCompressed") usingCompressed:     Boolean,
+    @arg(name = "vaddrBits") vaddrBits:                 Int,
+    @arg(name = "entries") entries:                     Int,
+    @arg(name = "vaddrBitsExtended") vaddrBitsExtended: Int,
+    @arg(name = "bhtHistoryLength") bhtHistoryLength:   Option[Int],
+    @arg(name = "bhtCounterLength") bhtCounterLength:   Option[Int]) {
+    def convert: IBufParameter = IBufParameter(
+      useAsyncReset,
+      xLen,
+      usingCompressed,
+      vaddrBits,
+      entries,
+      vaddrBitsExtended,
+      bhtHistoryLength,
+      bhtCounterLength
+    )
+  }
+
+  implicit def IBufParameterMainParser: ParserForClass[IBufParameterMain] = ParserForClass[IBufParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: IBufParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[IBuf, IBufParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
