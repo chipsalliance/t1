28eb7839227be79266f3085d191b0cedd2929864
Jiuyang Liu <liu@jiuyang.me>
2024-06-30 02:15:31 +0800

    [rocketv] add elaborator for MulDiv and PipelinedMultiplier
    - MulDiv
     - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.MulDiv config --useAsyncReset true --latency 2 --width 32 --divUnroll 1 --divEarlyOut false --divEarlyOutGranularity 1 --mulUnroll 1 --mulEarlyOut false
     - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.MulDiv design --parameter ./MulDiv.json --run-firtool
    - PipelinedMultiplier
     - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.PipelinedMultiplier config --useAsyncReset true --latency 2 --width 32
     - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.PipelinedMultiplier design --parameter ./PipelinedMultiplier.json --run-firtool

---
+++ elaborator/src/rocketv/PipelinedMultiplier.scala
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{PipelinedMultiplier, PipelinedMultiplierParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object PipelinedMultiplier extends Elaborator {
+  @main
+  case class PipelinedMultiplierParameterMain(
+    @arg(name = "useAsyncReset") useAsyncReset:                   Boolean,
+    @arg(name = "latency") latency:                               Int,
+    @arg(name = "width") width:                                   Int) {
+    def convert: PipelinedMultiplierParameter = PipelinedMultiplierParameter(
+      useAsyncReset: Boolean,
+      latency:       Int,
+      width:         Int
+    )
+  }
+
+  implicit def PipelinedMultiplierParameterMainParser: ParserForClass[PipelinedMultiplierParameterMain] = ParserForClass[PipelinedMultiplierParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: PipelinedMultiplierParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[PipelinedMultiplier, PipelinedMultiplierParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
