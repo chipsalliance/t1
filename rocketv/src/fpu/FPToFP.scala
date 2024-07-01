91446d9099732923317e825f047b6fbbcd6feed3
Jiuyang Liu <liu@jiuyang.me>
2024-07-01 14:10:44 +0800

    [rocketv] migrate FPU

--- rocketv/src/fpu/FPToFP.scala
+++ rocketv/src/fpu/FPToFP.scala
@@ -1,29 +1,63 @@
-// See LICENSE.Berkeley for license details.
-// See LICENSE.SiFive for license details.
-
-package freechips.rocketchip.tile
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2012-2014 The Regents of the University of California
+// SPDX-FileCopyrightText: 2016-2017 SiFive, Inc
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.rocketv
 
 import chisel3._
+import chisel3.experimental.hierarchy.instantiable
+import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
 import chisel3.util.{Cat, Pipe, Valid}
-import freechips.rocketchip.tile.{FPInput, FPResult, FPUModule}
 
-class FPToFP(val latency: Int)(implicit p: Parameters) extends FPUModule()(p) with ShouldBeRetimed {
-  val io = IO(new Bundle {
-    val in = Flipped(Valid(new FPInput))
-    val out = Valid(new FPResult)
-    val lt = Input(Bool()) // from FPToInt
-  })
+object FPToFPParameter {
+  implicit def rwP: upickle.default.ReadWriter[FPToFPParameter] = upickle.default.macroRW[FPToFPParameter]
+}
+
+case class FPToFPParameter(
+  useAsyncReset: Boolean,
+  latency:       Int,
+  xLen:          Int,
+  fLen:          Int,
+  minFLen:       Int)
+    extends SerializableModuleParameter
+
+class FPToFPInterface(parameter: FPToFPParameter) extends Bundle {
+  val clock = Input(Clock())
+  val reset = Input(if (parameter.useAsyncReset) AsyncReset() else Bool())
+  val in = Flipped(Valid(new FPInput(parameter.fLen)))
+  val out = Valid(new FPResult(parameter.fLen))
+  val lt = Input(Bool()) // from FPToInt
+}
+
+@instantiable
+class FPToFP(val parameter: FPToFPParameter)
+    extends FixedIORawModule(new FPToFPInterface(parameter))
+    with SerializableModule[FPToFPParameter]
+    with ImplicitClock
+    with ImplicitReset {
+  override protected def implicitClock: Clock = io.clock
+  override protected def implicitReset: Reset = io.reset
+
+  val fLen = parameter.fLen
+  val minFLen = parameter.minFLen
+  val xLen = parameter.xLen
+  val latency = parameter.latency
+  val helper = new FPUHelper(minFLen, fLen, xLen)
+  val maxType = helper.maxType
+  val floatTypes = helper.floatTypes
+  def typeTag(t:     FType) = helper.typeTag(t)
+  def sanitizeNaN(x: UInt, t: FType) = helper.sanitizeNaN(x, t)
 
   val in = Pipe(io.in)
 
   val signNum = Mux(in.bits.rm(1), in.bits.in1 ^ in.bits.in2, Mux(in.bits.rm(0), ~in.bits.in2, in.bits.in2))
-  val fsgnj = Cat(signNum(fLen), in.bits.in1(fLen-1, 0))
+  val fsgnj = Cat(signNum(fLen), in.bits.in1(fLen - 1, 0))
 
-  val fsgnjMux = Wire(new FPResult)
+  val fsgnjMux = Wire(new FPResult(parameter.fLen))
   fsgnjMux.exc := 0.U
   fsgnjMux.data := fsgnj
 
-  when (in.bits.wflags) { // fmin/fmax
+  when(in.bits.fpuControl.wflags) { // fmin/fmax
     val isnan1 = maxType.isNaN(in.bits.in1)
     val isnan2 = maxType.isNaN(in.bits.in2)
     val isInvalid = maxType.isSNaN(in.bits.in1) || maxType.isSNaN(in.bits.in2)
@@ -33,16 +67,16 @@ class FPToFP(val latency: Int)(implicit p: Parameters) extends FPUModule()(p) wi
     fsgnjMux.data := Mux(isNaNOut, maxType.qNaN, Mux(isLHS, in.bits.in1, in.bits.in2))
   }
 
-  val inTag = in.bits.typeTagIn
-  val outTag = in.bits.typeTagOut
+  val inTag = in.bits.fpuControl.typeTagIn
+  val outTag = in.bits.fpuControl.typeTagOut
   val mux = WireDefault(fsgnjMux)
   for (t <- floatTypes.init) {
-    when (outTag === typeTag(t).U) {
+    when(outTag === typeTag(t).U) {
       mux.data := Cat(fsgnjMux.data >> t.recodedWidth, maxType.unsafeConvert(fsgnjMux.data, t))
     }
   }
 
-  when (in.bits.wflags && !in.bits.ren2) { // fcvt
+  when(in.bits.fpuControl.wflags && !in.bits.fpuControl.ren2) { // fcvt
     if (floatTypes.size > 1) {
       // widening conversions simply canonicalize NaN operands
       val widened = Mux(maxType.isNaN(in.bits.in1), maxType.qNaN, in.bits.in1)
@@ -52,17 +86,18 @@ class FPToFP(val latency: Int)(implicit p: Parameters) extends FPUModule()(p) wi
       // narrowing conversions require rounding (for RVQ, this could be
       // optimized to use a single variable-position rounding unit, rather
       // than two fixed-position ones)
-      for (outType <- floatTypes.init) when (outTag === typeTag(outType).U && ((typeTag(outType) == 0).B || outTag < inTag)) {
-        val narrower = Module(new hardfloat.RecFNToRecFN(maxType.exp, maxType.sig, outType.exp, outType.sig))
-        narrower.io.in := in.bits.in1
-        narrower.io.roundingMode := in.bits.rm
-        narrower.io.detectTininess := hardfloat.consts.tininess_afterRounding
-        val narrowed = sanitizeNaN(narrower.io.out, outType)
-        mux.data := Cat(fsgnjMux.data >> narrowed.getWidth, narrowed)
-        mux.exc := narrower.io.exceptionFlags
-      }
+      for (outType <- floatTypes.init)
+        when(outTag === typeTag(outType).U && ((typeTag(outType) == 0).B || outTag < inTag)) {
+          val narrower = Module(new hardfloat.RecFNToRecFN(maxType.exp, maxType.sig, outType.exp, outType.sig))
+          narrower.io.in := in.bits.in1
+          narrower.io.roundingMode := in.bits.rm
+          narrower.io.detectTininess := hardfloat.consts.tininess_afterRounding
+          val narrowed = sanitizeNaN(narrower.io.out, outType)
+          mux.data := Cat(fsgnjMux.data >> narrowed.getWidth, narrowed)
+          mux.exc := narrower.io.exceptionFlags
+        }
     }
   }
 
-  io.out <> Pipe(in.valid, mux, latency-1)
+  io.out <> Pipe(in.valid, mux, latency - 1)
 }
--- rocketv/src/fpu/FPToInt.scala
+++ rocketv/src/fpu/FPToInt.scala
@@ -1,55 +1,94 @@
-// See LICENSE.Berkeley for license details.
-// See LICENSE.SiFive for license details.
-
-package freechips.rocketchip.tile
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2012-2014 The Regents of the University of California
+// SPDX-FileCopyrightText: 2016-2017 SiFive, Inc
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.rocketv
 
 import chisel3._
-import chisel3.util.{Cat, Fill, RegEnable, Valid, log2Ceil}
-import freechips.rocketchip.tile.{FPConstants, FPInput, FPUModule, FType}
+import chisel3.experimental.hierarchy.instantiable
+import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
+import chisel3.util._
 
-class FPToInt(implicit p: Parameters) extends FPUModule()(p) with ShouldBeRetimed {
-  class Output extends Bundle {
-    val in = new FPInput
-    val lt = Bool()
-    val store = Bits(fLen.W)
-    val toint = Bits(xLen.W)
-    val exc = Bits(FPConstants.FLAGS_SZ.W)
-  }
-  val io = IO(new Bundle {
-    val in = Flipped(Valid(new FPInput))
-    val out = Valid(new Output)
-  })
+object FPToIntParameter {
+  implicit def rwP: upickle.default.ReadWriter[FPToIntParameter] = upickle.default.macroRW[FPToIntParameter]
+}
+
+case class FPToIntParameter(
+  useAsyncReset: Boolean,
+  xLen:          Int,
+  fLen:          Int,
+  minFLen:       Int)
+    extends SerializableModuleParameter
+class FPToIntInterface(parameter: FPToIntParameter) extends Bundle {
+  val clock = Input(Clock())
+  val reset = Input(if (parameter.useAsyncReset) AsyncReset() else Bool())
+  val in = Flipped(Valid(new FPInput(parameter.fLen)))
+  val out = Valid(new FPToIntOutput(parameter.fLen, parameter.xLen))
+}
+
+@instantiable
+class FPToInt(val parameter: FPToIntParameter)
+    extends FixedIORawModule(new FPToIntInterface(parameter))
+    with SerializableModule[FPToIntParameter]
+    with ImplicitClock
+    with ImplicitReset {
+  override protected def implicitClock: Clock = io.clock
+  override protected def implicitReset: Reset = io.reset
+
+  val minFLen: Int = parameter.minFLen
+  val fLen:    Int = parameter.fLen
+  val xLen:    Int = parameter.xLen
+  val helper = new FPUHelper(minFLen, fLen, xLen)
+  val maxExpWidth = helper.maxExpWidth
+  val maxSigWidth = helper.maxSigWidth
+  val floatTypes = helper.floatTypes
+  val maxType = helper.maxType
+  val minXLen = helper.minXLen
+  val nIntTypes = helper.nIntTypes
+  def ieee(x: UInt, t: FType = maxType) = helper.ieee(x, t)
 
   val in = RegEnable(io.in.bits, io.in.valid)
   val valid = RegNext(io.in.valid)
+
+  def sextTo(x: UInt, n: Int): UInt = {
+    require(x.getWidth <= n)
+    if (x.getWidth == n) x
+    else Cat(Fill(n - x.getWidth, x(x.getWidth - 1)), x)
+  }
 
   val dcmp = Module(new hardfloat.CompareRecFN(maxExpWidth, maxSigWidth))
   dcmp.io.a := in.in1
   dcmp.io.b := in.in2
   dcmp.io.signaling := !in.rm(1)
 
-  val tag = in.typeTagOut
-  val store = (floatTypes.map(t => if (t == FType.H) Fill(maxType.ieeeWidth / minXLen,   ieee(in.in1)(15, 0).sextTo(minXLen))
-  else              Fill(maxType.ieeeWidth / t.ieeeWidth, ieee(in.in1)(t.ieeeWidth - 1, 0))): Seq[UInt])(tag)
+  val tag = in.fpuControl.typeTagOut
+  val store = VecInit(
+    floatTypes.map(t =>
+      if (t == FType.H) Fill(maxType.ieeeWidth / minXLen, sextTo(ieee(in.in1)(15, 0), minXLen))
+      else Fill(maxType.ieeeWidth / t.ieeeWidth, ieee(in.in1)(t.ieeeWidth - 1, 0))
+    )
+  )(tag)
   val toint = WireDefault(store)
   val intType = WireDefault(in.fmt(0))
   io.out.bits.store := store
-  io.out.bits.toint := ((0 until nIntTypes).map(i => toint((minXLen << i) - 1, 0).sextTo(xLen)): Seq[UInt])(intType)
+  io.out.bits.toint := VecInit(
+    (0 until helper.nIntTypes).map(i => sextTo(toint((helper.minXLen << i) - 1, 0), xLen)): Seq[UInt]
+  )(intType)
   io.out.bits.exc := 0.U
 
-  when (in.rm(0)) {
-    val classify_out = (floatTypes.map(t => t.classify(maxType.unsafeConvert(in.in1, t))): Seq[UInt])(tag)
+  when(in.rm(0)) {
+    val classify_out = VecInit(floatTypes.map(t => t.classify(maxType.unsafeConvert(in.in1, t))))(tag)
     toint := classify_out | (store >> minXLen << minXLen)
     intType := false.B
   }
 
-  when (in.wflags) { // feq/flt/fle, fcvt
+  when(in.fpuControl.wflags) { // feq/flt/fle, fcvt
     toint := (~in.rm & Cat(dcmp.io.lt, dcmp.io.eq)).orR | (store >> minXLen << minXLen)
     io.out.bits.exc := dcmp.io.exceptionFlags
     intType := false.B
 
-    when (!in.ren2) { // fcvt
-      val cvtType = in.typ.extract(log2Ceil(nIntTypes), 1)
+    when(!in.fpuControl.ren2) { // fcvt
+      val cvtType = if (log2Ceil(nIntTypes) == 0) 0.U else in.typ(log2Ceil(nIntTypes), 1)
       intType := cvtType
       val conv = Module(new hardfloat.RecFNToIN(maxExpWidth, maxSigWidth, xLen))
       conv.io.in := in.in1
@@ -58,18 +97,18 @@ class FPToInt(implicit p: Parameters) extends FPUModule()(p) with ShouldBeRetime
       toint := conv.io.out
       io.out.bits.exc := Cat(conv.io.intExceptionFlags(2, 1).orR, 0.U(3.W), conv.io.intExceptionFlags(0))
 
-      for (i <- 0 until nIntTypes-1) {
+      for (i <- 0 until nIntTypes - 1) {
         val w = minXLen << i
-        when (cvtType === i.U) {
+        when(cvtType === i.U) {
           val narrow = Module(new hardfloat.RecFNToIN(maxExpWidth, maxSigWidth, w))
           narrow.io.in := in.in1
           narrow.io.roundingMode := in.rm
           narrow.io.signedOut := ~in.typ(0)
 
           val excSign = in.in1(maxExpWidth + maxSigWidth) && !maxType.isNaN(in.in1)
-          val excOut = Cat(conv.io.signedOut === excSign, Fill(w-1, !excSign))
+          val excOut = Cat(conv.io.signedOut === excSign, Fill(w - 1, !excSign))
           val invalid = conv.io.intExceptionFlags(2) || narrow.io.intExceptionFlags(1)
-          when (invalid) { toint := Cat(conv.io.out >> w, excOut) }
+          when(invalid) { toint := Cat(conv.io.out >> w, excOut) }
           io.out.bits.exc := Cat(invalid, 0.U(3.W), !invalid && conv.io.intExceptionFlags(0))
         }
       }
