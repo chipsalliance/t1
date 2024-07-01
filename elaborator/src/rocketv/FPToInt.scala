1adfbcfbe88b4859acad66be6f454a20ae63c185
Jiuyang Liu <liu@jiuyang.me>
2024-07-01 14:10:54 +0800

    [rocketv] add elaborator for FPU
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.FPU config --useAsyncReset true --useClockGating true --xLen 32 --fLen 32 --minFLen 32 --sfmaLatency 3 --dfmaLatency 4 --divSqrt true --hartIdLen 1
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.FPU design --parameter ./FPU.json --run-firtool

---
+++ elaborator/src/rocketv/FPToInt.scala
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{FPToInt, FPToIntParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object FPToInt extends Elaborator {
+  @main
+  case class FPToIntParameterMain(
+    @arg(name = "useAsyncReset") useAsyncReset: Boolean,
+    @arg(name = "xLen") xLen:                   Int,
+    @arg(name = "fLen") fLen:                   Int,
+    @arg(name = "minFLen") minFLen:             Int) {
+    def convert: FPToIntParameter = FPToIntParameter(
+      useAsyncReset,
+      xLen,
+      fLen,
+      minFLen
+    )
+  }
+
+  implicit def FPToIntParameterMainParser: ParserForClass[FPToIntParameterMain] = ParserForClass[FPToIntParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: FPToIntParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[FPToInt, FPToIntParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
---
+++ elaborator/src/rocketv/FPUFMAPipe.scala
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{FPUFMAPipe, FPUFMAPipeParameter, FType}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object FPUFMAPipe extends Elaborator {
+  @main
+  case class FPUFMAPipeParameterMain(
+    useAsyncReset: Boolean,
+    latency:       Int,
+    xLen:          Int,
+    fLen:          Int,
+    minFLen:       Int,
+    t:             String) {
+
+    def convert: FPUFMAPipeParameter = FPUFMAPipeParameter(
+      useAsyncReset,
+      latency,
+      xLen,
+      fLen,
+      minFLen,
+      t match {
+        case s"e${exp}s${sig}" => FType(exp.toInt, sig.toInt)
+        case "h"               => FType(5, 11)
+        case "s"               => FType(8, 24)
+        case "d"               => FType(11, 53)
+      }
+    )
+  }
+
+  implicit def FPUFMAPipeParameterMainParser: ParserForClass[FPUFMAPipeParameterMain] =
+    ParserForClass[FPUFMAPipeParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: FPUFMAPipeParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[FPUFMAPipe, FPUFMAPipeParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
