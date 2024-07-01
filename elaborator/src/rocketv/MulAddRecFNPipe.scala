ce08bea1db1de3fd0066c3ccd15333fa6e209e74
Jiuyang Liu <liu@jiuyang.me>
2024-07-01 14:10:54 +0800

    [rocketv] add elaborator for FPU
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.FPU config --useAsyncReset true --useClockGating true --xLen 32 --fLen 32 --minFLen 32 --sfmaLatency 3 --dfmaLatency 4 --divSqrt true --hartIdLen 1
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.FPU design --parameter ./FPU.json --run-firtool

---
+++ elaborator/src/rocketv/MulAddRecFNPipe.scala
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{MulAddRecFNPipe, MulAddRecFNPipeParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object MulAddRecFNPipe extends Elaborator {
+  @main
+  case class MulAddRecFNPipeParameterMain(
+    @arg(name = "useAsyncReset") useAsyncReset: Boolean,
+    @arg(name = "latency") latency:             Int,
+    @arg(name = "expWidth") expWidth:           Int,
+    @arg(name = "sigWidth") sigWidth:           Int) {
+    def convert: MulAddRecFNPipeParameter = MulAddRecFNPipeParameter(useAsyncReset, latency, expWidth, sigWidth)
+  }
+
+  implicit def MulAddRecFNPipeParameterMainParser: ParserForClass[MulAddRecFNPipeParameterMain] =
+    ParserForClass[MulAddRecFNPipeParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: MulAddRecFNPipeParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[MulAddRecFNPipe, MulAddRecFNPipeParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
