8a5fb13a23e4aeb70ada01e77472776bb8a4773e
Jiuyang Liu <liu@jiuyang.me>
2024-07-02 19:52:08 +0800

    [rocketv] add elaborator for Frontend
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.Frontend config --useAsyncReset false --clockGate true --xLen 32 --usingAtomics true --usingDataScratchpad false --usingVM false --usingCompressed true --usingBTB false --itlbNSets 1 --itlbNWays 32 --itlbNSectors 4 --itlbNSuperpageEntries 4 --blockBytes 32 --iCacheNSets 64 --iCacheNWays 4 --iCachePrefetch false --btbEntries 28 --btbNMatchBits 14 --btbUpdatesOutOfOrder false --nPages 6 --nRAS 6 --nPMPs 8 --paddrBits 32 --pgLevels 2 --asidBits 0 --bhtNEntries 512 --bhtCounterLength 1 --bhtHistoryLength 8 --bhtHistoryBits 3 --legal 00000000-ffffffff --cacheable 80000000-ffffffff --read 00000000-ffffffff --write 00000000-ffffffff --putPartial 00000000-ffffffff --exec 80000000-ffffffff --sideEffects 00000000-3fffffff
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.Frontend design --parameter ./Frontend.json --run-firtool

---
+++ elaborator/src/rocketv/Frontend.scala
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import chisel3.util.BitPat
+import chisel3.util.experimental.BitSet
+import mainargs._
+import org.chipsalliance.rocketv.{BHTParameter, Frontend, FrontendParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object Frontend extends Elaborator {
+  implicit object BitSetRead extends TokensReader.Simple[BitSet] {
+    def shortName = "bitset"
+    def read(strs: Seq[String]) = {
+      Right(
+        strs.head
+          .split(",")
+          .map { opt =>
+            if (opt.contains("-")) {
+              val range = opt.split("-")
+              require(range.size == 2)
+              val from = BigInt(range.head, 16)
+              val to = BigInt(range.last, 16) + 1
+              BitSet.fromRange(from, to - from, range.head.length * 4)
+            } else if (opt.contains("+")) {
+              val range = opt.split("\\+")
+              require(range.size == 2)
+              val from = BigInt(range.head, 16)
+              val length = BigInt(range.last, 16)
+              BitSet.fromRange(from, length, range.head.length * 4)
+            } else {
+              BitPat(s"b$opt")
+            }
+          }
+          .reduce(_.union(_))
+      )
+    }
+  }
+
+  @main
+  case class FrontendParameterMain(
+    @arg(name = "useAsyncReset") useAsyncReset:                 Boolean,
+    @arg(name = "clockGate") clockGate:                         Boolean,
+    @arg(name = "xLen") xLen:                                   Int,
+    @arg(name = "usingAtomics") usingAtomics:                   Boolean,
+    @arg(name = "usingDataScratchpad") usingDataScratchpad:     Boolean,
+    @arg(name = "usingVM") usingVM:                             Boolean,
+    @arg(name = "usingCompressed") usingCompressed:             Boolean,
+    @arg(name = "usingBTB") usingBTB:                           Boolean,
+    @arg(name = "itlbNSets") itlbNSets:                         Int,
+    @arg(name = "itlbNWays") itlbNWays:                         Int,
+    @arg(name = "itlbNSectors") itlbNSectors:                   Int,
+    @arg(name = "itlbNSuperpageEntries") itlbNSuperpageEntries: Int,
+    @arg(name = "blockBytes") blockBytes:                       Int,
+    @arg(name = "iCacheNSets") iCacheNSets:                     Int,
+    @arg(name = "iCacheNWays") iCacheNWays:                     Int,
+    @arg(name = "iCachePrefetch") iCachePrefetch:               Boolean,
+    @arg(name = "btbEntries") btbEntries:                       Int,
+    @arg(name = "btbNMatchBits") btbNMatchBits:                 Int,
+    @arg(name = "btbUpdatesOutOfOrder") btbUpdatesOutOfOrder:   Boolean,
+    @arg(name = "nPages") nPages:                               Int,
+    @arg(name = "nRAS") nRAS:                                   Int,
+    @arg(name = "nPMPs") nPMPs:                                 Int,
+    @arg(name = "paddrBits") paddrBits:                         Int,
+    @arg(name = "pgLevels") pgLevels:                           Int,
+    @arg(name = "asidBits") asidBits:                           Int,
+    @arg(name = "bhtNEntries") bhtNEntries:                     Option[Int],
+    @arg(name = "bhtCounterLength") bhtCounterLength:           Option[Int],
+    @arg(name = "bhtHistoryLength") bhtHistoryLength:           Option[Int],
+    @arg(name = "bhtHistoryBits") bhtHistoryBits:               Option[Int],
+    @arg(name = "legal") legal:                                 Seq[BitSet],
+    @arg(name = "cacheable") cacheable:                         Seq[BitSet],
+    @arg(name = "read") read:                                   Seq[BitSet],
+    @arg(name = "write") write:                                 Seq[BitSet],
+    @arg(name = "putPartial") putPartial:                       Seq[BitSet],
+    @arg(name = "logic") logic:                                 Seq[BitSet],
+    @arg(name = "arithmetic") arithmetic:                       Seq[BitSet],
+    @arg(name = "exec") exec:                                   Seq[BitSet],
+    @arg(name = "sideEffects") sideEffects:                     Seq[BitSet]) {
+    def convert: FrontendParameter = FrontendParameter(
+      useAsyncReset:         Boolean,
+      clockGate:             Boolean,
+      xLen:                  Int,
+      usingAtomics:          Boolean,
+      usingDataScratchpad:   Boolean,
+      usingVM:               Boolean,
+      usingCompressed:       Boolean,
+      usingBTB:              Boolean,
+      itlbNSets:             Int,
+      itlbNWays:             Int,
+      itlbNSectors:          Int,
+      itlbNSuperpageEntries: Int,
+      blockBytes:            Int,
+      iCacheNSets:           Int,
+      iCacheNWays:           Int,
+      iCachePrefetch:        Boolean,
+      btbEntries:            Int,
+      btbNMatchBits:         Int,
+      btbUpdatesOutOfOrder:  Boolean,
+      nPages:                Int,
+      nRAS:                  Int,
+      nPMPs:                 Int,
+      paddrBits:             Int,
+      pgLevels:              Int,
+      asidBits:              Int,
+      bhtNEntries
+        .lazyZip(bhtCounterLength)
+        .lazyZip(bhtHistoryLength)
+        .lazyZip(bhtHistoryBits)
+        .map {
+          case (bhtNEntries, bhtCounterLength, bhtHistoryLength, bhtHistoryBits) =>
+            BHTParameter(bhtNEntries, bhtCounterLength, bhtHistoryLength, bhtHistoryBits)
+        }
+        .headOption,
+      legal.foldLeft(BitSet.empty)(_.union(_)),
+      cacheable.foldLeft(BitSet.empty)(_.union(_)),
+      read.foldLeft(BitSet.empty)(_.union(_)),
+      write.foldLeft(BitSet.empty)(_.union(_)),
+      putPartial.foldLeft(BitSet.empty)(_.union(_)),
+      logic.foldLeft(BitSet.empty)(_.union(_)),
+      arithmetic.foldLeft(BitSet.empty)(_.union(_)),
+      exec.foldLeft(BitSet.empty)(_.union(_)),
+      sideEffects.foldLeft(BitSet.empty)(_.union(_))
+    )
+  }
+
+  implicit def FrontendParameterMainParser: ParserForClass[FrontendParameterMain] =
+    ParserForClass[FrontendParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: FrontendParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[Frontend, FrontendParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
