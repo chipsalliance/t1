0551461c4512fb958c515c8ca26c108ce967b07c
Jiuyang Liu <liu@jiuyang.me>
2024-07-02 19:51:20 +0800

    fix ICache

--- elaborator/src/rocketv/ICache.scala
+++ elaborator/src/rocketv/ICache.scala
@@ -9,10 +9,24 @@ import org.chipsalliance.t1.elaborator.Elaborator
 object ICache extends Elaborator {
   @main
   case class ICacheParameterMain(
-                               @arg(name = "vaddrBits") vaddrBits: Int,
-                               @arg(name = "paddrBits") paddrBits: Int,
-                             ) {
-    def convert: ICacheParameter = ICacheParameter(vaddrBits, paddrBits)
+    @arg(name = "useAsyncReset") useAsyncReset: Boolean,
+    @arg(name = "prefetch") prefetch:           Boolean,
+    @arg(name = "nSets") nSets:                 Int,
+    @arg(name = "nWays") nWays:                 Int,
+    @arg(name = "blockBytes") blockBytes:       Int,
+    @arg(name = "usingVM") usingVM:             Boolean,
+    @arg(name = "vaddrBits") vaddrBits:         Int,
+    @arg(name = "paddrBits") paddrBits:         Int) {
+    def convert: ICacheParameter = ICacheParameter(
+      useAsyncReset,
+      prefetch,
+      nSets,
+      nWays,
+      blockBytes,
+      usingVM,
+      vaddrBits,
+      paddrBits
+    )
   }
 
   implicit def ICacheParameterMainParser: ParserForClass[ICacheParameterMain] = ParserForClass[ICacheParameterMain]
