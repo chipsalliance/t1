b7e697dd3b35f8439ea5bb432891a1c7603e75cc
Jiuyang Liu <liu@jiuyang.me>
2024-07-03 16:34:12 +0800

    [rocketv] add elaborator for Rocket
    - generate parameter json: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.Rocket config --useAsyncReset true --clockGate true --instructionSets rv32_i --vLen 0 --usingUser false --hartIdLen 1 --nPMPs 8 --asidBits 0 --nBreakpoints 1 --usingBTB false --useBPWatch false --mcontextWidth 0 --scontextWidth 0 --mulDivLantency 2 --divUnroll 1 --divEarlyOut false --divEarlyOutGranularity 1 --mulUnroll 1 --mulEarlyOut false --paddrBits 32 --cacheBlockBytes 32 --hasBeu false --fenceIFlushDCache false --fastLoadByte false --fastLoadWord false --dcacheNSets 64 --flushOnFenceI false
    - generate verilog: mill elaborator.runMain org.chipsalliance.t1.elaborator.rocketv.Rocket design --parameter ./Rocket.json --run-firtool

---
+++ elaborator/src/rocketv/Rocket.scala
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.rocketv.{Rocket, RocketParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object Rocket extends Elaborator {
+  @main
+  case class RocketParameterMain(
+    @arg(name = "useAsyncReset") useAsyncReset:                   Boolean,
+    @arg(name = "clockGate") clockGate:                           Boolean,
+    @arg(name = "instructionSets") instructionSets:               Set[String],
+    @arg(name = "vLen") vLen:                                     Int,
+    @arg(name = "usingUser") usingUser:                           Boolean,
+    @arg(name = "hartIdLen") hartIdLen:                           Int,
+    @arg(name = "nPMPs") nPMPs:                                   Int,
+    @arg(name = "asidBits") asidBits:                             Int,
+    @arg(name = "nBreakpoints") nBreakpoints:                     Int,
+    @arg(name = "usingBTB") usingBTB:                             Boolean,
+    @arg(name = "useBPWatch") useBPWatch:                         Boolean,
+    @arg(name = "mcontextWidth") mcontextWidth:                   Int,
+    @arg(name = "scontextWidth") scontextWidth:                   Int,
+    @arg(name = "mulDivLantency") mulDivLantency:                 Int,
+    @arg(name = "divUnroll") divUnroll:                           Int,
+    @arg(name = "divEarlyOut") divEarlyOut:                       Boolean,
+    @arg(name = "divEarlyOutGranularity") divEarlyOutGranularity: Int,
+    @arg(name = "mulUnroll") mulUnroll:                           Int,
+    @arg(name = "mulEarlyOut") mulEarlyOut:                       Boolean,
+    @arg(name = "paddrBits") paddrBits:                           Int,
+    @arg(name = "cacheBlockBytes") cacheBlockBytes:               Int,
+    @arg(name = "hasBeu") hasBeu:                                 Boolean,
+    @arg(name = "fastLoadByte") fastLoadByte:                     Boolean,
+    @arg(name = "fastLoadWord") fastLoadWord:                     Boolean,
+    @arg(name = "dcacheNSets") dcacheNSets:                       Int,
+    @arg(name = "flushOnFenceI") flushOnFenceI:                   Boolean) {
+    def convert: RocketParameter = RocketParameter(
+      useAsyncReset,
+      clockGate,
+      instructionSets,
+      vLen,
+      usingUser,
+      hartIdLen,
+      nPMPs,
+      asidBits,
+      nBreakpoints,
+      usingBTB,
+      useBPWatch,
+      mcontextWidth,
+      scontextWidth,
+      mulDivLantency,
+      divUnroll,
+      divEarlyOut,
+      divEarlyOutGranularity,
+      mulUnroll,
+      mulEarlyOut,
+      paddrBits,
+      cacheBlockBytes,
+      hasBeu,
+      fastLoadByte,
+      fastLoadWord,
+      dcacheNSets,
+      flushOnFenceI
+    )
+  }
+
+  implicit def RocketParameterMainParser: ParserForClass[RocketParameterMain] = ParserForClass[RocketParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: RocketParameterMain) = configImpl(parameter.convert)
+
+  @main
+  def design(@arg(name = "parameter") parameter: os.Path, @arg(name = "run-firtool") runFirtool: mainargs.Flag) =
+    designImpl[Rocket, RocketParameter](parameter, runFirtool.value)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
