e8cb067493d5a12cfb7ee850a52d529a31e75b6e
Jiuyang Liu <liu@jiuyang.me>
2024-07-04 12:35:22 +0800

    [ipemu] fix dumpwave

--- ipemu/src/DumpWave.scala
+++ ipemu/src/DumpWave.scala
@@ -10,6 +10,7 @@ class DumpWave extends ExtModule with HasExtModuleInline {
   setInline(
     s"DumpWave.sv",
     s"""module DumpWave;
+       |export "DPI-C" function DumpWave;
        |function DumpWave(input string file);
        |$$dumpfile(file);
        |$$dumpvars(0);
