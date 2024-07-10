2e5601ccd807c3cbdda3dc8b1094ee1905f9a404
Avimitin <dev@avimit.in>
2024-07-10 17:08:28 +0800

    [dpi] expose AXI4 agent as C-API
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ rocketemu/dpi/dpi_pre_link.h
@@ -0,0 +1,22 @@
+// This header provides seveal functions to be used in Rust
+//
+// dpi_pre_link should be linked before libverilated.so because in
+// uses symbols in libverilated.so
+
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int verilator_main_c(int argc, char **argv);
+
+void dump_wave_c(char *path);
+
+uint64_t get_t_c();
+
+#ifdef __cplusplus
+}
+#endif
