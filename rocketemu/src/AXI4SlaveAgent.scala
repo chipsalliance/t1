724b69896d30fc61316db38ed050809d9f4b8325
Avimitin <dev@avimit.in>
2024-07-12 16:23:19 +0800

    [rocketemu] update TestBench implementation
    This commit keep AXI agent implementation in sync with the axi4-t1
    branch, with ClockGen and DumpWave module embedded inside the TestBench
    module.
    
    Signed-off-by: Avimitin <dev@avimit.in>

--- rocketemu/src/AXI4SlaveAgent.scala
+++ rocketemu/src/AXI4SlaveAgent.scala
@@ -5,48 +5,32 @@ package org.chipsalliance.t1.rocketv.dpi
 
 // TODO: upstream to AMBA as VIP
 import chisel3._
-import chisel3.util.circt.dpi.{RawClockedNonVoidFunctionCall, RawClockedVoidFunctionCall}
-import chisel3.util.{scanLeftOr, OHToUInt, Reverse, Valid}
-import org.chipsalliance.amba.axi4.bundle.{
-  ARChannel,
-  ARFlowControl,
-  AWChannel,
-  AWFlowControl,
-  AXI4BundleParameter,
-  AXI4ROIrrevocableVerilog,
-  AXI4RWIrrevocableVerilog,
-  AXI4WOIrrevocableVerilog,
-  BChannel,
-  BFlowControl,
-  RChannel,
-  RFlowControl,
-  WChannel,
-  WFlowControl
-}
+import chisel3.util.circt.dpi.{RawClockedVoidFunctionCall, RawUnclockedNonVoidFunctionCall}
+import chisel3.util.{OHToUInt, scanLeftOr}
+import org.chipsalliance.amba.axi4.bundle.{ARChannel, ARFlowControl, AWChannel, AWFlowControl, AXI4BundleParameter, AXI4ROIrrevocableVerilog, AXI4RWIrrevocableVerilog, AXI4WOIrrevocableVerilog, BChannel, BFlowControl, RChannel, RFlowControl, WChannel, WFlowControl}
 
-case class AXI4SlaveAgentParameter(name: String, axiParameter: AXI4BundleParameter, outstanding: Int)
+case class AXI4SlaveAgentParameter(name: String, axiParameter: AXI4BundleParameter, outstanding: Int, readPayloadSize: Int, writePayloadSize: Int)
 
 class AXI4SlaveAgentInterface(parameter: AXI4SlaveAgentParameter) extends Bundle {
   val clock:     Clock = Input(Clock())
   val reset:     Reset = Input(Reset())
-  val channelId: UInt = Input(Const(UInt(64.W)))
+  val channelId: UInt =  Input(Const(UInt(64.W)))
+  // don't issue read DPI
+  val gateRead: Bool = Input(Bool())
+  // don't issue write DPI
+  val gateWrite: Bool = Input(Bool())
   val channel = Flipped(
     org.chipsalliance.amba.axi4.bundle.verilog.irrevocable(parameter.axiParameter)
   )
 }
 
-class WritePayload(dataWidth: Int) extends Bundle {
-  val data = Vec(256, UInt(dataWidth.W))
-  val strb = Vec(256, UInt((dataWidth / 8).W))
+class WritePayload(length: Int, dataWidth: Int) extends Bundle {
+  val data = Vec(length, UInt(dataWidth.W))
+  val strb = Vec(length, UInt((dataWidth / 8).W))
 }
 
-class ReadPayload(dataWidth: Int) extends Bundle {
-  require(
-    Seq(8, 16, 32, 64, 128, 256, 512, 1024).contains(dataWidth),
-    "A1.2.1: The data bus, which can be 8, 16, 32, 64, 128, 256, 512, or 1024 bits wide. A read response signal indicating the completion status of the read transaction."
-  )
-  val data = Vec(256, UInt(dataWidth.W))
-  val beats = UInt(8.W)
+class ReadPayload(length: Int,dataWidth: Int) extends Bundle {
+  val data = Vec(length, UInt(dataWidth.W))
 }
 
 // consume transaction from DPI, drive RTL signal
@@ -66,69 +50,87 @@ class AXI4SlaveAgent(parameter: AXI4SlaveAgentParameter)
   private class WriteManager(
     channel: AWChannel with AWFlowControl with WChannel with WFlowControl with BChannel with BFlowControl) {
     withClockAndReset(io.clock, io.reset) {
-
-      /** There is an valid write transaction. */
-      val valid = RegInit(0.U.asTypeOf(Bool()))
+      /** indicate AW is issued. */
+      val awIssued = RegInit(0.U.asTypeOf(Bool()))
+      /** indicate W is finished, used to wake up B channel. */
+      val last = RegInit(0.U.asTypeOf(Bool()))
+      /** indicate there is an ongoing write transaction. */
+      val busy = RegInit(0.U.asTypeOf(Bool()))
 
       /** memory to store the write payload
         * @todo limit the payload size based on the RTL configuration.
         */
-      val writePayload = RegInit(0.U.asTypeOf(new WritePayload(parameter.axiParameter.dataWidth)))
-
+      val writePayload = RegInit(0.U.asTypeOf(new WritePayload(parameter.writePayloadSize, parameter.axiParameter.dataWidth)))
       /** AWID, latch at AW fire, used at B fire. */
-      val writeId = RegInit(0.U(16.W))
+      val awid = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWID)))
+      val awaddr = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWADDR)))
+      val awlen = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWLEN)))
+      val awsize = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWSIZE)))
+      val awburst = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWBURST)))
+      val awlock = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWLOCK)))
+      val awcache = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWCACHE)))
+      val awprot = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWPROT)))
+      val awqos = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWQOS)))
+      val awregion = RegInit(0.U.asTypeOf(chiselTypeOf(channel.AWREGION)))
 
       /** index the payload, used to write [[writePayload]] */
       val writeIdx = RegInit(0.U.asTypeOf(UInt(8.W)))
 
-      /** indicate W is finished, used to wake up B channel. */
-      val last = RegInit(0.U.asTypeOf(Bool()))
-
       // AW
-      channel.AWREADY := !valid
+      channel.AWREADY := !busy || (busy && !awIssued)
       when(channel.AWREADY && channel.AWVALID) {
-        assert(valid === false.B)
-        writeId := channel.AWID
-        valid := true.B
-        writeIdx := 0.U
+        awIssued := true.B
+        busy := true.B
+        awid := channel.AWID
+        awaddr := channel.AWADDR
+        awlen := channel.AWLEN
+        awsize := channel.AWSIZE
+        awburst := channel.AWBURST
+        awlock := channel.AWLOCK
+        awcache := channel.AWCACHE
+        awprot := channel.AWPROT
+        awqos := channel.AWQOS
+        awregion := channel.AWREGION
       }
 
       // W
-      channel.WREADY := true.B
+      channel.WREADY := !busy || (busy && !last)
       when(channel.WVALID && channel.WREADY) {
+        busy := true.B
         writePayload.data(writeIdx) := channel.WDATA
         writePayload.strb(writeIdx) := channel.WSTRB
         writeIdx := writeIdx + 1.U
         when(channel.WLAST) {
           last := true.B
-          RawClockedVoidFunctionCall(s"axi_write_${parameter.name}")(
-            io.clock,
-            when.cond,
-            io.channelId,
-            channel.AWID.asTypeOf(UInt(64.W)),
-            channel.AWADDR.asTypeOf(UInt(64.W)),
-            channel.AWLEN.asTypeOf(UInt(64.W)),
-            channel.AWSIZE.asTypeOf(UInt(64.W)),
-            channel.AWBURST.asTypeOf(UInt(64.W)),
-            channel.AWLOCK.asTypeOf(UInt(64.W)),
-            channel.AWCACHE.asTypeOf(UInt(64.W)),
-            channel.AWPROT.asTypeOf(UInt(64.W)),
-            channel.AWQOS.asTypeOf(UInt(64.W)),
-            channel.AWREGION.asTypeOf(UInt(64.W)),
-            WireDefault(writePayload)
-          )
         }
       }
 
       // B
-      channel.BVALID := last
-      channel.BID := writeId
+      channel.BVALID := last && awIssued
+      channel.BID := awid
       channel.BRESP := 0.U(2.W) // OK
       channel.BUSER := DontCare
       when(channel.BVALID && channel.BREADY) {
-        assert(valid === true.B)
-        valid := false.B
+        RawClockedVoidFunctionCall(s"axi_write_${parameter.name}")(
+          io.clock,
+          when.cond && !io.gateWrite,
+          io.channelId,
+          // handle AW and W at same beat.
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWID, awid.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWADDR, awaddr.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWLEN, awlen.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWSIZE, awsize.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWBURST, awburst.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWLOCK, awlock.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWCACHE, awcache.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWPROT, awprot.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWQOS, awqos.asTypeOf(UInt(64.W))),
+          Mux(channel.AWREADY && channel.AWVALID, channel.AWREGION, awregion.asTypeOf(UInt(64.W))),
+          WireDefault(writePayload)
+        )
+        awIssued := false.B
         last := false.B
+        writeIdx := 0.U
       }
     }
   }
@@ -137,43 +139,29 @@ class AXI4SlaveAgent(parameter: AXI4SlaveAgentParameter)
     withClockAndReset(io.clock, io.reset) {
       class CAMValue extends Bundle {
         val arid = UInt(16.W)
-        val readPayload = new ReadPayload(parameter.axiParameter.dataWidth)
+        val arlen = UInt(8.W)
+        val readPayload = new ReadPayload(parameter.readPayloadSize, parameter.axiParameter.dataWidth)
         val readPayloadIndex = UInt(8.W)
         val valid = Bool()
       }
-
       /** CAM to maintain order of read requests. This is maintained as FIFO. */
       val cam: Vec[CAMValue] = RegInit(0.U.asTypeOf(Vec(parameter.outstanding, new CAMValue)))
-
       /** find first one circuit. */
       def ffo(input: UInt): UInt = ((~(scanLeftOr(input) << 1)).asUInt & input)(input.getWidth - 1, 0)
-
       /** find first non-valid slot in [[cam]] */
       val firstEmpty: UInt = OHToUInt(ffo(VecInit(cam.map(!_.valid)).asUInt))
-
       /** there are no outstanding read requests. */
       val camIsEmpty = VecInit(cam.map(content => !content.valid)).asUInt.andR
-
-      /** find oldest read. */
+      /** find oldest to index which cam to use. */
       val oldest = OHToUInt(ffo(VecInit(cam.map(content => content.valid)).asUInt))
 
-      /** index to select value from [[cam]]
-        * if cam empty, always select the next allocate value.
-        * if cam non-empty, update to oldest at each transaction end, this can be changed to random response with LFSR.
-        * @todo in the future, we can provide a fine-grand control to this index to provide out-of-order return.
-        */
-      val rIndex = RegInit(0.U.asTypeOf(UInt(16.W)))
-
       // AR
-      channel.ARREADY := VecInit(cam.map(!_.valid)).asUInt.andR
+      channel.ARREADY := VecInit(cam.map(!_.valid)).asUInt.orR
       when(channel.ARREADY && channel.ARVALID) {
         cam(firstEmpty).arid := channel.ARID
-        cam(firstEmpty).readPayload := RawClockedNonVoidFunctionCall(
-          s"axi_read_${parameter.name}",
-          new ReadPayload(parameter.axiParameter.dataWidth)
-        )(
-          io.clock,
-          when.cond,
+        cam(firstEmpty).arlen := channel.ARLEN
+        cam(firstEmpty).readPayload := RawUnclockedNonVoidFunctionCall(s"axi_read_${parameter.name}", new ReadPayload(parameter.readPayloadSize, parameter.axiParameter.dataWidth))(
+          when.cond && !io.gateRead,
           io.channelId,
           channel.ARID.asTypeOf(UInt(64.W)),
           channel.ARADDR.asTypeOf(UInt(64.W)),
@@ -191,27 +179,17 @@ class AXI4SlaveAgent(parameter: AXI4SlaveAgentParameter)
       }
 
       // R
-      rIndex := Mux(
-        camIsEmpty,
-        firstEmpty, // if cam empty, always select the next allocate value.
-        Mux(
-          channel.RREADY && channel.RVALID && channel.RLAST,
-          oldest, // if cam non-empty, update to oldest at each transaction end, this can be changed to random response with LFSR.
-          rIndex
-        )
-      )
-
       channel.RVALID := VecInit(cam.map(_.valid)).asUInt.orR
-      channel.RID := cam(rIndex).arid
-      channel.RDATA := cam(rIndex).readPayload.data(cam(rIndex).readPayloadIndex)
+      channel.RID := cam(oldest).arid
+      channel.RDATA := cam(oldest).readPayload.data(cam(oldest).readPayloadIndex)
       channel.RRESP := 0.U // OK
-      channel.RLAST := cam(rIndex).readPayload.beats === cam(rIndex).readPayloadIndex
+      channel.RLAST := (cam(oldest).arlen === cam(oldest).readPayloadIndex) && cam(oldest).valid
       channel.RUSER := DontCare
       when(channel.RREADY && channel.RVALID) {
         // increase index
-        cam(rIndex).readPayloadIndex := cam(rIndex).readPayloadIndex + 1.U
+        cam(oldest).readPayloadIndex := cam(oldest).readPayloadIndex + 1.U
         when(channel.RLAST) {
-          cam(rIndex).valid := false.B
+          cam(oldest).valid := false.B
         }
       }
     }
