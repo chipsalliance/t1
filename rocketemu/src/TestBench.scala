9d8ec5e1e689362c17dd727af444069a36d4cfc4
Avimitin <dev@avimit.in>
2024-07-12 16:23:19 +0800

    [rocketemu] update TestBench implementation
    This commit keep AXI agent implementation in sync with the axi4-t1
    branch, with ClockGen and DumpWave module embedded inside the TestBench
    module.
    
    Signed-off-by: Avimitin <dev@avimit.in>

--- rocketemu/src/TestBench.scala
+++ rocketemu/src/TestBench.scala
@@ -4,29 +4,52 @@
 package org.chipsalliance.t1.rocketv
 
 import chisel3._
-import chisel3.experimental.SerializableModuleGenerator
+import chisel3.experimental.{ExtModule, SerializableModuleGenerator}
 import chisel3.experimental.dataview.DataViewable
-import chisel3.util.circt.dpi.{
-  RawClockedNonVoidFunctionCall,
-  RawClockedVoidFunctionCall,
-  RawUnlockedNonVoidFunctionCall
-}
+import chisel3.util.{log2Ceil, HasExtModuleInline, PopCount, UIntToOH, Valid}
 import org.chipsalliance.amba.axi4.bundle._
 import org.chipsalliance.t1.rocketv.dpi._
-import org.chipsalliance.rocketv.{Frontend, RocketTile, RocketTileParameter}
+import org.chipsalliance.rocketv.{RocketTile, RocketTileParameter}
 
-class TestBench(generator: SerializableModuleGenerator[RocketTile, RocketTileParameter]) extends RawModule with ImplicitClock with ImplicitReset {
-  val clockGen = Module(new ClockGen)
-  Module(new DumpWave)
-
+class TestBench(generator: SerializableModuleGenerator[RocketTile, RocketTileParameter])
+    extends RawModule
+    with ImplicitClock
+    with ImplicitReset {
   override protected def implicitClock: Clock = clockGen.clock.asClock
   override protected def implicitReset: Reset = clockGen.reset
 
-  val clock: Clock = clockGen.clock.asClock
-  val reset: Bool = clockGen.reset
+  val clockGen = Module(new ExtModule with HasExtModuleInline {
+    override def desiredName = "ClockGen"
+    setInline(
+      s"$desiredName.sv",
+      s"""module $desiredName(output reg clock, output reg reset);
+         |  export "DPI-C" function dump_wave;
+         |  function dump_wave(input string file);
+         |    $$dumpfile(file);
+         |    $$dumpvars(0);
+         |  endfunction;
+         |
+         |  import "DPI-C" function void cosim_init();
+         |  initial begin
+         |    cosim_init();
+         |    clock = 1'b0;
+         |    reset = 1'b1;
+         |  end
+         |  initial #(11) reset = 1'b0;
+         |  always #10 clock = ~clock;
+         |endmodule
+         |""".stripMargin
+    )
+    val clock = IO(Output(Bool()))
+    val reset = IO(Output(Bool()))
+  })
+
+  val clock:          Clock = clockGen.clock.asClock
+  val reset:          Bool = clockGen.reset
+  val simulationTime: UInt = withClockAndReset(clock, reset)(RegInit(0.U(64.W)))
+  simulationTime := simulationTime + 1.U
 
   val dut: RocketTile = withClockAndReset(clock, reset)(Module(generator.module()))
-
   dut.io.clock := clockGen.clock.asClock
   dut.io.reset := clockGen.reset
   dut.io.hartid := 0.U
@@ -38,25 +61,6 @@ class TestBench(generator: SerializableModuleGenerator[RocketTile, RocketTilePar
 
   dut.io.resetVector := 10000.U
 
-  val simulationTime = withClockAndReset(clock, reset)(RegInit(0.U(64.W)))
-  simulationTime := simulationTime + 1.U
-
-  // simulation env
-  withClockAndReset(clock, reset) {
-    // TODO: this initial way cannot happen before reset...
-    val initFlag = RegInit(true.B)
-    val callInit = RawUnlockedNonVoidFunctionCall("cosim_init", Bool())(initFlag).asInstanceOf[Bool]
-    when(callInit) {
-      initFlag := false.B
-      printf(cf"""{"event":"simulationStart","parameter":{"cycle": ${simulationTime}}}\n""")
-    }
-    val watchdog =
-      RawUnlockedNonVoidFunctionCall("cosim_watchdog", UInt(8.W))(simulationTime(9, 0) === 0.U).asInstanceOf[UInt]
-    when(watchdog =/= 0.U) {
-      stop(cf"""{"event":"simulationStop","parameter":{"reason": ${watchdog},"cycle": ${simulationTime}}}\n""")
-    }
-  }
-
   // Memory Drivers
   val instFetchAXI = dut.io.instructionFetchAXI.viewAs[AXI4ROIrrevocableVerilog]
   val instFetchAgent = Module(
@@ -64,7 +68,9 @@ class TestBench(generator: SerializableModuleGenerator[RocketTile, RocketTilePar
       AXI4SlaveAgentParameter(
         name = "instructionFetchAXI",
         axiParameter = instFetchAXI.parameter,
-        outstanding = 4
+        outstanding = 4,
+        readPayloadSize = 1,
+        writePayloadSize = 1
       )
     ).suggestName("axi4_channel0_instructionFetchAXI")
   )
@@ -74,11 +80,19 @@ class TestBench(generator: SerializableModuleGenerator[RocketTile, RocketTilePar
   instFetchAgent.io.clock := clock
   instFetchAgent.io.reset := reset
   instFetchAgent.io.channelId := 0.U
+  instFetchAgent.io.gateRead := false.B
+  instFetchAgent.io.gateWrite := true.B
 
   val loadStoreAXI = dut.io.loadStoreAXI.viewAs[AXI4RWIrrevocableVerilog]
   val loadStoreAgent = Module(
     new AXI4SlaveAgent(
-      AXI4SlaveAgentParameter(name = "loadStoreAXI", axiParameter = loadStoreAXI.parameter, outstanding = 4)
+      AXI4SlaveAgentParameter(
+        name = "loadStoreAXI",
+        axiParameter = loadStoreAXI.parameter,
+        outstanding = 4,
+        readPayloadSize = 1,
+        writePayloadSize = 1
+      )
     ).suggestName("axi4_channel1_loadStoreAXI")
   )
   loadStoreAgent.io.channel match {
@@ -86,5 +100,7 @@ class TestBench(generator: SerializableModuleGenerator[RocketTile, RocketTilePar
   }
   loadStoreAgent.io.clock := clock
   loadStoreAgent.io.reset := reset
-  loadStoreAgent.io.channelId := 1.U
+  loadStoreAgent.io.channelId := 0.U
+  loadStoreAgent.io.gateRead := false.B
+  loadStoreAgent.io.gateWrite := true.B
 }
