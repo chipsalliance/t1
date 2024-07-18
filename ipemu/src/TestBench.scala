1d646871779bf5dc64a24e2a3d649f27a0441644
Jiuyang Liu <liu@jiuyang.me>
2024-07-18 22:25:38 +0800

    [ipemu] rewrite T1 testbench with DPI

--- ipemu/src/TestBench.scala
+++ ipemu/src/TestBench.scala
@@ -4,9 +4,13 @@
 package org.chipsalliance.t1.ipemu
 
 import chisel3._
-import chisel3.experimental.SerializableModuleGenerator
+import chisel3.experimental.dataview.DataViewable
 import chisel3.experimental.hierarchy.{Instance, Instantiate, instantiable, public}
+import chisel3.experimental.{ExtModule, SerializableModuleGenerator}
 import chisel3.properties.{AnyClassType, Class, ClassType, Property}
+import chisel3.util.circt.dpi.{RawClockedNonVoidFunctionCall, RawClockedVoidFunctionCall, RawUnclockedNonVoidFunctionCall}
+import chisel3.util.{HasExtModuleInline, PopCount, UIntToOH, Valid}
+import org.chipsalliance.amba.axi4.bundle._
 import org.chipsalliance.t1.ipemu.dpi._
 import org.chipsalliance.t1.rtl.{T1, T1Parameter}
 
@@ -26,36 +30,156 @@ class TestBench(generator: SerializableModuleGenerator[T1, T1Parameter]) extends
   val om: Property[ClassType] = IO(Output(Property[omType.Type]()))
   om := omInstance.getPropertyReference
 
-  // Scheduler to schedule different DPI calls for online difftest,
-  // TODO: after switching to offline version, everything should be cleaned up.
-  val clockRate = 5
-  val latPokeInst = 1
-  val negLatPeekWriteQueue = 1
-  val latPeekLsuEnq = 1
-  val latPeekIssue = 2
-  val latPeekTL = 2
-  val latPokeTL = 1
-
-  val clockGen = Module(new ClockGen(ClockGenParameter(clockRate)))
-  val dpiDumpWave = Module(new DpiDumpWave)
-  val dpiFinish = Module(new DpiFinish)
-  val dpiError = Module(new DpiError)
-  val dpiInit = Module(new DpiInitCosim)
-  val dpiTimeoutCheck = Module(new TimeoutCheck(TimeoutCheckParameter(clockRate)))
+  val clockGen = Module(new ExtModule with HasExtModuleInline {
+    override def desiredName = "ClockGen"
+    setInline(s"$desiredName.sv",
+      s"""module $desiredName(output reg clock, output reg reset);
+         |  export "DPI-C" function dump_wave;
+         |  function dump_wave(input string file);
+         |    $$dumpfile(file);
+         |    $$dumpvars(0);
+         |  endfunction;
+         |
+         |  import "DPI-C" function void cosim_init();
+         |  initial begin
+         |    cosim_init();
+         |    clock = 1'b0;
+         |    reset = 1'b1;
+         |  end
+         |  initial #(11) reset = 1'b0;
+         |  always #10 clock = ~clock;
+         |endmodule
+         |""".stripMargin
+    )
+    val clock = IO(Output(Bool()))
+    val reset = IO(Output(Bool()))
+  })
 
   val clock: Clock = clockGen.clock.asClock
   val reset: Bool = clockGen.reset
+  val simulationTime: UInt = withClockAndReset(clock, reset)(RegInit(0.U(64.W)))
+  simulationTime := simulationTime + 1.U
 
   val dut: T1 = withClockAndReset(clock, reset)(Module(generator.module()))
-  dut.storeBufferClear := true.B
   omInstance.t1In := Property(dut.om.asAnyClassType)
+  // Instruction Drivers
+  withClockAndReset(clock, reset) {
+
+    // TODO: this initial way cannot happen before reset...
+    val initFlag = RegInit(false.B)
+    when(!initFlag) {
+      initFlag := true.B
+      printf(cf"""{"event":"SimulationStart","cycle":${simulationTime}}\n""")
+    }
+    val watchdog = RawUnclockedNonVoidFunctionCall("cosim_watchdog", UInt(8.W))(simulationTime(9, 0) === 0.U)
+    when(watchdog =/= 0.U) {
+      stop(cf"""{"event":"SimulationStop","reason": ${watchdog},"cycle":${simulationTime}}\n""")
+    }
+
+    // uint32_t -> svBitVecVal -> reference type with 7 length.
+    class Issue extends Bundle {
+      val instruction: UInt = UInt(32.W)
+      val src1Data: UInt = UInt(32.W)
+      val src2Data: UInt = UInt(32.W)
+      // mstatus, vstatus?
+      val vtype: UInt = UInt(32.W)
+      val vl: UInt = UInt(32.W)
+      // vlenb
+      val vstart: UInt = UInt(32.W)
+      // vxrm, vxsat are merged to vcsr
+      val vcsr: UInt = UInt(32.W)
+      // meta is used to control the simulation.
+      // 0 is reserved, aka not valid
+      // 1 is normal, it's a valid instruction
+      // 2 is fence, it will request
+      // others are exit, will end the simulation immediately
+      val meta: UInt = UInt(32.W)
+    }
+    class Retire extends Bundle {
+      val rd: UInt = UInt(32.W)
+      val data: UInt = UInt(32.W)
+      val writeRd: UInt = UInt(32.W)
+      val vxsat: UInt = UInt(32.W)
+    }
+    val issue = WireDefault(0.U.asTypeOf(new Issue))
+    val fence = RegInit(false.B)
+    val outstanding = RegInit(0.U(4.W))
+    val doIssue: Bool = dut.request.ready && !fence
+    outstanding := outstanding + (doIssue && (issue.meta === 1.U)) - dut.response.valid
+    fence := Mux(doIssue, issue.meta === 2.U, fence && !dut.response.valid && !(outstanding === 0.U))
+
+    issue := RawClockedNonVoidFunctionCall("issue_vector_instruction", new Issue)(
+      clock,
+      doIssue,
+    )
+    dut.request.bits.instruction := issue.instruction
+    dut.request.bits.src1Data := issue.src1Data
+    dut.request.bits.src2Data := issue.src2Data
+    dut.csrInterface.vlmul := issue.vtype(2, 0)
+    dut.csrInterface.vSew := issue.vtype(5, 3)
+    dut.csrInterface.vta := issue.vtype(6)
+    dut.csrInterface.vma := issue.vtype(7)
+    dut.csrInterface.vl := issue.vl
+    dut.csrInterface.vStart := issue.vstart
+    dut.csrInterface.vxrm := issue.vcsr(2, 1)
+
+    dut.csrInterface.ignoreException := 0.U
+    dut.storeBufferClear := true.B
+    dut.request.valid := issue.meta === 1.U
+    when(issue.meta =/= 0.U && issue.meta =/= 1.U && issue.meta =/= 2.U) {
+      stop(cf"""{"event":"SimulationStop","reason": ${issue.meta},"cycle":${simulationTime}}\n""")
+    }
+    val retire = Wire(new Retire)
+    retire.rd := dut.response.bits.rd.bits
+    retire.data := dut.response.bits.data
+    retire.writeRd := dut.response.bits.rd.valid
+    retire.vxsat := dut.response.bits.vxsat
+    RawClockedVoidFunctionCall("retire_vector_instruction")(clock, dut.response.valid, retire)
+    val dummy = Wire(Bool())
+    dummy := false.B
+    RawClockedVoidFunctionCall("retire_vector_mem")(clock, dut.response.bits.mem && dut.response.valid, dummy)
+  }
+
+  // Memory Drivers
+  Seq(
+    dut.highBandwidthLoadStorePort,
+    dut.indexedLoadStorePort
+  ).map(_.viewAs[AXI4RWIrrevocableVerilog]).lazyZip(
+    Seq("highBandwidthPort", "indexedAccessPort")
+  ).zipWithIndex.foreach {
+    case ((bundle: AXI4RWIrrevocableVerilog, channelName: String), index: Int) =>
+      val agent = Module(new AXI4SlaveAgent(
+        AXI4SlaveAgentParameter(
+          name= channelName,
+          axiParameter = bundle.parameter,
+          outstanding = 4,
+          readPayloadSize = 1,
+          writePayloadSize = 1,
+        )
+      )).suggestName(s"axi4_channel${index}_${channelName}")
+      agent.io.channel match {
+        case io: AXI4RWIrrevocableVerilog => io <> bundle
+      }
+      agent.io.clock := clock
+      agent.io.reset := reset
+      agent.io.channelId := index.U
+      agent.io.gateRead := false.B
+      agent.io.gateWrite := false.B
+  }
+
+  // Events for difftest and performance modeling
 
   val laneProbes = dut.laneProbes.zipWithIndex.map{case (p, idx) =>
     val wire = Wire(p.cloneType).suggestName(s"lane${idx}Probe")
     wire := probe.read(p)
+    wire
   }
 
   val lsuProbe = probe.read(dut.lsuProbe).suggestName("lsuProbe")
+
+  val storeUnitProbe = lsuProbe.storeUnitProbe.suggestName("storeUnitProbe")
+
+  val otherUnitProbe = lsuProbe.otherUnitProbe.suggestName("otherUnitProbe")
 
   val laneVrfProbes = dut.laneVrfProbes.zipWithIndex.map{ case (p, idx) =>
     val wire = Wire(p.cloneType).suggestName(s"lane${idx}VrfProbe")
@@ -63,135 +187,53 @@ class TestBench(generator: SerializableModuleGenerator[T1, T1Parameter]) extends
     wire
   }
 
-  val storeUnitProbe = lsuProbe.storeUnitProbe.suggestName("storeUnitProbe")
-  val otherUnitProbe = lsuProbe.otherUnitProbe.suggestName("otherUnitProbe")
-
   val t1Probe = probe.read(dut.t1Probe)
 
   withClockAndReset(clock, reset) {
-    // count cycle for peek tl
-    val cycleCounter = RegInit(0.U(64.W))
-    cycleCounter := cycleCounter + 1.U
-
-    // vrf write from lsu
-    lsuProbe.slots.zipWithIndex.foreach { case (mshr, i) => when(mshr.writeValid)(printf(cf"""{"event":"vrfWriteFromLsu","parameter":{"idx":$i,"vd":${mshr.dataVd},"offset":${mshr.dataOffset},"mask":"${mshr.dataMask}","data":"${mshr.dataData}","instruction":${mshr.dataInstruction},"lane":${mshr.targetLane},"cycle": ${cycleCounter}}}\n""")) }
-    // vrf write from lane
-    laneVrfProbes.zipWithIndex.foreach { case (lane, i) => when(lane.valid)(printf(cf"""{"event":"vrfWriteFromLane","parameter":{"idx":$i,"vd":${lane.requestVd},"offset":${lane.requestOffset},"mask":"${lane.requestMask}","data":"${lane.requestData}","instruction":${lane.requestInstruction},"cycle": ${cycleCounter}}}\n""")) }
+    // vrf write
+    laneVrfProbes.zipWithIndex.foreach { case (lane, i) => when(lane.valid)(printf(cf"""{"event":"VrfWrite","issue_idx":${lane.requestInstruction},"vd":${lane.requestVd},"offset":${lane.requestOffset},"mask":"${lane.requestMask}%x","data":"${lane.requestData}%x","lane":$i,"cycle":${simulationTime}}\n""")) }
     // memory write from store unit
-    when(storeUnitProbe.valid)(printf(cf"""{"event":"memoryWrite","parameter":{"mask":"${storeUnitProbe.mask}","data":"${storeUnitProbe.data}","address":${storeUnitProbe.address},"source":${storeUnitProbe.index},"cycle": ${cycleCounter}}}\n""")) 
+    when(storeUnitProbe.valid)(printf(cf"""{"event":"MemoryWrite","lsu_idx":${storeUnitProbe.index},"mask":"${storeUnitProbe.mask}%x","data":"${storeUnitProbe.data}%x","address":"${storeUnitProbe.address}%x","cycle":${simulationTime}}\n"""))
     // memory write from other unit
-    when(otherUnitProbe.valid)(printf(cf"""{"event":"memoryWrite","parameter":{"mask":"${otherUnitProbe.mask}","data":"${otherUnitProbe.data}","address":${otherUnitProbe.address},"source":${otherUnitProbe.index},"cycle": ${cycleCounter}}}\n""")) 
+    when(otherUnitProbe.valid)(printf(cf"""{"event":"MemoryWrite","lsu_idx":${otherUnitProbe.index},"mask":"${otherUnitProbe.mask}%x","data":"${otherUnitProbe.data}%x","address":"${otherUnitProbe.address}%x","cycle":${simulationTime}}\n"""))
     // issue
-    when(dut.request.fire)(printf(cf"""{"event":"issue","parameter":{"idx":${t1Probe.instructionCounter},"cycle": ${cycleCounter}}}\n"""))
-    // inst
-    when(dut.response.valid)(printf(cf"""{"event":"inst","parameter":{"data":"${dut.response.bits.data}","vxsat":${dut.response.bits.vxsat},"rd_valid":${dut.response.bits.rd.valid},"rd":${dut.response.bits.rd.bits},"mem":${dut.response.bits.mem},"cycle": ${cycleCounter}}}\n"""))
+    when(dut.request.fire)(printf(cf"""{"event":"Issue","idx":${t1Probe.instructionCounter},"cycle":${simulationTime}}\n"""))
+    // check rd
+    when(dut.response.bits.rd.valid)(printf(cf"""{"event":"CheckRd","data":"${dut.response.bits.data}%x","issue_idx":${t1Probe.responseCounter},"cycle":${simulationTime}}\n"""))
     // lsu enq
-    when(lsuProbe.reqEnq.orR)(printf(cf"""{"event":"lsuEnq","parameter":{"enq":${lsuProbe.reqEnq},"cycle": ${cycleCounter}}}\n"""))
-  }
+    when(lsuProbe.reqEnq.orR)(printf(cf"""{"event":"LsuEnq","enq":${lsuProbe.reqEnq},"cycle":${simulationTime}}\n"""))
 
-  // Monitors
-  // TODO: These monitors should be purged out after offline difftest is landed
-  val peekLsuEnq = Module(new PeekLsuEnq(PeekLsuEnqParameter(dut.parameter.lsuParameters.lsuMSHRSize, latPeekLsuEnq)))
-  peekLsuEnq.clock.ref := clockGen.clock
-  peekLsuEnq.enq.ref := lsuProbe.reqEnq
-
-  lsuProbe.slots.zipWithIndex.foreach {
-    case (mshr, i) =>
-      val peekWriteQueue = Module(new PeekWriteQueue(PeekWriteQueueParameter(
-        dut.parameter.vrfParam.regNumBits,
-        dut.parameter.laneNumber,
-        dut.parameter.vrfParam.vrfOffsetBits,
-        dut.parameter.vrfParam.instructionIndexBits,
-        dut.parameter.vrfParam.datapathWidth,
-        negLatPeekWriteQueue,
-      )))
-      peekWriteQueue.mshrIdx.ref := i.U
-      peekWriteQueue.clock.ref := clockGen.clock
-      peekWriteQueue.data_vd.ref := mshr.dataVd
-      peekWriteQueue.data_offset.ref := mshr.dataOffset
-      peekWriteQueue.data_mask.ref := mshr.dataMask
-      peekWriteQueue.data_data.ref := mshr.dataData
-      peekWriteQueue.data_instruction.ref := mshr.dataInstruction
-      peekWriteQueue.writeValid.ref := mshr.writeValid
-      peekWriteQueue.targetLane.ref := mshr.targetLane
-  }
-
-  laneVrfProbes.zipWithIndex.foreach {
-    case (lane, i) =>
-      val peekVrfWrite = Module(new PeekVrfWrite(PeekVrfWriteParameter(
-        dut.parameter.vrfParam.regNumBits,
-        dut.parameter.laneNumber,
-        dut.parameter.vrfParam.vrfOffsetBits,
-        dut.parameter.vrfParam.instructionIndexBits,
-        dut.parameter.vrfParam.datapathWidth,
-        negLatPeekWriteQueue,
-      )))
-      peekVrfWrite.landIdx.ref := i.U
-      peekVrfWrite.valid.ref := lane.valid
-      peekVrfWrite.clock.ref := clockGen.clock
-      peekVrfWrite.request_vd.ref := lane.requestVd
-      peekVrfWrite.request_offset.ref := lane.requestOffset
-      peekVrfWrite.request_mask.ref := lane.requestMask
-      peekVrfWrite.request_data.ref := lane.requestData
-      peekVrfWrite.request_instruction.ref := lane.requestInstruction
-  }
-
-  val pokeInst = Module(new PokeInst(PokeInstParameter(dut.parameter.xLen, dut.parameter.laneParam.vlMaxBits, latPokeInst)))
-  pokeInst.clock.ref := clockGen.clock
-  pokeInst.respValid.ref := dut.response.valid
-  pokeInst.response_data.ref := dut.response.bits.data
-  pokeInst.response_vxsat.ref := dut.response.bits.vxsat
-  pokeInst.response_rd_valid.ref := dut.response.bits.rd.valid
-  pokeInst.response_rd_bits.ref := dut.response.bits.rd.bits
-  pokeInst.response_mem.ref := dut.response.bits.mem
-  dut.csrInterface.vl := pokeInst.csrInterface_vl.ref
-  dut.csrInterface.vStart := pokeInst.csrInterface_vStart.ref
-  dut.csrInterface.vlmul := pokeInst.csrInterface_vlMul.ref
-  dut.csrInterface.vSew := pokeInst.csrInterface_vSew.ref
-  dut.csrInterface.vxrm := pokeInst.csrInterface_vxrm.ref
-  dut.csrInterface.vta := pokeInst.csrInterface_vta.ref
-  dut.csrInterface.vma := pokeInst.csrInterface_vma.ref
-  dut.csrInterface.ignoreException := pokeInst.csrInterface_ignoreException.ref
-  dut.request.bits.instruction := pokeInst.request_instruction.ref
-  dut.request.bits.src1Data := pokeInst.request_src1Data.ref
-  dut.request.bits.src2Data := pokeInst.request_src2Data.ref
-  dut.request.valid := pokeInst.instructionValid.ref
-
-  val peekIssue = Module(new PeekIssue(PeekIssueParameter(dut.parameter.instructionIndexBits, latPeekIssue)))
-  peekIssue.clock.ref := clockGen.clock
-  peekIssue.ready.ref := dut.request.ready
-  peekIssue.issueIdx.ref := t1Probe.instructionCounter
-
-  dut.memoryPorts.zipWithIndex.foreach {
-    case (bundle, idx) =>
-      val peek = Module(new PeekTL(dut.parameter.tlParam.bundle(), latPeekTL))
-      peek.clock.ref := clockGen.clock
-      peek.channel.ref := idx.U
-      peek.aBits_opcode.ref := bundle.a.bits.opcode
-      peek.aBits_param.ref := bundle.a.bits.param
-      peek.aBits_size.ref := bundle.a.bits.size
-      peek.aBits_source.ref := bundle.a.bits.source
-      peek.aBits_address.ref := bundle.a.bits.address
-      peek.aBits_mask.ref := bundle.a.bits.mask
-      peek.aBits_data.ref := bundle.a.bits.data
-      peek.aBits_corrupt.ref := bundle.a.bits.corrupt
-
-      peek.aValid.ref := bundle.a.valid
-      peek.dReady.ref := bundle.d.ready
-
-      val poke = Module(new PokeTL(dut.parameter.tlParam.bundle(), latPokeTL))
-      poke.clock.ref := clockGen.clock
-      poke.channel.ref := idx.U
-      bundle.d.bits.opcode := poke.dBits_opcode.ref
-      bundle.d.bits.param := poke.dBits_param.ref
-      bundle.d.bits.sink := poke.dBits_sink.ref
-      bundle.d.bits.source := poke.dBits_source.ref
-      bundle.d.bits.size := poke.dBits_size.ref
-      bundle.d.bits.denied := poke.dBits_denied.ref
-      bundle.d.bits.data := poke.dBits_data.ref
-      bundle.d.bits.corrupt := poke.dBits_corrupt.ref
-      bundle.d.valid := poke.dValid.ref
-      poke.dReady.ref := bundle.d.ready
-      bundle.a.ready := poke.aReady.ref
+    // allocate 2 * chainingSize scoreboards
+    val vrfWriteScoreboard: Seq[Valid[UInt]] = Seq.tabulate(2 * dut.parameter.chainingSize) { _ => RegInit(0.U.asTypeOf(Valid(UInt(16.W))))}
+    vrfWriteScoreboard.foreach(scoreboard => dontTouch(scoreboard))
+    val instructionValid =
+      (laneProbes.map(laneProbe => laneProbe.instructionValid ## laneProbe.instructionValid) :+
+        lsuProbe.lsuInstructionValid :+ t1Probe.instructionValid).reduce(_ | _)
+    val scoreboardEnq = Mux(t1Probe.instructionIssue, UIntToOH(t1Probe.issueTag), 0.U((2 * dut.parameter.chainingSize).W))
+    vrfWriteScoreboard.zipWithIndex.foreach { case (scoreboard, tag) =>
+      val writeEnq: UInt = VecInit(
+        // vrf write from lane
+        laneProbes.flatMap(laneProbe => laneProbe.slots.map(slot =>
+          slot.writeTag === tag.U && slot.writeQueueEnq && slot.writeMask.orR
+        )) ++ laneProbes.flatMap(laneProbe => laneProbe.crossWriteProbe.map(cp =>
+          cp.bits.writeTag === tag.U && cp.valid && cp.bits.writeMask.orR
+        )) ++
+          // vrf write from lsu
+          lsuProbe.slots.map(slot => slot.dataInstruction === tag.U && slot.writeValid && slot.dataMask.orR) ++
+          // vrf write from Sequencer
+          Some(t1Probe.writeQueueEnq.bits === tag.U && t1Probe.writeQueueEnq.valid && t1Probe.writeQueueEnqMask.orR)
+        ).asUInt
+      // always equal to array index
+      scoreboard.bits := scoreboard.bits + PopCount(writeEnq)
+      when(scoreboard.valid && !instructionValid(tag)){
+        printf(cf"""{"event":"VrfScoreboardReport","count":${scoreboard.bits},"issue_idx":${tag},"cycle":${simulationTime}}\n""")
+        scoreboard.valid := false.B
+      }
+      when(scoreboardEnq(tag)) {
+        scoreboard.valid := true.B
+        assert(!scoreboard.valid)
+        scoreboard.bits := 0.U
+      }
+    }
   }
 }
