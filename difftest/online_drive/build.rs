08facaa393675ac1d56c2e9894d6ed754379e65b
Jiuyang Liu <liu@jiuyang.me>
2024-07-21 05:42:16 +0800

    [ipemu] rename dpi_pre_link to verilator_shim

--- difftest/online_drive/build.rs
+++ difftest/online_drive/build.rs
@@ -3,17 +3,17 @@ use cmake::Config;
 fn main() {
   #[cfg(feature = "trace")]
   let dst =
-    Config::new("dpi_c").define("VM_TRACE", "1").very_verbose(true).always_configure(true).build();
+    Config::new("verilator_shim").define("VM_TRACE", "1").very_verbose(true).always_configure(true).build();
   #[cfg(not(feature = "trace"))]
-  let dst = Config::new("dpi_c").very_verbose(true).always_configure(true).build();
+  let dst = Config::new("verilator_shim").very_verbose(true).always_configure(true).build();
 
   println!("cargo::rustc-link-search=native={}/lib", dst.display());
 
   // link order matters!
-  // verilator_main <- VTestBench <-- verilated <- dpi_c <- stdc++
+  // verilator_main <- VTestBench <-- verilated <- verilator_shim <- stdc++
   // verilated <- libz
-  // that's why we must split verilator_main and dpi_c
-  println!("cargo::rustc-link-lib=static=dpi_pre_link");
+  // that's why we must split verilator_main and verilator_shim
+  println!("cargo::rustc-link-lib=static=verilator_shim");
   println!("cargo::rustc-link-lib=static=VTestBench");
   println!("cargo::rustc-link-lib=static=verilated");
   println!("cargo::rustc-link-lib=stdc++");
