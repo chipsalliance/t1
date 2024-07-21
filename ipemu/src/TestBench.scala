4476742113513694aaffde8e6e24edf322cac4f4
Jiuyang Liu <liu@jiuyang.me>
2024-07-21 11:14:07 +0800

    [ipemu] add fsdbdump support in TestBench

--- ipemu/src/TestBench.scala
+++ ipemu/src/TestBench.scala
@@ -41,8 +41,15 @@ class TestBench(generator: SerializableModuleGenerator[T1, T1Parameter])
       s"""module $desiredName(output reg clock, output reg reset);
          |  export "DPI-C" function dump_wave;
          |  function dump_wave(input string file);
+         |`ifdef VCS
+         |    $$fsdbDumpfile(file);
+         |    $$fsdbDumpvars("+all");
+         |    $$fsdbDumpon;
+         |`endif
+         |`ifdef VERILATOR
          |    $$dumpfile(file);
          |    $$dumpvars(0);
+         |`endif
          |  endfunction;
          |
          |  import "DPI-C" context function void t1_cosim_init();
