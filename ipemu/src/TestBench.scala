94e0e11440ecdc0e101ef400940cce6121b33455
Shupei Fan <dymarkfan@outlook.com>
2024-07-26 16:14:44 +0000

    [ipemu] mark cosim_init as context

--- ipemu/src/TestBench.scala
+++ ipemu/src/TestBench.scala
@@ -45,7 +45,7 @@ class TestBench(generator: SerializableModuleGenerator[T1, T1Parameter])
          |    $$dumpvars(0);
          |  endfunction;
          |
-         |  import "DPI-C" function void t1_cosim_init();
+         |  import "DPI-C" context function void t1_cosim_init();
          |  initial begin
          |    t1_cosim_init();
          |    clock = 1'b0;
