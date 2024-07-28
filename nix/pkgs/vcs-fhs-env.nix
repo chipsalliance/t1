fcfd813b5f65ab45f73a5818458440ec15250be1
Jiuyang Liu <liu@jiuyang.me>
2024-07-29 03:25:52 +0800

    [nix] refactor nix for vcs+verilator emulator

---
+++ nix/pkgs/vcs-fhs-env.nix
@@ -0,0 +1,91 @@
+{ buildFHSEnv
+, vcStaticHome
+, snpslmdLicenseFile
+}:
+buildFHSEnv {
+  name = "vcs-fhs-env";
+
+  profile = ''
+    [ ! -e "${vcStaticHome}"  ] && echo "env VC_STATIC_HOME not set" && exit 1
+    [ ! -d "${vcStaticHome}"  ] && echo "VC_STATIC_HOME not accessible" && exit 1
+    [ -z "${snpslmdLicenseFile}"  ] && echo "env SNPS LICENSE not set" && exit 1
+    export VC_STATIC_HOME=${vcStaticHome}
+
+    export TCL_TZ=UTC
+    export VC_STATIC_HOME=$VC_STATIC_HOME
+    export VCS_HOME=$VC_STATIC_HOME/vcs-mx
+    export VCS_TARGET_ARCH=amd64
+    export VCS_ARCH_OVERRIDE=linux
+    export VERDI_HOME=$VC_STATIC_HOME/verdi
+    export NOVAS_HOME=$VC_STATIC_HOME/verdi
+    export SPYGLASS_HOME=$VC_STATIC_HOME/SG_COMPAT/SPYGLASS_HOME
+    export SNPS_VERDI_CBUG_LCA=1
+    export SNPSLMD_LICENSE_FILE=${snpslmdLicenseFile}
+
+    export PATH=$VC_STATIC_HOME/bin:$PATH
+    export PATH=$VC_STATIC_HOME/verdi/bin:$PATH
+    export PATH=$VC_STATIC_HOME/vcs-mx/bin:$PATH
+    export PATH=$VC_STATIC_HOME/SG_COMPAT/SPYGLASS_HOME/bin:$PATH
+
+    export LD_LIBRARY_PATH=/usr/lib64/
+    export LD_LIBRARY_PATH=$VC_STATIC_HOME/verdi/share/PLI/lib/LINUX64:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$VC_STATIC_HOME/verdi/share/NPI/lib/LINUX64:$LD_LIBRARY_PATH
+
+    export _oldVcsEnvPath="$PATH"
+    preHook() {
+      PATH="$PATH:$_oldVcsEnvPath"
+    }
+    export -f preHook
+  '';
+  targetPkgs = (ps: with ps; [
+    libGL
+    util-linux
+    libxcrypt-legacy
+    coreutils-full
+    ncurses5
+    gmp5
+    bzip2
+    glib
+    bc
+    time
+    elfutils
+    ncurses5
+    e2fsprogs
+    cyrus_sasl
+    expat
+    sqlite
+    nssmdns
+    (libkrb5.overrideAttrs rec {
+      version = "1.18.2";
+      src = fetchurl {
+        url = "https://kerberos.org/dist/krb5/${lib.versions.majorMinor version}/krb5-${version}.tar.gz";
+        hash = "sha256-xuTJ7BqYFBw/XWbd8aE1VJBQyfq06aRiDumyIIWHOuA=";
+      };
+      sourceRoot = "krb5-${version}/src";
+    })
+    (gnugrep.overrideAttrs rec {
+      version = "3.1";
+      doCheck = false;
+      src = fetchurl {
+        url = "mirror://gnu/grep/grep-${version}.tar.xz";
+        hash = "sha256-22JcerO7PudXs5JqXPqNnhw5ka0kcHqD3eil7yv3oH4=";
+      };
+    })
+    keyutils
+    graphite2
+    libpulseaudio
+    libxml2
+    gcc
+    gnumake
+    xorg.libX11
+    xorg.libXft
+    xorg.libXScrnSaver
+    xorg.libXext
+    xorg.libxcb
+    xorg.libXau
+    xorg.libXrender
+    xorg.libXcomposite
+    xorg.libXi
+    zlib
+  ]);
+}
