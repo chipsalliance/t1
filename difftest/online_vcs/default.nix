ddf18fa138c365b9bb9385f41efd8a2753dd806b
Shupei Fan <dymarkfan@outlook.com>
2024-07-29 03:24:17 +0800

    [ipemu] add VCS DPI library build target

---
+++ difftest/online_vcs/default.nix
@@ -0,0 +1,44 @@
+{ lib
+, elaborateConfig
+, rustPlatform
+, libspike
+, spike_interfaces
+, enable-trace ? false
+, vcStaticHome
+}:
+
+rustPlatform.buildRustPackage {
+  name = "vcs-dpi-lib";
+  src = with lib.fileset; toSource {
+    root = ../.;
+    fileset = unions [
+      ../spike_rs
+      ../offline
+      ../online_dpi
+      ../online_drive
+      ../online_vcs
+      ../test_common
+      ../Cargo.lock
+      ../Cargo.toml
+    ];
+  };
+
+  buildFeatures = lib.optionals enable-trace [ "trace" ];
+  buildAndTestSubdir = "./online_vcs";
+
+  env = {
+    VCS_LIB_DIR = "${vcStaticHome}/vcs-mx/linux64/lib";
+    SPIKE_LIB_DIR = "${libspike}/lib";
+    SPIKE_INTERFACES_LIB_DIR = "${spike_interfaces}/lib";
+    DESIGN_VLEN = elaborateConfig.parameter.vLen;
+    DESIGN_DLEN = elaborateConfig.parameter.dLen;
+  };
+
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+  };
+
+  passthru = {
+    inherit enable-trace;
+  };
+}
