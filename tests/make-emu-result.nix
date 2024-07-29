3594eb47062296e31654709a6e0f0a2a3008cfee
Avimitin <dev@avimit.in>
2024-07-29 18:34:15 +0800

    [nix] add derivation for vcs artifacts

--- tests/make-emu-result.nix
+++ tests/make-emu-result.nix
@@ -5,6 +5,8 @@
 , zstd
 , verilator-emu
 , verilator-emu-trace
+, vcs-emu
+, vcs-emu-trace
 , elaborateConfigJson
 }:
 
@@ -28,6 +30,7 @@ let
       "--log-level"
       "ERROR"
     ];
+    rtlEventOutPath = "${placeholder "out"}/${testCase.pname}-rtl-event.jsonl";
 
     buildPhase = ''
       runHook preBuild
@@ -36,7 +39,7 @@ let
 
       echo "[nix] Running test case ${testCase.pname} with args $difftestArgs"
 
-      RUST_BACKTRACE=full "$difftestDriver" $difftestArgs 2> $out/rtl-event.jsonl
+      RUST_BACKTRACE=full "$difftestDriver" $difftestArgs 2> "$rtlEventOutPath"
 
       echo "[nix] online driver done"
 
@@ -47,15 +50,15 @@ let
     checkPhase = ''
       runHook preCheck
 
-      if [ ! -r $out/rtl-event.jsonl ]; then
-        echo -e "[nix] \033[0;31mInternal Error\033[0m: no rtl-event.jsonl found in output"
+      if [ ! -r "$rtlEventOutPath" ]; then
+        echo -e "[nix] \033[0;31mInternal Error\033[0m: no $rtlEventOutPath found in output"
         exit 1
       fi
 
-      if ! jq --stream -c -e '.[]' "$out/rtl-event.jsonl" >/dev/null 2>&1; then
-        echo -e "[nix] \033[0;31mInternal Error\033[0m: invalid JSON file rtl-event.jsonl, showing original file:"
+      if ! jq --stream -c -e '.[]' "$rtlEventOutPath" >/dev/null 2>&1; then
+        echo -e "[nix] \033[0;31mInternal Error\033[0m: invalid JSON file $rtlEventOutPath, showing original file:"
         echo "--------------------------------------------"
-        cat $out/rtl-event.jsonl
+        cat $rtlEventOutPath
         echo "--------------------------------------------"
         exit 1
       fi
@@ -67,17 +70,20 @@ let
       runHook preInstall
 
       echo "[nix] compressing event log"
-      zstd $out/rtl-event.jsonl -o $out/rtl-event.jsonl.zstd
-      rm $out/rtl-event.jsonl
+      zstd $rtlEventOutPath -o $rtlEventOutPath.zstd
+      rm $rtlEventOutPath
 
-      mv perf.txt $out/
+      if [ -r perf.txt ]; then
+        mv perf.txt $out/
+      fi
 
       runHook postInstall
     '';
 
     passthru.with-trace = self.overrideAttrs (old: {
-      difftestDriver = "${verilator-emu-trace}/bin/online_drive";
-      difftestArgs = old.difftestArgs ++ [ "--wave-path" "${placeholder "out"}/wave.fst" ];
+      name = old.name + "-with-trace";
+      emuDriver = "${verilator-emu-trace}/bin/online_drive";
+      emuDriverArgs = old.emuDriverArgs ++ [ "--wave-path" "${placeholder "out"}/wave.fst" ];
       postCheck = ''
         if [ ! -r "$out/wave.fst" ]; then
           echo -e "[nix] \033[0;31mInternal Error\033[0m: waveform not found in output"
@@ -87,14 +93,90 @@ let
     });
 
     passthru.with-offline = self.overrideAttrs (old: {
+      name = old.name + "-with-offline";
       preInstall = ''
         set +e
         "${verilator-emu}/bin/offline" \
           --elf-file ${testCase}/bin/${testCase.pname}.elf \
-          --log-file $out/rtl-event.jsonl \
+          --log-file $rtlEventOutPath \
           --log-level ERROR &> $out/offline-check-journal
         printf "$?" > $out/offline-check-status
         set -e
+      '';
+    });
+
+    passthru.with-vcs = self.overrideAttrs (old: {
+      name = old.name + "-with-vcs";
+      __noChroot = true;
+
+      buildPhase = ''
+        runHook preBuild
+
+        mkdir -p "$out"
+        echo "[nix] Running VCS for ${testCase.pname}"
+
+        RUST_BACKTRACE=full "${vcs-emu}/bin/t1-vcs-simulator" \
+          --elf-file ${testCase}/bin/${testCase.pname}.elf \
+          1> /dev/null \
+          2> $rtlEventOutPath
+
+        echo "[nix] VCS emu done"
+
+        runHook postBuild
+      '';
+
+      postCheck = ''
+        set +e
+
+        "${verilator-emu}/bin/offline" \
+          --elf-file ${testCase}/bin/${testCase.pname}.elf \
+          --log-file $rtlEventOutPath \
+          --log-level ERROR &> $out/offline-check-journal
+        printf "$?" > $out/offline-check-status
+
+        set -e
+      '';
+    });
+
+    # TODO: We should write some framework like NixOS module to overlay these attribute, instead
+    # of override attribute one by one.
+    passthru.with-vcs-trace = self.overrideAttrs (old: {
+      name = old.name + "-with-vcs-trace";
+      __noChroot = true;
+      buildPhase = ''
+        runHook preBuild
+
+        mkdir -p "$out"
+        echo "[nix] Running VCS(TRACE) for ${testCase.pname}"
+
+        RUST_BACKTRACE=full "${vcs-emu-trace}/bin/t1-vcs-simulator" \
+          --elf-file ${testCase}/bin/${testCase.pname}.elf \
+          --wave-path ${testCase.pname}.fsdb \
+          1> /dev/null \
+          2> $rtlEventOutPath
+
+        echo "[nix] VCS emu done"
+
+        runHook postBuild
+      '';
+
+      postCheck = ''
+        set +e
+
+        "${verilator-emu}/bin/offline" \
+          --elf-file ${testCase}/bin/${testCase.pname}.elf \
+          --log-file $rtlEventOutPath \
+          --log-level ERROR &> $out/offline-check-journal
+        printf "$?" > $out/offline-check-status
+
+        set -e
+      '';
+
+      postInstall = ''
+        # VCS have weird behavior on file creation, it will report read-only filesystem on our output,
+        # while other tools can mutate file system correctly.
+        cp ${testCase.pname}.fsdb "$out"
+        cp -r ${vcs-emu-trace}/lib/t1-vcs-simulator.daidir "$out"
       '';
     });
   };
