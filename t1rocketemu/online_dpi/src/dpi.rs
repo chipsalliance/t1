3c6b672fbdb82de81c4ff5119ffa00352660885d
Clo91eaf <Clo91eaf@qq.com>
2024-08-04 15:05:01 +0800

    [t1rocket] migrate difftest framework from t1 and update dpi from rocket
    [t1rocket] refactor load_from_payload function to improve readability and performance
    
    [t1rocket] add elf crate dependency

---
+++ t1rocketemu/online_dpi/src/dpi.rs
@@ -0,0 +1,321 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+
+use clap::Parser;
+use std::ffi::{c_char, c_longlong, CString};
+use std::sync::Mutex;
+use tracing::debug;
+
+use crate::drive::Driver;
+use crate::svdpi::SvScope;
+use crate::OfflineArgs;
+
+pub type SvBitVecVal = u32;
+
+// --------------------------
+// preparing data structures
+// --------------------------
+
+static DPI_TARGET: Mutex<Option<Box<Driver>>> = Mutex::new(None);
+
+pub(crate) struct AxiReadPayload {
+  pub(crate) data: Vec<u8>,
+}
+
+unsafe fn write_to_pointer(dst: *mut u8, data: &[u8]) {
+  let dst = std::slice::from_raw_parts_mut(dst, data.len());
+  dst.copy_from_slice(data);
+}
+
+unsafe fn fill_axi_read_payload(dst: *mut SvBitVecVal, dlen: u32, payload: &AxiReadPayload) {
+  let data_len = 256 * (dlen / 8) as usize;
+  assert!(payload.data.len() <= data_len);
+  write_to_pointer(dst as *mut u8, &payload.data);
+}
+
+// Return (strobe in bit, data in byte)
+unsafe fn load_from_payload(
+  payload: &*const SvBitVecVal,
+  data_width: usize,
+  dlen: usize,
+) -> (&[u8], &[u8]) {
+  let src = *payload as *mut u8;
+  let data_width_in_byte = dlen / 8;
+  let strb_width_in_byte = dlen / data_width;
+  let payload_size_in_byte = strb_width_in_byte + data_width_in_byte; // data width in byte
+  let byte_vec = std::slice::from_raw_parts(src, payload_size_in_byte);
+  let strobe = &byte_vec[0..strb_width_in_byte];
+  let data = &byte_vec[strb_width_in_byte..];
+
+  let strb_width_in_bit = data_width / 8;
+  let masks: Vec<bool> = strobe
+    .into_iter()
+    .flat_map(|strb| {
+      let mask: Vec<bool> = (0..strb_width_in_bit).map(|i| (strb & (1 << i)) != 0).collect();
+      mask
+    })
+    .collect();
+  assert!(
+    masks.len() == data.len(),
+    "strobe bit width is not aligned with data byte width"
+  );
+
+  debug!(
+    "load {payload_size_in_byte} byte from payload: raw_data={} strb={} data={}",
+    hex::encode(byte_vec),
+    hex::encode(strobe),
+    hex::encode(data),
+  );
+
+  (&masks, data)
+}
+
+//----------------------
+// dpi functions
+//----------------------
+
+/// evaluate after AW and W is finished at corresponding channel_id.
+#[no_mangle]
+unsafe extern "C" fn axi_write_highBandwidthAXI(
+  channel_id: c_longlong,
+  awid: c_longlong,
+  awaddr: c_longlong,
+  awlen: c_longlong,
+  awsize: c_longlong,
+  awburst: c_longlong,
+  awlock: c_longlong,
+  awcache: c_longlong,
+  awprot: c_longlong,
+  awqos: c_longlong,
+  awregion: c_longlong,
+  // struct packed {bit [255:0][DLEN:0] data;
+  // bit [255:0][DLEN/8:0] strb; } payload
+  payload: *const SvBitVecVal,
+) {
+  debug!(
+    "axi_write_highBandwidth (channel_id={channel_id}, awid={awid}, awaddr={awaddr:#x}, \
+  awlen={awlen}, awsize={awsize}, awburst={awburst}, awlock={awlock}, awcache={awcache}, \
+  awprot={awprot}, awqos={awqos}, awregion={awregion})"
+  );
+  let mut driver = DPI_TARGET.lock().unwrap();
+  let driver = driver.as_mut().unwrap();
+  let data_width = 32; // TODO: get from driver
+  let (strobe, data) = load_from_payload(&payload, 32, driver.dlen);
+  driver.axi_write_high_bandwidth(awaddr as u32, awsize as u64, strobe, data);
+}
+
+/// evaluate at AR fire at corresponding channel_id.
+#[no_mangle]
+unsafe extern "C" fn axi_read_highBandwidthAXI(
+  channel_id: c_longlong,
+  arid: c_longlong,
+  araddr: c_longlong,
+  arlen: c_longlong,
+  arsize: c_longlong,
+  arburst: c_longlong,
+  arlock: c_longlong,
+  arcache: c_longlong,
+  arprot: c_longlong,
+  arqos: c_longlong,
+  arregion: c_longlong,
+  // struct packed {bit [255:0][DLEN:0] data; byte beats; } payload
+  payload: *mut SvBitVecVal,
+) {
+  debug!(
+    "axi_read_highBandwidth (channel_id={channel_id}, arid={arid}, araddr={araddr:#x}, \
+  arlen={arlen}, arsize={arsize}, arburst={arburst}, arlock={arlock}, arcache={arcache}, \
+  arprot={arprot}, arqos={arqos}, arregion={arregion})"
+  );
+  let mut driver = DPI_TARGET.lock().unwrap();
+  let driver = driver.as_mut().unwrap();
+  let response = driver.axi_read_high_bandwidth(araddr as u32, arsize as u64);
+  fill_axi_read_payload(payload, driver.dlen, &response);
+}
+
+/// evaluate after AW and W is finished at corresponding channel_id.
+#[no_mangle]
+unsafe extern "C" fn axi_write_indexedAccessAXI(
+  channel_id: c_longlong,
+  awid: c_longlong,
+  awaddr: c_longlong,
+  awlen: c_longlong,
+  awsize: c_longlong,
+  awburst: c_longlong,
+  awlock: c_longlong,
+  awcache: c_longlong,
+  awprot: c_longlong,
+  awqos: c_longlong,
+  awregion: c_longlong,
+  // struct packed {bit [255:0][31:0] data; bit [255:0][3:0] strb; } payload
+  payload: *const SvBitVecVal,
+) {
+  debug!(
+    "axi_write_indexed (channel_id={channel_id}, awid={awid}, awaddr={awaddr:#x}, \
+  awlen={awlen}, awsize={awsize}, awburst={awburst}, awlock={awlock}, awcache={awcache}, \
+  awprot={awprot}, awqos={awqos}, awregion={awregion})"
+  );
+  let mut driver = DPI_TARGET.lock().unwrap();
+  let driver = driver.as_mut().unwrap();
+  let data_width = 32; // TODO: get from driver
+  let (strobe, data) = load_from_payload(&payload, data_width, 32);
+  driver.axi_write_indexed_access(awaddr as u32, awsize as u64, strobe, data);
+}
+
+/// evaluate at AR fire at corresponding channel_id.
+#[no_mangle]
+unsafe extern "C" fn axi_read_indexedAccessAXI(
+  channel_id: c_longlong,
+  arid: c_longlong,
+  araddr: c_longlong,
+  arlen: c_longlong,
+  arsize: c_longlong,
+  arburst: c_longlong,
+  arlock: c_longlong,
+  arcache: c_longlong,
+  arprot: c_longlong,
+  arqos: c_longlong,
+  arregion: c_longlong,
+  // struct packed {bit [255:0][DLEN:0] data; byte beats; } payload
+  payload: *mut SvBitVecVal,
+) {
+  debug!(
+    "axi_read_indexed (channel_id={channel_id}, arid={arid}, araddr={araddr:#x}, \
+  arlen={arlen}, arsize={arsize}, arburst={arburst}, arlock={arlock}, arcache={arcache}, \
+  arprot={arprot}, arqos={arqos}, arregion={arregion})"
+  );
+  let mut driver = DPI_TARGET.lock().unwrap();
+  let driver = driver.as_mut().unwrap();
+  let response = driver.axi_read_indexed(araddr as u32, arsize as u64);
+  fill_axi_read_payload(payload, driver.dlen, &response);
+}
+
+#[no_mangle]
+unsafe extern "C" fn axi_write_loadStoreAXI(
+  target: *mut (),
+  channel_id: c_longlong,
+  awid: c_longlong,
+  awaddr: c_longlong,
+  awlen: c_longlong,
+  awsize: c_longlong,
+  awburst: c_longlong,
+  awlock: c_longlong,
+  awcache: c_longlong,
+  awprot: c_longlong,
+  awqos: c_longlong,
+  awregion: c_longlong,
+  payload: *const SvBitVecVal,
+) {
+  debug!(
+    "axi_write_loadStore (channel_id={channel_id}, awid={awid}, awaddr={awaddr:#x}, \
+  awlen={awlen}, awsize=2^{awsize}, awburst={awburst}, awlock={awlock}, awcache={awcache}, \
+  awprot={awprot}, awqos={awqos}, awregion={awregion})"
+  );
+  let mut driver = DPI_TARGET.lock().unwrap();
+  let driver = driver.as_mut().unwrap();
+  let data_width = 32; // TODO: get from sim
+  let (strobe, data) = load_from_payload(&payload, data_width, driver.dlen as usize);
+  driver.axi_write_load_store(awaddr as u32, awsize, strobe, data);
+}
+
+#[no_mangle]
+unsafe extern "C" fn axi_read_loadStoreAXI(
+  target: *mut (),
+  channel_id: c_longlong,
+  arid: c_longlong,
+  araddr: c_longlong,
+  arlen: c_longlong,
+  arsize: c_longlong,
+  arburst: c_longlong,
+  arlock: c_longlong,
+  arcache: c_longlong,
+  arprot: c_longlong,
+  arqos: c_longlong,
+  arregion: c_longlong,
+  payload: *mut SvBitVecVal,
+) {
+  debug!(
+    "axi_read_loadStoreAXI (channel_id={channel_id}, arid={arid}, araddr={araddr:#x}, \
+  arlen={arlen}, arsize={arsize}, arburst={arburst}, arlock={arlock}, arcache={arcache}, \
+  arprot={arprot}, arqos={arqos}, arregion={arregion})"
+  );
+  let mut driver = DPI_TARGET.lock().unwrap();
+  let driver = driver.as_mut().unwrap();
+  let response = driver.axi_read_load_store(araddr as u32, arsize as u64);
+  fill_axi_read_payload(payload, driver.dlen, &response.data);
+}
+
+#[no_mangle]
+unsafe extern "C" fn axi_read_instructionFetchAXI(
+  target: *mut (),
+  channel_id: c_longlong,
+  arid: c_longlong,
+  araddr: c_longlong,
+  arlen: c_longlong,
+  arsize: c_longlong,
+  arburst: c_longlong,
+  arlock: c_longlong,
+  arcache: c_longlong,
+  arprot: c_longlong,
+  arqos: c_longlong,
+  arregion: c_longlong,
+  payload: *mut SvBitVecVal,
+) {
+  debug!(
+    "axi_read_instructionFetchAXI (channel_id={channel_id}, arid={arid}, araddr={araddr:#x}, \
+  arlen={arlen}, arsize={arsize}, arburst={arburst}, arlock={arlock}, arcache={arcache}, \
+  arprot={arprot}, arqos={arqos}, arregion={arregion})"
+  );
+  let mut driver = DPI_TARGET.lock().unwrap();
+  let driver = driver.as_mut().unwrap();
+  let response = driver.axi_read_instruction_fetch(araddr as u32, arsize as u64);
+  fill_axi_read_payload(payload, driver.dlen, &response.data);
+}
+
+#[no_mangle]
+unsafe extern "C" fn cosim_init() {
+  let args = OfflineArgs::parse();
+  args.common_args.setup_logger().unwrap();
+
+  let scope = SvScope::get_current().expect("failed to get scope in cosim_init");
+
+  let driver = Box::new(Driver::new(scope, &args));
+  let mut dpi_target = DPI_TARGET.lock().unwrap();
+  assert!(
+    dpi_target.is_none(),
+    "cosim_init should be called only once"
+  );
+  *dpi_target = Some(driver);
+}
+
+#[no_mangle]
+unsafe extern "C" fn get_resetvector(target: *mut (), resetvector: *mut c_longlong) {
+  if !target.is_null() {
+    let mut driver = DPI_TARGET.lock().unwrap();
+    let driver = driver.as_mut().unwrap();
+    *resetvector = driver.e_entry as c_longlong
+  }
+}
+
+//--------------------------------
+// import functions and wrappers
+//--------------------------------
+
+mod dpi_export {
+  use std::ffi::c_char;
+  extern "C" {
+    #[cfg(feature = "trace")]
+    /// `export "DPI-C" function dump_wave(input string file)`
+    pub fn dump_wave(path: *const c_char);
+  }
+}
+
+#[cfg(feature = "trace")]
+pub(crate) fn dump_wave(scope: crate::svdpi::SvScope, path: &str) {
+  use crate::svdpi;
+  let path_cstring = CString::new(path).unwrap();
+
+  svdpi::set_scope(scope);
+  unsafe {
+    dpi_export::dump_wave(path_cstring.as_ptr());
+  }
+}
---
+++ t1rocketemu/spike_interfaces/spike_interfaces.cc
@@ -0,0 +1,252 @@
+#include <iomanip>
+
+#include "spike_interfaces.h"
+
+constexpr uint32_t CSR_MSIMEND = 0x7cc;
+
+void *ffi_target;
+
+cfg_t make_spike_cfg() {
+  cfg_t cfg;
+  cfg.initrd_bounds = std::make_pair((reg_t)0, (reg_t)0),
+  cfg.bootargs = nullptr;
+  cfg.isa = DEFAULT_ISA;
+  cfg.priv = DEFAULT_PRIV;
+  cfg.misaligned = false;
+  cfg.endianness = endianness_little;
+  cfg.pmpregions = 16;
+  cfg.pmpgranularity = 4;
+  cfg.mem_layout = std::vector<mem_cfg_t>();
+  cfg.hartids = std::vector<size_t>();
+  cfg.explicit_hartids = false;
+  cfg.real_time_clint = false;
+  cfg.trigger_count = 4;
+  return cfg;
+}
+
+Spike::Spike(const char *set, const char *lvl,
+             size_t lane_number)
+    : sim(), isa(set, lvl), cfg(make_spike_cfg()),
+      proc(
+          /*isa*/ &isa,
+          /*cfg*/ &cfg,
+          /*sim*/ &sim,
+          /*id*/ 0,
+          /*halt on reset*/ true,
+          /*log_file_t*/ nullptr,
+          /*sout*/ std::cerr) {
+  proc.VU.lane_num = lane_number;
+  proc.VU.lane_granularity = 32;
+
+  auto &csrmap = proc.get_state()->csrmap;
+  csrmap[CSR_MSIMEND] = std::make_shared<basic_csr_t>(&proc, CSR_MSIMEND, 1);
+  proc.enable_log_commits();
+}
+
+spike_t *spike_new(const char *set, const char *lvl,
+                   size_t lane_number) {
+  return new spike_t{new Spike(set, lvl, lane_number)};
+}
+
+const char *proc_disassemble(spike_processor_t *proc) {
+  auto pc = proc->p->get_state()->pc;
+  auto mmu = proc->p->get_mmu();
+  auto disasm = proc->p->get_disassembler();
+  auto fetch = mmu->load_insn(pc);
+  return strdup(disasm->disassemble(fetch.insn).c_str());
+}
+
+spike_processor_t *spike_get_proc(spike_t *spike) {
+  return new spike_processor_t{spike->s->get_proc()};
+}
+
+void proc_reset(spike_processor_t *proc) { proc->p->reset(); }
+
+spike_state_t *proc_get_state(spike_processor_t *proc) {
+  return new spike_state_t{proc->p->get_state()};
+}
+
+reg_t proc_func(spike_processor_t *proc) {
+  auto pc = proc->p->get_state()->pc;
+  auto mmu = proc->p->get_mmu();
+  auto fetch = mmu->load_insn(pc);
+  try {
+    return fetch.func(proc->p, fetch.insn, pc);
+  } catch (trap_t &trap) {
+    std::cerr << "Error: spike trapped with " << trap.name()
+              << " (tval=" << std::uppercase << std::setfill('0')
+              << std::setw(8) << std::hex << trap.get_tval()
+              << ", tval2=" << std::setw(8) << std::hex << trap.get_tval2()
+              << ", tinst=" << std::setw(8) << std::hex << trap.get_tinst()
+              << ")" << std::endl;
+    throw trap;
+  }
+}
+
+reg_t proc_get_insn(spike_processor_t *proc) {
+  auto pc = proc->p->get_state()->pc;
+  auto mmu = proc->p->get_mmu();
+  auto fetch = mmu->load_insn(pc);
+  return fetch.insn.bits();
+}
+
+uint8_t proc_get_vreg_data(spike_processor_t *proc, uint32_t vreg_idx,
+                           uint32_t vreg_offset) {
+  return proc->p->VU.elt<uint8_t>(vreg_idx, vreg_offset);
+}
+
+uint32_t extract_f32(freg_t f) { return (uint32_t)f.v[0]; }
+
+inline uint32_t clip(uint32_t binary, int a, int b) {
+  int nbits = b - a + 1;
+  uint32_t mask = nbits >= 32 ? (uint32_t)-1 : (1 << nbits) - 1;
+  return (binary >> a) & mask;
+}
+
+uint32_t proc_get_rs1(spike_processor_t *proc) {
+  auto pc = proc->p->get_state()->pc;
+  auto fetch = proc->p->get_mmu()->load_insn(pc);
+  return (uint32_t)fetch.insn.rs1();
+}
+
+uint32_t proc_get_rs2(spike_processor_t *proc) {
+  auto pc = proc->p->get_state()->pc;
+  auto fetch = proc->p->get_mmu()->load_insn(pc);
+  return (uint32_t)fetch.insn.rs2();
+}
+
+uint32_t proc_get_rd(spike_processor_t *proc) {
+  auto pc = proc->p->get_state()->pc;
+  auto fetch = proc->p->get_mmu()->load_insn(pc);
+  return fetch.insn.rd();
+}
+
+uint64_t proc_vu_get_vtype(spike_processor_t *proc) {
+  return proc->p->VU.vtype->read();
+}
+
+uint32_t proc_vu_get_vxrm(spike_processor_t *proc) {
+  return proc->p->VU.vxrm->read();
+}
+
+uint32_t proc_vu_get_vnf(spike_processor_t *proc) {
+  auto pc = proc->p->get_state()->pc;
+  auto fetch = proc->p->get_mmu()->load_insn(pc);
+  return fetch.insn.v_nf();
+}
+
+bool proc_vu_get_vill(spike_processor_t *proc) { return proc->p->VU.vill; }
+
+bool proc_vu_get_vxsat(spike_processor_t *proc) {
+  return proc->p->VU.vxsat->read();
+}
+
+uint32_t proc_vu_get_vl(spike_processor_t *proc) {
+  return proc->p->VU.vl->read();
+}
+
+uint16_t proc_vu_get_vstart(spike_processor_t *proc) {
+  return proc->p->VU.vstart->read();
+}
+
+reg_t state_get_pc(spike_state_t *state) { return state->s->pc; }
+
+void state_set_mcycle(spike_state_t *state, size_t mcycle) {
+  state->s->mcycle->write((int64_t)mcycle);
+}
+
+void state_clear(spike_state_t *state) {
+  state->s->log_reg_write.clear();
+  state->s->log_mem_read.clear();
+  state->s->log_mem_write.clear();
+}
+
+static void state_set_serialized(spike_state_t *state, bool serialized) {
+  state->s->serialized = serialized;
+}
+
+uint64_t state_handle_pc(spike_state_t *state, uint64_t new_pc) {
+  if ((new_pc & 1) == 0) {
+    state_set_pc(state, new_pc);
+  } else {
+    switch (new_pc) {
+    case PC_SERIALIZE_BEFORE:
+      state_set_serialized(state, true);
+      break;
+    case PC_SERIALIZE_AFTER:
+      break;
+    default:
+      return -1;
+    }
+  }
+  return 0;
+}
+
+void state_set_pc(spike_state_t *state, uint64_t pc) { state->s->pc = pc; }
+
+uint32_t state_get_reg(spike_state_t *state, uint32_t index, bool is_fp) {
+  if (is_fp) {
+    auto &fr = state->s->FPR;
+    return extract_f32(fr[index]);
+  }
+  auto &xr = state->s->XPR;
+  return (uint32_t)xr[index];
+}
+
+uint32_t state_get_reg_write_size(spike_state_t *state) {
+  reg_write_index_vec.clear();
+  for (auto [idx, data] : state->s->log_reg_write) {
+    reg_write_index_vec.push_back(idx);
+  }
+  return state->s->log_reg_write.size();
+}
+
+uint32_t state_get_reg_write_index(spike_state_t *state, uint32_t index) {
+  return reg_write_index_vec[index];
+}
+
+uint32_t state_get_mem_write_size(spike_state_t *state) {
+  return state->s->log_mem_write.size();
+}
+
+uint32_t state_get_mem_write_addr(spike_state_t *state, uint32_t index) {
+  return std::get<0>(state->s->log_mem_write[index]) & 0xffffffff;
+}
+
+uint64_t state_get_mem_write_value(spike_state_t *state, uint32_t index) {
+  return std::get<1>(state->s->log_mem_write[index]);
+}
+
+uint8_t state_get_mem_write_size_by_byte(spike_state_t *state, uint32_t index) {
+  return std::get<2>(state->s->log_mem_write[index]);
+}
+
+uint32_t state_get_mem_read_size(spike_state_t *state) {
+  return state->s->log_mem_read.size();
+}
+
+uint32_t state_get_mem_read_addr(spike_state_t *state, uint32_t index) {
+  return std::get<0>(state->s->log_mem_read[index]) & 0xffffffff;
+}
+
+uint8_t state_get_mem_read_size_by_byte(spike_state_t *state, uint32_t index) {
+  return std::get<2>(state->s->log_mem_read[index]);
+}
+
+reg_t state_exit(spike_state_t *state) {
+  auto &csrmap = state->s->csrmap;
+  return csrmap[CSR_MSIMEND]->read();
+}
+
+void spike_register_callback(void *ffi_target_, ffi_callback callback) {
+  ffi_addr_to_mem = callback;
+  ffi_target = ffi_target_;
+
+  return;
+}
+
+void spike_destruct(spike_t *spike) { delete spike; }
+
+void proc_destruct(spike_processor_t *proc) { delete proc; }
+
+void state_destruct(spike_state_t *state) { delete state; }
