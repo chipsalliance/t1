18d36083eb9b2b41956e1b84ca11da6ac9f094a2
Clo91eaf <Clo91eaf@qq.com>
2024-08-13 17:37:08 +0800

    [t1rocket] add rtl unexcepted quit check in difftest

--- t1rocketemu/online_drive/verilator_shim/verilator_shim.cc
+++ t1rocketemu/online_drive/verilator_shim/verilator_shim.cc
@@ -6,15 +6,21 @@ class VTestBench;
 extern "C" int verilator_main_c(int argc, char **argv) {
   // Setup context, defaults, and parse command line
   Verilated::debug(0);
-  VerilatedContext* contextp = new VerilatedContext();
+
+  // use unique_ptr to ensure deletion of context and model
+  std::unique_ptr<VerilatedContext> contextp;
+  std::unique_ptr<VTestBench> topp;
+
+  // Construct the Verilated context
+  contextp = std::make_unique<VerilatedContext>();
+  // Construct the Verilated model, from Vtop.h generated from Verilating
+  topp = std::make_unique<VTestBench>(contextp.get());
+
   contextp->fatalOnError(false);
   contextp->commandArgs(argc, argv);
 #ifdef VM_TRACE
   contextp->traceEverOn(true);
 #endif
-
-  // Construct the Verilated model, from Vtop.h generated from Verilating
-  VTestBench* topp = new VTestBench(contextp);
 
   // Simulate until $finish
   while (!contextp->gotFinish()) {
@@ -28,13 +34,16 @@ extern "C" int verilator_main_c(int argc, char **argv) {
 
   if (!contextp->gotFinish()) {
     VL_DEBUG_IF(VL_PRINTF("+ Exiting without $finish; no events left\n"););
+    return 1;
+  }
+
+  if (contextp->gotError()) {
+    VL_DEBUG_IF(VL_PRINTF("+ Exiting due to errors\n"););
+    return 1;
   }
 
   // Final model cleanup
   topp->final();
-
-  delete topp;
-  delete contextp;
 
   return 0;
 }
