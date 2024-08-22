1f036e4546958f7a4e3b1fb3a7b2327fe29a6298
Avimitin <dev@avimit.in>
2024-08-22 21:36:50 +0000

    [deps] Bump T1 dependencies

--- nix/t1/_sources/generated.nix
+++ nix/t1/_sources/generated.nix
@@ -27,15 +27,15 @@
   };
   chisel = {
     pname = "chisel";
-    version = "1d50b782cab542ca8c1822a59f25033e5adfc93f";
+    version = "5c129120ba06229e6c5d28c8120b35166770fa09";
     src = fetchFromGitHub {
       owner = "chipsalliance";
       repo = "chisel";
-      rev = "1d50b782cab542ca8c1822a59f25033e5adfc93f";
+      rev = "5c129120ba06229e6c5d28c8120b35166770fa09";
       fetchSubmodules = false;
-      sha256 = "sha256-Yg8gCwOIYoBb/pehHVBJIzFIXp77qB+D5n4XQaceAtg=";
+      sha256 = "sha256-6BQKesJGI93xG7CxblM1YKAv6eN2U+EEcwlGsgMUTeI=";
     };
-    date = "2024-08-12";
+    date = "2024-08-21";
   };
   chisel-interface = {
     pname = "chisel-interface";
