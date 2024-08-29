d177766909dae8c362de2527ccb15f47fbde0cbc
Shupei Fan <dymarkfan@outlook.com>
2024-08-29 13:21:28 +0000

    [difftest] add dpi_common crate

---
+++ difftest/dpi_common/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod dpi_target;
+pub mod dump;
+pub mod plusarg;
+
+pub use dpi_target::DpiTarget;
+
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+pub fn setup_logger() {
+  let global_logger = FmtSubscriber::builder()
+    .with_env_filter(EnvFilter::from_default_env()) // default level: error
+    .without_time()
+    .with_target(false)
+    .with_ansi(true)
+    .compact()
+    .finish();
+  tracing::subscriber::set_global_default(global_logger)
+    .expect("internal error: fail to setup log subscriber");
+}
