fc7d9bc5d5a3d4e7dc66b4920a14c3c2e66e3b08
Shupei Fan <dymarkfan@outlook.com>
2024-09-05 14:07:47 +0000

    [difftest] unsuccessful simulation will panic in final block
    can be suppressed by setting 'T1_SUPPRESS_PANIC_IN_FINAL=1'

--- difftest/dpi_common/src/util.rs
+++ difftest/dpi_common/src/util.rs
@@ -1,6 +1,12 @@
 //! utility functions
 
 pub fn write_perf_json(cycle: u64, success: bool) {
+  // unsuccessful simulation will panic by default.
+  // However, it could be suppressed by setting env T1_SUPPRESS_PANIC_IN_FINAL=1
+  if !success && !suppress_panic_in_final() {
+    panic!("simulation ends unsuccessfully [T={cycle}]");
+  }
+
   let mut content = String::new();
   content += "{\n";
   content += &format!("    \"total_cycles\": {cycle},\n");
@@ -13,4 +19,8 @@ pub fn write_perf_json(cycle: u64, success: bool) {
       tracing::error!("failed to write 'perf.json': {e}");
     }
   }
+}
+
+fn suppress_panic_in_final() -> bool {
+  std::env::var("T1_SUPPRESS_PANIC_IN_FINAL").as_deref() == Ok("1")
 }
