c5f3ccd9a6cd9941bed14657a5c371bc7f564d9b
Avimitin <dev@avimit.in>
2024-10-12 18:04:24 +0800

    [nix] use latest elaborator cmdline for chisel-to-mlirbc
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/t1/conversion/chisel-to-mlirbc.nix
+++ nix/t1/conversion/chisel-to-mlirbc.nix
@@ -8,38 +8,40 @@
 }:
 
 { outputName
-, elaboratorArgs ? null
+, generatorClassName
+, elaboratorArgs
 }:
 
-assert lib.assertMsg (elaboratorArgs != null) "elaborateArgs is not set";
-assert lib.assertMsg (builtins.typeOf elaboratorArgs == "list") "elaborateArgs is not a list";
-assert lib.assertMsg (!(lib.elem "--target-dir" elaboratorArgs)) "--target-dir is set internally, please remove it from top scope";
+assert lib.assertMsg (builtins.typeOf elaboratorArgs == "string") "elaborateArgs should be string";
+assert lib.assertMsg (builtins.typeOf generatorClassName == "string") "arg `generator` should be string";
+assert lib.assertMsg (lib.hasPrefix "org.chipsalliance.t1" generatorClassName) "Wrong generator name ${generatorClassName}";
+assert lib.assertMsg (!(lib.hasInfix generatorClassName elaboratorArgs)) "Duplicated generator name in elaboratorArgs";
 
-let
-  elaborateArgStr = lib.escapeShellArgs elaboratorArgs;
-in
 stdenvNoCC.mkDerivation {
   name = outputName;
 
   nativeBuildInputs = [ espresso circt ];
 
-  elaborateOutDir = "elaborate";
   buildCommand = ''
-    mkdir -p "$out" "$elaborateOutDir"
+    mkdir -p "$out"
 
-    # ---------------------------------------------------------------------------------
-    # Run elaborator
-    # ---------------------------------------------------------------------------------
-    elaborateProc="${elaborator}/bin/elaborator ${elaborateArgStr} --target-dir $elaborateOutDir"
-    echo "[nix] running elaborator: $elaborateProc"
-    $builder -c "$elaborateProc"
+    mkdir stage1 && pushd stage1
+
+    elaborateProc="${elaborator}/bin/elaborator ${generatorClassName} ${elaboratorArgs}"
+    echo "[nix] Generating config with cmd opt: $elaborateProc"
+    eval "$elaborateProc"
+    popd
+
+    mkdir stage2 && pushd stage2
+    echo "[nix] Elaborating with generated config"
+    ${elaborator}/bin/elaborator ${generatorClassName} design --parameter ../stage1/*.json
 
     # ---------------------------------------------------------------------------------
     # Run circt toolchain
     # ---------------------------------------------------------------------------------
     echo "[nix] elaborate finish, parsing output with firtool"
-    firtool elaborate/*.fir \
-      --annotation-file elaborate/*.anno.json \
+    firtool *.fir \
+      --annotation-file *.json \
       --emit-bytecode \
       --parse-only \
       -o $out/$name
