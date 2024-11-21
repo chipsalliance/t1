e93af2dec94a113bf34f5f0c8ef60becfc96ee93
SharzyL <me@sharzy.in>
2024-11-21 16:18:24 +0000

    [cases] add eval.mmm_mem (to be fixed)

---
+++ tests/eval/_mmm_mem/default.nix
@@ -0,0 +1,32 @@
+{ linkerScript
+, makeBuilder
+, t1main
+}:
+
+let
+  builder = makeBuilder { casePrefix = "eval"; };
+  build_ntt = caseName /* must be consistent with attr name */ : len: kernel_src:
+    builder {
+      caseName = caseName;
+
+      src = ./.;
+
+      passthru.featuresRequired = { };
+
+      buildPhase = ''
+        runHook preBuild
+
+        $CC -T${linkerScript} -DLEN=${toString len} \
+          ${./mmm_main.c} ${kernel_src} \
+          ${t1main} \
+          -o $pname.elf
+
+        runHook postBuild
+      '';
+
+      meta.description = "test case 'ntt'";
+    };
+
+in {
+  mmm_mem_4096_vl4096 = build_ntt "mmm_mem_4096_vl4096" 4096 ./mmm_4096_vl4096.S;
+}
