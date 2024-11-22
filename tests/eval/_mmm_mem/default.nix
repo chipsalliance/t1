fe9c7e4e9f297349f15bb9e4a47941d9d014443a
SharzyL <me@sharzy.in>
2024-11-22 12:31:25 +0800

    [cases] add more config for eval.mmm_mem*

--- tests/eval/_mmm_mem/default.nix
+++ tests/eval/_mmm_mem/default.nix
@@ -1,11 +1,12 @@
-{ linkerScript
+{ lib
+, linkerScript
 , makeBuilder
 , t1main
 }:
 
 let
   builder = makeBuilder { casePrefix = "eval"; };
-  build_ntt = caseName /* must be consistent with attr name */ : len: kernel_src:
+  build_mmm = caseName /* must be consistent with attr name */: bn: vl:
     builder {
       caseName = caseName;
 
@@ -16,8 +17,8 @@ let
       buildPhase = ''
         runHook preBuild
 
-        $CC -T${linkerScript} -DLEN=${toString len} \
-          ${./mmm_main.c} ${kernel_src} \
+        $CC -T${linkerScript} -DLEN=${toString bn} \
+          ${./mmm_main.c} ./mmm_${toString bn}_vl${toString vl}.S \
           ${t1main} \
           -o $pname.elf
 
@@ -26,8 +27,14 @@ let
 
       meta.description = "test case 'ntt'";
     };
-
-in {
-  mmm_mem_512_vl4096 = build_ntt "mmm_mem_512_vl4096" 4096 ./mmm_512_vl4096.S;
-  mmm_mem_256_vl4096 = build_ntt "mmm_mem_256_vl4096" 4096 ./mmm_256_vl4096.S;
-}
+  configs = lib.cartesianProduct { bn = [ 256 512 ]; vl = [ 128 256 4096 ]; };
+in
+builtins.listToAttrs (builtins.map
+  (
+    { bn, vl }:
+    let name = "mmm_mem_${toString bn}_vl${toString vl}"; in
+    lib.nameValuePair
+      name
+      (build_mmm name bn vl)
+  )
+  configs)
