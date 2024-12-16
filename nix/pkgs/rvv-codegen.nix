27769bc30ef96d96da01dda41bc555412f03b477
Avimitin <dev@avimit.in>
2024-12-17 01:51:52 +0800

    [tests] bump rvv-codegen
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/pkgs/rvv-codegen.nix
+++ nix/pkgs/rvv-codegen.nix
@@ -1,29 +1,22 @@
 { fetchFromGitHub, buildGoModule }:
-let
-  riscv-test-env = fetchFromGitHub {
-    owner = "riscv";
-    repo = "riscv-test-env";
-    rev = "1c577dc7c7d6aee27b8d5cb0e2e87c8473e3ad12";
-    hash = "sha256-JZ7Yn4cTel9uVo6uGu0zs3IMMySoGzjSMW544YLYips=";
-  };
-in
 buildGoModule {
   pname = "riscv-vector-test";
   version = "unstable-2023-04-12";
   src = fetchFromGitHub {
     owner = "chipsalliance";
     repo = "riscv-vector-tests";
-    rev = "6f24d9aab1e64a452e3493c85297e9bde805e30c";
-    hash = "sha256-5Ie5gZMOqxl8L8tuzpYdrqb/7F6o5UzOMGhn4xzPvQo=";
+    rev = "52ccb798b355d4442f5d86a710e6bfa0eeb20cc2";
+    hash = "sha256-vfr7iMkqy3QUjkfM1by4RYUMpPHIsOk0XZSW8Big39s=";
+    fetchSubmodules = true;
   };
   doCheck = false;
-  vendorHash = "sha256-9cQlivpHg6IDYpmgBp34n6BR/I0FIYnmrXCuiGmAhNE=";
+  vendorHash = "sha256-1A5yCj9AJHp9tcUIpLKamXn4Tl3KPFEtzke5X7h6V+4=";
   # Get rid of copying the whole source
   postInstall = ''
     cp -r $src/configs $out/configs
 
     mkdir $out/include
-    cp ${riscv-test-env}/encoding.h $out/include
+    cp env/riscv-test-env/encoding.h $out/include
   '';
 }
 
