6243e3ddc7f88eadbfc4d75f0dfd7ab5def43e39
Avimitin <dev@avimit.in>
2025-01-09 02:20:09 +0800

    [emurt] add simple write char test
    Signed-off-by: Avimitin <dev@avimit.in>

--- tests/default.nix
+++ tests/default.nix
@@ -90,11 +90,12 @@ let
     rvv_bench = casesSelf.callPackage ./rvv_bench { };
     pytorch = casesSelf.callPackage ./pytorch { };
     disp = casesSelf.callPackage ./disp { };
+    emurt-test = casesSelf.callPackage ./emurt/tests { };
   }));
 
   # remove non-case attributes in scope
   scopeStripped = {
-    inherit (scope) mlir intrinsic asm perf codegen rvv_bench pytorch disp;
+    inherit (scope) mlir intrinsic asm perf codegen rvv_bench pytorch disp emurt-test;
   };
 
   # This derivation is for internal CI use only.
