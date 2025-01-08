698141e44d78396a20b06dff53138932f5d4607a
Avimitin <dev@avimit.in>
2025-01-09 01:47:55 +0800

    [chore] format code
    Signed-off-by: Avimitin <dev@avimit.in>

--- tests/emurt/emurt.h
+++ tests/emurt/emurt.h
@@ -3,12 +3,11 @@
 #include <stdint.h>
 
 struct uartlite_regs {
-    volatile unsigned int rx_fifo;
-    volatile unsigned int tx_fifo;
-    volatile unsigned int status;
-    volatile unsigned int control;
+  volatile unsigned int rx_fifo;
+  volatile unsigned int tx_fifo;
+  volatile unsigned int status;
+  volatile unsigned int control;
 };
 
 void get(char *s, int n);
 void print_s(const char *c);
-
