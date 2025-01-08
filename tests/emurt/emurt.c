f0b6d954fde45d197d5172d91dc5720a3be05f2e
Avimitin <dev@avimit.in>
2025-01-09 02:19:49 +0800

    [emurt] update write design and add new program instrument function
    Signed-off-by: Avimitin <dev@avimit.in>

--- tests/emurt/emurt.c
+++ tests/emurt/emurt.c
@@ -1,54 +1,19 @@
 #include "emurt.h"
 
+#define EXIT_REG 0x10000000
+#define UART_W_REG 0x10000010
+#define PERF_REG 0x10000014
+
+void t1_put_char(char c) { *(uint32_t *)(UART_W_REG) = (uint32_t)c; }
+void place_counter(int i) { *(int *)(PERF_REG) = i; }
+
 ///////////////////////
 // uart
 ///////////////////////
-
-struct uartlite_regs *const ttyUL0 = (struct uartlite_regs *)0x10000000;
-
-#define SR_TX_FIFO_FULL (1 << 3)       /* transmit FIFO full */
-#define SR_TX_FIFO_EMPTY (1 << 2)      /* transmit FIFO empty */
-#define SR_RX_FIFO_VALID_DATA (1 << 0) /* data in receive FIFO */
-#define SR_RX_FIFO_FULL (1 << 1)       /* receive FIFO full */
-
-#define ULITE_CONTROL_RST_TX 0x01
-#define ULITE_CONTROL_RST_RX 0x02
-
-void uart_put_c(char c) {
-  while (ttyUL0->status & SR_TX_FIFO_FULL)
-    ;
-  ttyUL0->tx_fifo = c;
-}
-
-char uart_check_read() { // 1: data ready, 0: no data
-  return (ttyUL0->status & SR_RX_FIFO_VALID_DATA) != 0;
-}
-
-char uart_get_c() { return ttyUL0->rx_fifo; }
-
-void get(char *s, int n) {
-  char debug[20] = "Enter get()";
-  print_s(debug);
-  int i = 0;
-  while (uart_check_read() != 1)
-    ;
-  while (uart_check_read() && i < n - 1) {
-    print_s(debug);
-    char c = uart_get_c();
-    uart_put_c(c);
-    if (c == '\r' || c == '\n') {
-      break; // Break if carriage return or newline is encountered
-    }
-    *(s + i) = c;
-    i++;
-  }
-  s[i] = '\0';
-}
-
 int _write(int file, char *ptr, int len) {
   int i = 0;
   for (; i < len; i++) {
-    uart_put_c(ptr[i]);
+    t1_put_char(ptr[i]);
   }
   return i;
 }
@@ -63,7 +28,7 @@ void _exit(int code) {
 
 void print_s(const char *c) {
   while (*c) {
-    uart_put_c(*c);
+    t1_put_char(*c);
     c++;
   }
 }
