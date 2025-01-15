460c563f9ffda273aacb7a36a309753d9ef7195a
Avimitin <dev@avimit.in>
2025-01-15 16:07:19 +0800

    [docker] fix example destination directory name
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/t1/release/docker-image.nix
+++ nix/t1/release/docker-image.nix
@@ -1,14 +1,10 @@
 { lib
   # build deps
 , dockerTools
-, buildEnv
-, runCommand
-, runtimeShell
 
   # Runtime deps
 , bashInteractive
 , which
-, stdenv
 , jq
 , coreutils
 , findutils
@@ -32,49 +28,53 @@
 }:
 
 let
-  self = dockerTools.streamLayeredImage {
-    name = "chipsalliance/t1-${configName}";
-    tag = "latest";
-
-    contents = with dockerTools; [
-      usrBinEnv
-      binSh
-      bashInteractive
-      which
-      rv32-stdenv.cc
-      coreutils
-      findutils
-      diffutils
-      gnused
-      gnugrep
-      gawk
-      gnutar
-      gzip
-      bzip2
-      gnumake
-      patch
-      xz
-      file
-      verilator-emu
-    ];
-
-    enableFakechroot = true;
-    fakeRootCommands = ''
-      echo "Start finalizing rootfs"
-      mkdir -p /tmp
-
-      echo "Creating testcase directory"
-      mkdir -p /workspace/examples
-      pushd /workspace/examples
-      cp -r ${cases.intrinsic.matmul.src} .
-      cp -r ${cases.intrinsic.linear_normalization.src} .
-      cp -r ${cases.asm.strlen.src} .
-      popd
-    '';
-
-    config = {
-      WorkingDir = "/workspace";
-    };
-  };
+  selectedCases = with cases; [
+    intrinsic.matmul
+    intrinsic.linear_normalization
+    asm.strlen
+    disp.simple
+  ];
 in
-self
+
+dockerTools.streamLayeredImage {
+  name = "chipsalliance/t1-${configName}";
+  tag = "latest";
+
+  contents = with dockerTools; [
+    usrBinEnv
+    binSh
+    bashInteractive
+    which
+    rv32-stdenv.cc
+    coreutils
+    findutils
+    diffutils
+    gnused
+    gnugrep
+    gawk
+    gnutar
+    gzip
+    bzip2
+    gnumake
+    patch
+    xz
+    file
+    verilator-emu
+    jq
+  ];
+
+  enableFakechroot = true;
+  fakeRootCommands = ''
+    echo "Start finalizing rootfs"
+    mkdir -p /tmp
+
+    echo "Creating testcase directory"
+    mkdir -p /workspace/examples
+    ${map (drv: "cp -r ${drv.src} /workspace/example/${drv.caseName}") selectedCases}
+    mkdir -p /workspace/share
+  '';
+
+  config = {
+    WorkingDir = "/workspace";
+  };
+}
