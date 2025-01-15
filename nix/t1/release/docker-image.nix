e734ccc5bf0303564faed3a60b27d092ff8ed103
Avimitin <dev@avimit.in>
2025-01-15 17:40:06 +0800

    [docker] add t1-cc to handle default CFLAGS
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/t1/release/docker-image.nix
+++ nix/t1/release/docker-image.nix
@@ -1,6 +1,8 @@
 { lib
   # build deps
 , dockerTools
+, makeWrapper
+, runCommand
 
   # Runtime deps
 , bashInteractive
@@ -22,6 +24,8 @@
 
   # T1 Stuff
 , rv32-stdenv
+, emurt
+, rtlDesignMetadata
 , verilator-emu
 , cases
 , configName
@@ -34,6 +38,42 @@ let
     asm.strlen
     disp.simple
   ];
+
+  NIX_CFLAGS_COMPILE =
+    let
+      march = lib.pipe rtlDesignMetadata.march [
+        (lib.splitString "_")
+        (map (ext: if ext == "zvbb" then "zvbb1" else ext))
+        (lib.concatStringsSep "_")
+      ];
+    in
+    toString ([
+      "-I${emurt}/include"
+      "-L${emurt}/lib"
+      "-mabi=ilp32f"
+      "-march=${march}"
+      "-mno-relax"
+      "-static"
+      "-mcmodel=medany"
+      "-fvisibility=hidden"
+      "-fno-PIC"
+      "-g"
+      "-O3"
+    ] ++ lib.optionals (lib.elem "zvbb" (lib.splitString "_" rtlDesignMetadata.march)) [ "-menable-experimental-extensions" ]);
+
+  t1-cc =
+    let
+      cc-prefix-safe = lib.replaceStrings [ "-" ] [ "_" ] rv32-stdenv.targetPlatform.config;
+    in
+    runCommand "${rv32-stdenv.cc.pname}-${rv32-stdenv.cc.version}-t1-wrapped"
+      { nativeBuildInputs = [ makeWrapper ]; env = { inherit NIX_CFLAGS_COMPILE; }; }
+      ''
+        mkdir -p $out/bin
+        makeWrapper ${rv32-stdenv.cc}/bin/${rv32-stdenv.targetPlatform.config}-cc $out/bin/t1-cc \
+          --set "NIX_CFLAGS_COMPILE_${cc-prefix-safe}" "$NIX_CFLAGS_COMPILE"
+        makeWrapper ${rv32-stdenv.cc}/bin/${rv32-stdenv.targetPlatform.config}-c++ $out/bin/t1-c++ \
+          --set "NIX_CFLAGS_COMPILE_${cc-prefix-safe}" "$NIX_CFLAGS_COMPILE"
+      '';
 in
 
 dockerTools.streamLayeredImage {
@@ -46,6 +86,7 @@ dockerTools.streamLayeredImage {
     bashInteractive
     which
     rv32-stdenv.cc
+    t1-cc
     coreutils
     findutils
     diffutils
@@ -70,11 +111,24 @@ dockerTools.streamLayeredImage {
 
     echo "Creating testcase directory"
     mkdir -p /workspace/examples
-    ${map (drv: "cp -r ${drv.src} /workspace/example/${drv.caseName}") selectedCases}
+    ${lib.concatStringsSep "\n" (
+      # I don't know why I can't cp -r here. I can only workaround these permission issue with mkdir myself.
+      map (drv: ''
+        mkdir -p /workspace/examples/${drv.pname}
+        cp ${drv.src}/* /workspace/examples/${drv.pname}/
+      '') selectedCases
+    )}
+
     mkdir -p /workspace/share
+    cp ${../../../tests/t1.ld} /workspace/share/t1.ld
+    cp ${../../../tests/t1_main.S} /workspace/share/main.S
   '';
 
   config = {
     WorkingDir = "/workspace";
+  };
+
+  passthru = {
+    inherit t1-cc;
   };
 }
