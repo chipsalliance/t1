32248a7fea3f491bec2222532735d8689eca6a57
Avimitin <dev@avimit.in>
2025-01-20 18:28:41 +0800

    [nix] refactor run-vcs-emu
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/t1/run/run-vcs-emu.nix
+++ nix/t1/run/run-vcs-emu.nix
@@ -6,6 +6,7 @@
 , snps-fhs-env
 , writeShellScriptBin
 , python3
+, runCoverHook
 }:
 
 { emulator
@@ -18,110 +19,115 @@ assert lib.assertMsg (!emulator.isRuntimeLoad || (dpilib != null)) "dpilib must 
 
 stdenvNoCC.mkDerivation (rec {
   name = "${testCase.pname}-vcs-result" + (lib.optionalString emulator.enableTrace "-trace");
-  nativeBuildInputs = [ zstd jq python3 ];
+  nativeBuildInputs = [
+    zstd
+    jq
+    python3
+  ] ++ lib.optionals (emulator.enableCover) [
+    runCoverHook
+    snps-fhs-env
+  ];
   __noChroot = true;
 
   passthru = {
-    caseName = testCase.pname;
-
     # to open 'profileReport.html' in firefox,
     # set 'security.fileuri.strict_origin_policy = false' in 'about:config'
     profile = writeShellScriptBin "runSimProfile" ''
-      ${emuDriver} \
+      ${lib.getExe emulator} \
         ${lib.escapeShellArgs emuDriverArgs} \
         -simprofile time \
         2> ${testCase.pname}-rtl-event.jsonl
     '';
   };
 
-  emuDriver = "${emulator}/bin/${emulator.mainProgram}";
-  emuDriverArgs = lib.optionals emulator.isRuntimeLoad [
-    "-sv_root"
-    "${dpilib}/lib"
-    "-sv_lib"
-    "${dpilib.svLibName}"
-  ]
-  ++ [
-    "-exitstatus"
-    "-assert"
-    "global_finish_maxfail=10000"
-    "+t1_elf_file=${testCase}/bin/${testCase.pname}.elf"
-  ]
-  ++ lib.optionals emulator.enableCover [
-    "-cm"
-    "assert"
-    "-assert"
-    "hier=${testCase}/${testCase.pname}.cover"
-  ]
-  ++ lib.optionals emulator.enableTrace [
-    "+t1_wave_path=${testCase.pname}.fsdb"
-  ];
+  caseName = testCase.pname;
+  emuDriverArgs =
+    assert lib.assertMsg (emulator ? enableProfile && emulator.enableProfile)
+      "ERROR: emulator provided has `profile` feature enable, \
+        which is inherently nondetermistic, use '${name}.profile --impure' instead";
+    lib.escapeShellArgs (lib.optionals emulator.isRuntimeLoad [
+      "-sv_root"
+      "${dpilib}/lib"
+      "-sv_lib"
+      "${dpilib.svLibName}"
+    ]
+    ++ [
+      "-exitstatus"
+      "-assert"
+      "global_finish_maxfail=10000"
+      "+t1_elf_file=${testCase}/bin/${testCase.pname}.elf"
+    ]
+    ++ lib.optionals emulator.enableCover [
+      "-cm"
+      "assert"
+      "-assert"
+      "hier=${testCase}/${testCase.pname}.cover"
+    ]
+    ++ lib.optionals emulator.enableTrace [
+      "+t1_wave_path=${testCase.pname}.fsdb"
+    ]);
 
-  buildCommand = ''
-    ${lib.optionalString emulator.enableProfile ''
-      echo "ERROR: 'enableProfile = true' is inherently nondetermistic"
-      echo "  use 'nix run <...>.profile --impure' instead" 
-      exit 1
-    ''}
+  offlineCheckArgs = lib.escapeShellArgs ([
+    "--elf-file"
+    "${testCase}/bin/${testCase.pname}.elf"
+    "--log-file"
+    "$rtlEventOutPath"
+    "--log-level"
+    "ERROR"
+  ]);
+
+  buildPhase = ''
+    runHook preBuild
 
     mkdir -p "$out"
 
-    emuDriverArgs="${lib.escapeShellArgs emuDriverArgs}"
-
-    rtlEventOutPath="$out/${testCase.pname}-rtl-event.jsonl"
-
-    echo "[nix] Running VCS ${testCase.pname} with args $emuDriverArgs"
-
-    printError() {
-      echo -e "\033[0;31m[nix]\033[0m: online driver run failed"
-      cat $rtlEventOutPath
-      echo -e "\033[0;31m[nix]\033[0m: Try rerun with '\033[0;34m$emuDriver $emuDriverArgs\033[0m'"
+    fatal() {
+      local msg="$1"; shift
+      printf "\033[0;31m[nix]\033[0m: $msg\n" "$@" >&2
       exit 1
     }
 
+    echo "[nix] Running VCS for $caseName with args $emuDriverArgs"
     export RUST_BACKTRACE=full
+    rtlEventOutPath="$out/$caseName-rtl-event.jsonl"
 
-    "$emuDriver" $emuDriverArgs 1> >(tee $out/online-drive-emu-journal) 2>$rtlEventOutPath || printError
+    if ! "${lib.getExe emulator}" $emuDriverArgs \
+      1> >(tee $out/online-drive-emu-journal) \
+      2>$rtlEventOutPath
+    then
+      fatal "online driver run failed: %s\n\n%s" \
+        "$(cat "$rtlEventOutPath")" \
+        "Try rerun with '$emuDriver $emuDriverArgs'"
+    fi
 
-    echo "[nix] VCS run done"
+    echo "[nix] $name run done"
 
     if [ ! -r "$rtlEventOutPath" ]; then
-      echo -e "[nix] \033[0;31mInternal Error\033[0m: no $rtlEventOutPath found in output"
-      exit 1
+      fatal "no $rtlEventOutPath found in output"
     fi
 
     if ! jq --stream -c -e '.[]' "$rtlEventOutPath" >/dev/null 2>&1; then
-      echo -e "[nix] \033[0;31mInternal Error\033[0m: invalid JSON file $rtlEventOutPath, showing original file:"
-      echo "--------------------------------------------"
-      cat $rtlEventOutPath
-      echo "--------------------------------------------"
-      exit 1
+      fatal "invalid JSON file $rtlEventOutPath"
     fi
 
-    set +e
-    offlineCheckArgsArray=(
-      "--elf-file"
-      "${testCase}/bin/${testCase.pname}.elf"
-      "--log-file"
-      "$rtlEventOutPath"
-      "--log-level"
-      "ERROR"
-    )
-    offlineCheckArgs="''${offlineCheckArgsArray[@]}"
-    echo -e "[nix] running offline check: \033[0;34m${emulator}/bin/offline $offlineCheckArgs\033[0m"
-    "${offline-checker}/bin/offline" $offlineCheckArgs &> >(tee $out/offline-check-journal)
-
-    printf "$?" > $out/offline-check-status
-    if [ "$(cat $out/offline-check-status)" != "0" ]; then
-      echo "[nix] Offline check FAIL"
-    else
+    echo -e "[nix] running offline check"
+    if "${lib.getExe offline-checker}" $offlineCheckArgs &> >(tee $out/offline-check-journal); then
+      printf '0' > $out/offline-check-status
       echo "[nix] Offline check PASS"
+    else
+      printf '1' > $out/offline-check-status
+      echo "[nix] Offline check FAIL"
     fi
-    set -e
 
     echo "[nix] compressing event log"
     zstd $rtlEventOutPath -o $rtlEventOutPath.zstd
     rm $rtlEventOutPath
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
 
     if [ -r perf.json ]; then
       cp -v perf.json $out/
@@ -133,15 +139,12 @@ stdenvNoCC.mkDerivation (rec {
       jq ".profile_total_cycles=$(python3 ${./calculate-cycle.py})" perf.json > "$out/perf.json"
     fi
 
-    ${lib.optionalString emulator.enableCover ''
-      ${snps-fhs-env}/bin/snps-fhs-env -c "urg -dir cm.vdb -format text -metric assert -show summary"
-      # TODO: add a flag to specify 'vdb only generated in ci mode'
-      cp -vr cm.vdb $out/
-      cp -vr urgReport $out/
+    ${lib.optionalString emulator.enableTrace ''
+      if [ -r "${testCase.pname}.fsdb" ]; then
+        cp -v "${testCase.pname}.fsdb" "$out"
+      fi
     ''}
 
-    ${lib.optionalString emulator.enableTrace ''
-      cp -v ${testCase.pname}.fsdb "$out"
-    ''}
+    runHook postInstall
   '';
 })
