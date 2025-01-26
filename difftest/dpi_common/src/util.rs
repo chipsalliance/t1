f68a1168cc1c3117b8660dd82c2f850ce7f19d09
Shupei Fan <dymarkfan@outlook.com>
2025-01-26 15:17:27 +0000

    [difftest] record metadata for offline in online dpi

--- difftest/dpi_common/src/util.rs
+++ difftest/dpi_common/src/util.rs
@@ -1,14 +1,40 @@
 //! utility functions
 
-pub fn write_perf_json(cycle: u64, success: bool) {
+use std::path::Path;
+
+pub struct MetaConfig {
+  pub vlen: u32,
+  pub dlen: u32,
+  pub isa: String,
+  pub elf_file: Option<String>,
+}
+
+pub fn write_perf_json(flavor: &str, cycle: u64, success: bool, meta: &MetaConfig) {
   // unsuccessful simulation will panic by default.
   // However, it could be suppressed by setting env T1_SUPPRESS_PANIC_IN_FINAL=1
   if !success && !suppress_panic_in_final() {
     panic!("simulation ends unsuccessfully [T={cycle}]");
   }
 
+  // we construct json manually,
+  // to avoid pull extra dependencies
+
   let mut content = String::new();
   content += "{\n";
+  content += &format!("    \"flavor\": \"{}\",\n", flavor.escape_default());
+  content += &format!("    \"meta_vlen\": {},\n", meta.vlen);
+  content += &format!("    \"meta_dlen\": {},\n", meta.dlen);
+  content += &format!("    \"meta_isa\": \"{}\",\n", meta.isa.escape_default());
+  if let Some(elf_file) = &meta.elf_file {
+    // we record it for offline use,
+    // relative path is not very useful for offline
+    if Path::new(elf_file).is_absolute() {
+      content += &format!(
+        "    \"meta_elf_file\": \"{}\",\n",
+        elf_file.escape_default()
+      );
+    }
+  }
   content += &format!("    \"total_cycles\": {cycle},\n");
   content += &format!("    \"success\": {success}\n");
   content += "}\n";
