f85a25fb63f19623e38df248775104b9035b5087
Avimitin <dev@avimit.in>
2025-02-07 15:26:47 +0800

    [nix] bump mill to 0.12.5-unstable
    Signed-off-by: Avimitin <dev@avimit.in>

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -40,11 +40,15 @@ rec {
   snps-fhs-env = final.callPackage ./pkgs/snps-fhs-env.nix { };
 
   mill = let jre = final.jdk21; in
-    (prev.mill.override { inherit jre; }).overrideAttrs (_: {
+    (prev.mill.override { inherit jre; }).overrideAttrs {
+      # Fixed the buggy sorting issue in target resolve
+      version = "unstable-0.12.5-173-15dded";
+      src = final.fetchurl {
+        url = "https://github.com/com-lihaoyi/mill/releases/download/0.12.5/0.12.5-173-15dded-assembly";
+        hash = "sha256-xP59tONOu0CG5Gce4ru+st5KUH7Wcd10d/pQdELjSJM=";
+      };
       passthru = { inherit jre; };
-      # --interactive implies --no-server
-      postInstall = ''wrapProgram $out/bin/mill --add-flags "--interactive"'';
-    });
+    };
 
   # some symbols in newlib libgloss uses ecall, which does not work in emulator
   # emurt provides hand-written implementations for these symbols
