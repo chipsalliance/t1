c646004b214f2d1123edcea71e6513ae49577ccc
Avimitin <dev@avimit.in>
2025-02-07 15:35:53 +0800

    [nix] use mill-flows overlay in chisel-nix
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -4,17 +4,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-for-circt.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    chisel-nix.url = "github:chipsalliance/chisel-nix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixpkgs-for-circt }@inputs:
+  outputs = { self, nixpkgs, chisel-nix, flake-utils, nixpkgs-for-circt }@inputs:
     let
       overlay = import ./nix/overlay.nix { inherit self; };
     in
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+          pkgs = import nixpkgs { inherit system; overlays = [ chisel-nix.overlays.mill-flows overlay ]; };
         in
         {
           legacyPackages = pkgs;
