598b8fa71cbb07b55477e8252b5373d5ab6a8db5
Avimitin <dev@avimit.in>
2025-03-18 02:02:07 +0800

    [ci] enable bump script
    Signed-off-by: Avimitin <dev@avimit.in>

--- dependencies/prepare-t1.sh
+++ dependencies/prepare-t1.sh
@@ -3,5 +3,5 @@
 set -e
 
 ivyLocal=$(nix build '.#t1.submodules.ivyLocalRepo' --no-link --print-out-paths --max-jobs auto)
-export JAVA_TOOL_OPTIONS="-Dcoursier.ivy.home=$ivyLocal -Divy.home=$ivyLocal"
+export JAVA_TOOL_OPTIONS="-Dcoursier.ivy.home=$ivyLocal -Divy.home=$ivyLocal $JAVA_TOOL_OPTIONS"
 ./dependencies/genlock.sh "$PWD" "t1"
