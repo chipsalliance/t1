1f390202defdc59b1c9b32a928a2ec558af6a320
Avimitin <dev@avimit.in>
2025-03-20 21:27:27 +0800

    [nix] refactor bump script
    Signed-off-by: Avimitin <dev@avimit.in>

--- dependencies/prepare-t1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-ivyLocal=$(nix build '.#t1.submodules.ivyLocalRepo' --no-link --print-out-paths --max-jobs auto)
-export JAVA_TOOL_OPTIONS="-Dcoursier.ivy.home=$ivyLocal -Divy.home=$ivyLocal $JAVA_TOOL_OPTIONS"
-./dependencies/genlock.sh "$PWD" "t1"
