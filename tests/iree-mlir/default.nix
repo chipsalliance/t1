7bfdbc7b319bf1049c598a96b35839cc294105e7
Letu Ren <fantasquex@gmail.com>
2025-03-21 13:39:05 +0000

    tmp

--- tests/iree-mlir/default.nix
+++ tests/iree-mlir/default.nix
@@ -20,13 +20,13 @@ let
       nativeBuildInputs = [ iree ];
 
       ireeCompileArgs = [
-          "--iree-stream-partitioning-favor=min-peak-memory"
-          "--iree-hal-target-backends=llvm-cpu"
-          "--iree-llvmcpu-target-triple=riscv32-pc-linux-elf"
-          "--iree-llvmcpu-target-cpu=generic-rv32"
-          "--iree-llvmcpu-target-cpu-features=+m,+f"
-          "--iree-llvmcpu-target-abi=ilp32"
-          "--iree-llvmcpu-debug-symbols=false"
+        "--iree-stream-partitioning-favor=min-peak-memory"
+        "--iree-hal-target-backends=llvm-cpu"
+        "--iree-llvmcpu-target-triple=riscv32-pc-linux-elf"
+        "--iree-llvmcpu-target-cpu=generic-rv32"
+        "--iree-llvmcpu-target-cpu-features=+m,+f"
+        "--iree-llvmcpu-target-abi=ilp32"
+        "--iree-llvmcpu-debug-symbols=false"
       ];
 
       buildPhase = ''
