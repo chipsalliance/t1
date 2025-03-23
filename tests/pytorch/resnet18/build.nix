b6387cfad193e02095c22802e0b7b911957c9548
Avimitin <dev@avimit.in>
2025-03-23 20:19:59 +0800

    [tests] add pytorch resnet18 AOT importer

---
+++ tests/pytorch/resnet18/build.nix
@@ -0,0 +1,69 @@
+{ fetchurl
+, buildBuddyE2ETest
+}:
+let
+  checkpointFile = "resnet18-f37072fd.pth";
+  modelCache = fetchurl {
+    url = "https://download.pytorch.org/models/${checkpointFile}";
+    hash = "sha256-83By/UfonF6CdiHFuv+nUAgZ94lrus7BYLGhbFYOB+w=";
+  };
+in
+buildBuddyE2ETest {
+  caseName = "mobilenet";
+
+  optPhase = ''
+    mkdir -p pytorchCache/hub/checkpoints/
+    cp -v ${modelCache} pytorchCache/hub/checkpoints/${checkpointFile}
+    export TORCH_HOME=pytorchCache
+
+    python3 ./resnet18.py --output-dir $PWD
+
+    echo "Lowering forward.mlir"
+    buddy-opt forward.mlir -pass-pipeline \
+        "builtin.module(func.func(tosa-to-linalg-named, tosa-to-linalg, tosa-to-tensor, tosa-to-arith), \
+            empty-tensor-to-alloc-tensor, convert-elementwise-to-linalg, arith-bufferize, \
+            func.func(linalg-bufferize, tensor-bufferize), func-bufferize)" \
+      | buddy-opt -pass-pipeline \
+        "builtin.module(func.func(buffer-deallocation-simplification, convert-linalg-to-loops), \
+            eliminate-empty-tensors, func.func(llvm-request-c-wrappers), \
+            convert-math-to-llvm, convert-math-to-libm, convert-scf-to-cf, \
+            convert-arith-to-llvm, expand-strided-metadata, finalize-memref-to-llvm, \
+            convert-func-to-llvm, reconcile-unrealized-casts)" \
+      > forward-lowered.mlir
+
+    echo "Lowering subgraph0.mlir"
+    buddy-opt subgraph0.mlir -pass-pipeline \
+        "builtin.module(func.func(tosa-to-linalg-named, tosa-to-arith, tosa-to-linalg, tosa-to-tensor))" \
+      | buddy-opt \
+          -convert-elementwise-to-linalg \
+          -func-bufferize-dynamic-offset \
+          -arith-bufferize \
+          -func-bufferize \
+          -tensor-bufferize \
+          -linalg-bufferize \
+          -finalizing-bufferize \
+          -convert-linalg-to-loops \
+          -lower-affine \
+          -convert-scf-to-cf \
+          -llvm-request-c-wrappers \
+          -convert-math-to-llvm \
+          -convert-math-to-libm \
+          -convert-arith-to-llvm \
+          -convert-func-to-llvm \
+          -expand-strided-metadata \
+          -finalize-memref-to-llvm \
+          -reconcile-unrealized-casts \
+      > subgraph0-lowered.mlir
+
+    echo "Compiling memrefCopy library"
+    $CXX -nostdlib -c ${../lib/MemrefCopy.cc} -o memrefCopy.o
+    llcArtifacts+=(
+      memrefCopy.o
+    )
+
+    optArtifacts+=(
+      "forward-lowered.mlir"
+      "subgraph0-lowered.mlir"
+    )
+  '';
+}
