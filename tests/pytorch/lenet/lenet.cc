ead4d237f59c8c48db030beab3df9123003d3bf2
Shupei Fan <dymarkfan@outlook.com>
2025-03-26 17:13:32 +0000

    [tests] fix lenet

--- tests/pytorch/lenet/lenet.cc
+++ tests/pytorch/lenet/lenet.cc
@@ -13,14 +13,14 @@ __attribute((section(".vdata"))) float output_0[OUTPUT_N];
 __attribute((section(".vdata"))) float param_0[PARAM_N];
 
 // Define the sizes of the input and output tensors.
-static const int32_t sizesInput[4] = {INPUT_N, INPUT_C, INPUT_H, INPUT_W};
-static const int32_t sizesOutput[2] = {1, OUTPUT_N};
-static const int32_t sizesParams[1] = {PARAM_N};
+// static const int32_t sizesInput[4] = {INPUT_N, INPUT_C, INPUT_H, INPUT_W};
+// static const int32_t sizesOutput[2] = {1, OUTPUT_N};
+// static const int32_t sizesParams[1] = {PARAM_N};
 
 // Create input and output containers for the image and model output.
-MemRef<float, 4> input(input_0, sizesInput);
-MemRef<float, 2> output(output_0, sizesOutput);
-MemRef<float, 1> params(param_0, 2.0, sizesParams);
+// MemRef<float, 4> input(input_0, sizesInput);
+// MemRef<float, 2> output(output_0, sizesOutput);
+// MemRef<float, 1> params(param_0, 2.0, sizesParams);
 
 // Declare the target model C interface.
 extern "C" {
@@ -29,6 +29,12 @@ void _mlir_ciface_forward(MemRef<float, 2> *output, MemRef<float, 1> *arg0,
 }
 
 extern "C" int test() {
+  int32_t sizesInput[4] = {INPUT_N, INPUT_C, INPUT_H, INPUT_W};
+  int32_t sizesOutput[2] = {1, OUTPUT_N};
+  int32_t sizesParams[1] = {PARAM_N};
+  MemRef<float, 4> input(input_0, sizesInput);
+  MemRef<float, 2> output(output_0, sizesOutput);
+  MemRef<float, 1> params(param_0, 2.0, sizesParams);
   _mlir_ciface_forward(&output, &params, &input);
   return 0;
 }
