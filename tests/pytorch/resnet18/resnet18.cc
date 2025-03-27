f84b377801cfcf90d0e985b86ca064cecaf54897
Avimitin <dev@avimit.in>
2025-03-27 15:22:17 +0800

    [WIP] sync Container implementation from upstream
    Signed-off-by: Avimitin <dev@avimit.in>

--- tests/pytorch/resnet18/resnet18.cc
+++ tests/pytorch/resnet18/resnet18.cc
@@ -1,20 +1,24 @@
-#include "arg0.inc"
-#include "generated-img.inc"
-#include "memref.hpp"
+#include <buddy/Core/Container.h>
 
 // Declare the resnet C interface.
 extern "C" void _mlir_ciface_forward(MemRef<float, 2> *output,
                                      MemRef<float, 1> *arg0,
                                      MemRef<float, 4> *input);
 
-static const int32_t sizes[2] = {1, 1000};
-__attribute((section(".vdata"))) float output_float_1[1000];
-MemRef<float, 2> output(output_float_1, sizes);
-
-MemRef<float, 4> inputResize(IMAGE, IMAGE_SIZES);
-MemRef<float, 1> paramsContainer(PARAMS, PARAMS_SIZES);
+__attribute((section(".vdata"))) float output_float_1[1000] = {0};
+__attribute((section(".vdata"))) float IMAGE[300] = {0};
+__attribute((section(".vdata"))) float PARAMS[100] = {0};
 
 extern "C" int test() {
+  static int32_t sizes[2] = {1, 1000};
+  static int32_t params_sizes[1] = {100};
+  static int32_t image_sizes[4] = {1, 3, 100, 100};
+
+  MemRef<float, 2> output(output_float_1, sizes);
+
+  MemRef<float, 4> inputResize(IMAGE, image_sizes);
+  MemRef<float, 1> paramsContainer(PARAMS, params_sizes);
+
   _mlir_ciface_forward(&output, &paramsContainer, &inputResize);
   return 0;
 }
