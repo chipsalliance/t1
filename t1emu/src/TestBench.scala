f719200b88bce9e96d3d6eab5f871425329b3168
Clo91eaf <Clo91eaf@qq.com>
2025-04-01 01:51:23 +0800

    [t1emu & t1rocketemu] fix vrf idx calculation
    Signed-off-by: Clo91eaf <Clo91eaf@qq.com>
    
    [t1emu & t1rocketemu] fix vrf idx calculation
    
    Signed-off-by: Clo91eaf <Clo91eaf@qq.com>

--- t1emu/src/TestBench.scala
+++ t1emu/src/TestBench.scala
@@ -176,9 +176,16 @@ class TestBench(val parameter: T1Parameter)
   // vrf write
   laneProbes.zipWithIndex.foreach { case (lane, i) =>
     val vrf = lane.vrfProbe.suggestName(s"lane${i}VrfProbe")
+
+    val datapathWidth = parameter.datapathWidth.U(32.W)
+
+    val vrfOffsetInBytes  = parameter.vLen.U(32.W) / 8.U(32.W) * vrf.requestVd
+    val laneOffsetInBytes = parameter.dLen.U(32.W) / 8.U(32.W) * vrf.requestOffset + datapathWidth / 8.U(32.W) * i.U(32.W)
+
+    val vrfIdx = vrfOffsetInBytes + laneOffsetInBytes
     when(vrf.valid)(
       printf(
-        cf"""{"event":"VrfWrite","issue_idx":${vrf.requestInstruction},"vd":${vrf.requestVd},"offset":${vrf.requestOffset},"mask":"${vrf.requestMask}%x","data":"${vrf.requestData}%x","lane":$i,"cycle":${simulationTime}}\n"""
+        cf"""{"event":"VrfWrite","issue_idx":${vrf.requestInstruction},"vrf_idx":${vrfIdx},"mask":"${vrf.requestMask}%x","data":"${vrf.requestData}%x","cycle":${simulationTime}}\n"""
       )
     )
   }
