7f198bf859124a717222d8f3bbae86b17a80bfeb
Avimitin <dev@avimit.in>
2025-04-07 19:30:14 +0800

    [tests] fix lenet build
    Signed-off-by: Avimitin <dev@avimit.in>

--- tests/pytorch/lenet/lenet.cc
+++ tests/pytorch/lenet/lenet.cc
@@ -1,4 +1,4 @@
-#include "memref.hpp"
+#include <buddy/Core/Container.h>
 
 #define INPUT_N 1
 #define INPUT_C 1
@@ -13,14 +13,9 @@ __attribute((section(".vdata"))) float output_0[OUTPUT_N];
 __attribute((section(".vdata"))) float param_0[PARAM_N];
 
 // Define the sizes of the input and output tensors.
-static const int32_t sizesInput[4] = {INPUT_N, INPUT_C, INPUT_H, INPUT_W};
-static const int32_t sizesOutput[2] = {1, OUTPUT_N};
-static const int32_t sizesParams[1] = {PARAM_N};
-
-// Create input and output containers for the image and model output.
-MemRef<float, 4> input(input_0, sizesInput);
-MemRef<float, 2> output(output_0, sizesOutput);
-MemRef<float, 1> params(param_0, 2.0, sizesParams);
+static int32_t sizesInput[4] = {INPUT_N, INPUT_C, INPUT_H, INPUT_W};
+static int32_t sizesOutput[2] = {1, OUTPUT_N};
+static int32_t sizesParams[1] = {PARAM_N};
 
 // Declare the target model C interface.
 extern "C" {
@@ -29,6 +24,11 @@ void _mlir_ciface_forward(MemRef<float, 2> *output, MemRef<float, 1> *arg0,
 }
 
 extern "C" int test() {
+  // Create input and output containers for the image and model output.
+  MemRef<float, 4> input(input_0, sizesInput);
+  MemRef<float, 2> output(output_0, sizesOutput);
+  MemRef<float, 1> params(param_0, 2.0, sizesParams);
+
   _mlir_ciface_forward(&output, &params, &input);
   return 0;
 }
