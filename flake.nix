95f7fafce29d39d49cc1f4e2b250f742f867c664
Avimitin <dev@avimit.in>
2025-04-14 16:50:28 +0800

    [nix] rewrite flake with flake-parts
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -3,63 +3,86 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     mill-ivy-fetcher.url = "github:Avimitin/mill-ivy-fetcher";
     zaozi.url = "github:sequencer/zaozi";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
       mill-ivy-fetcher,
-      flake-utils,
       zaozi,
-      treefmt-nix,
-    }@inputs:
+      ...
+    }:
     let
       overlay = import ./nix/overlay.nix { inherit self; };
     in
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            mill-ivy-fetcher.overlays.default
-            zaozi.overlays.default
-            overlay
-          ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      # Add supported platform here
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      flake = {
+        # Export github:chipsalliance/t1.overlays.t1-overlay to help other project have share package override
+        overlays = rec {
+          t1-overlay = overlay;
+
+          default = t1-overlay;
         };
-        treefmtEval = treefmt-nix.lib.evalModule pkgs {
-          projectRootFile = "flake.nix";
-          settings.on-unmatched = "debug";
-          programs.nixfmt.enable = true;
-          programs.scalafmt.enable = true;
-          programs.rustfmt.enable = true;
-        };
-      in
-      {
-        legacyPackages = pkgs;
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              ammonite
-              mill
-              t1-helper
-              zstd
+      };
+
+      imports = [
+        # Add treefmt flake module to automatically configure and add formatter to this flake
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              mill-ivy-fetcher.overlays.default
+              zaozi.overlays.default
+              overlay
             ];
           };
+        in
+        {
+          # Override the default "pkgs" attribute in per-system config.
+          _module.args.pkgs = pkgs;
+
+          # Although the pkgs attribute is already override, but I am afraid
+          # that the magical evaluation of "pkgs" is confusing, and will lead
+          # to debug hell. So here we use the "pkgs" in "let-in binding" to
+          # explicitly told every user we are using an overlayed version of
+          # nixpkgs.
+          legacyPackages = pkgs;
+
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                ammonite
+                mill
+                t1-helper
+                zstd
+              ];
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            settings.on-unmatched = "debug";
+            programs.nixfmt.enable = true;
+            programs.scalafmt.enable = true;
+            programs.rustfmt.enable = true;
+          };
         };
-        formatter = treefmtEval.config.build.wrapper;
-        checks = {
-          formatting = treefmtEval.config.build.check self;
-        };
-      }
-    )
-    // {
-      inherit inputs;
-      overlays.default = overlay;
     };
 }
