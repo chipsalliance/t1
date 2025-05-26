0d0255724b36ad3cd855741ebfe0f74815b5adc3
unlsycn <unlsycn@unlsycn.com>
2025-05-26 10:44:37 +0800

    [omreader] fix circtlib bump
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- omreader/src/OMReader.scala
+++ omreader/src/OMReader.scala
@@ -2,10 +2,11 @@
 // SPDX-FileCopyrightText: 2022 Jiuyang Liu <liu@jiuyang.me>
 package org.chipsalliance.t1.omreader
 
-import org.llvm.circt.scalalib.emit.capi.given_DialectHandleApi
-import org.llvm.circt.scalalib.firrtl.capi.given_DialectHandleApi
-import org.llvm.circt.scalalib.om.capi.{*, given}
-import org.llvm.mlir.scalalib.{Module as MlirModule, ModuleApi as MlirModuleApi, *, given}
+import org.llvm.circt.scalalib.capi.dialect.emit.{given_DialectApi, DialectApi as EmitDialectApi}
+import org.llvm.circt.scalalib.capi.dialect.firrtl.{given_DialectApi, DialectApi as FirrtlDialectApi}
+import org.llvm.circt.scalalib.capi.dialect.hw.{given_AttributeApi, given_DialectApi, DialectApi as HWDialectApi}
+import org.llvm.circt.scalalib.capi.dialect.om.{*, given}
+import org.llvm.mlir.scalalib.capi.ir.{Module as MlirModule, ModuleApi as MlirModuleApi, *, given}
 
 import scala.util.chaining.*
 import java.lang.foreign.Arena
@@ -14,16 +15,15 @@ trait OMReader(val mlirbc: Array[Byte], val topName: String):
   protected val arena = Arena.ofConfined()
   given Arena         = arena
 
-  protected val context = summon[ContextApi].contextCreate.tap(ctx =>
-    ctx.loadOmDialect()
-    ctx.loadFirrtlDialect()
-    ctx.loadEmitDialect()
-    ctx.allowUnregisteredDialects(true)
-  )
+  protected val context = summon[ContextApi].contextCreate
   given Context         = context
+  context.allowUnregisteredDialects(true)
+  summon[EmitDialectApi].loadDialect
+  summon[FirrtlDialectApi].loadDialect
+  summon[HWDialectApi].loadDialect
 
   protected val module    = summon[MlirModuleApi].moduleCreateParse(mlirbc)
   protected val evaluator = summon[EvaluatorApi].evaluatorNew(module)
-  protected val topClass  = evaluator.instantiate(topName, summon[EvaluatorValueApi].basePathGetEmpty)
+  protected val topClass  = evaluator.instantiate(topName, summon[EvaluatorApi].basePathGetEmpty)
 
 end OMReader
