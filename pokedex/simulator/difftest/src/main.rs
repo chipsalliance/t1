76754881e87b22e834e4fa1994667a2e22210bf4
Avimitin <dev@avimit.in>
2025-06-20 21:14:32 +0800

    [pokedex] add host emulator
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ pokedex/simulator/difftest/src/main.rs
@@ -0,0 +1,226 @@
+use anyhow::Context;
+use clap::Parser;
+use pokedex::{PokedexEvent, PokedexLog};
+use serde::Deserialize;
+use serde::Deserializer;
+use spike::SpikeLog;
+use tracing::{error, info};
+use tracing_subscriber;
+
+mod pokedex;
+mod spike;
+
+/// Describe the expected memory behavior for program exit
+#[derive(Debug, Deserialize)]
+struct EndPattern {
+    // Memory action a EndPattern should capture. Support only "write" now.
+    #[allow(dead_code)]
+    action: String,
+    // Hex string with "0x" prefix indicate the memory address a EndPattern should
+    // capture
+    #[serde(deserialize_with = "hex_to_u64")]
+    memory_address: u64,
+    // Hex string with "0x" prefix indicate the data value should be capture on the given memory address
+    #[serde(deserialize_with = "hex_to_u64")]
+    data: u64,
+}
+
+fn hex_to_u64<'de, D>(de: D) -> Result<u64, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let val: &str = Deserialize::deserialize(de)?;
+    u64::from_str_radix(val.trim_start_matches("0x"), 16)
+        .map_err(|err| serde::de::Error::custom(format!("cannot convert hex value to u64: {err}")))
+}
+
+#[derive(Debug, Deserialize)]
+struct CaseConfig {
+    elf_path_glob: String,
+    pokedex_args: Vec<String>,
+    spike_args: Vec<String>,
+    end_pattern: EndPattern,
+}
+
+#[derive(clap::Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct DiffTestArgs {
+    #[arg(short = 'c', long, default_value_t = String::from("./sail_difftest_config.json"))]
+    config_path: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = DiffTestArgs::parse();
+
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_file(false)
+        .with_line_number(false)
+        .without_time()
+        .init();
+
+    let cfg_raw =
+        std::fs::read(args.config_path).with_context(|| "fail to read sail difftest config")?;
+    let cfg: CaseConfig =
+        serde_json::from_slice(&cfg_raw).with_context(|| "fail to parse sail difftest config")?;
+
+    let all_elf_files = glob::glob(&cfg.elf_path_glob)
+        .with_context(|| format!("invalid path glob {}", cfg.elf_path_glob))?;
+
+    let mut errors = Vec::new();
+    for path in all_elf_files {
+        let path = path.with_context(|| "internal error: glob leads to unreadable path")?;
+
+        info!("running difftest for {path:?}");
+
+        let spike_log = spike::run_process(&cfg.spike_args, &path)?;
+        let pokedex_log = pokedex::run_process(&cfg.pokedex_args, &path)?;
+        let diff_result = diff(&spike_log, &pokedex_log, &cfg.end_pattern);
+
+        if !diff_result.is_same {
+            errors.push((path, diff_result.context));
+        } else {
+            info!("difftest pass")
+        }
+    }
+
+    if errors.is_empty() {
+        return Ok(());
+    }
+
+    for (path, err) in errors {
+        error!("{}\n{}", path.display(), err)
+    }
+
+    anyhow::bail!("difftest fail")
+}
+
+struct DiffMeta {
+    is_same: bool,
+    context: String,
+}
+
+impl DiffMeta {
+    fn passed() -> Self {
+        Self {
+            is_same: true,
+            context: String::new(),
+        }
+    }
+
+    fn failed(ctx: impl ToString) -> Self {
+        Self {
+            is_same: false,
+            context: ctx.to_string(),
+        }
+    }
+}
+
+fn diff(spike_log: &SpikeLog, pokedex_log: &PokedexLog, end_pattern: &EndPattern) -> DiffMeta {
+    assert!(!spike_log.is_empty());
+    assert!(!pokedex_log.is_empty());
+
+    let mut pokedex_log_cursor = 0;
+    let mut is_reset = false;
+
+    // spike contains vendored bootrom but doesn't provide a way to remove it.
+    // so we need to compares commit log from when the emulator run reset_vector
+    let reset_vector_addr = pokedex_log
+        .iter()
+        .find_map(|event| event.get_reset_vector())
+        .unwrap_or_else(|| {
+            unreachable!("reset_vector event not found");
+        });
+    let test_end_pc = spike_log
+        .has_memory_write_commits()
+        .into_iter()
+        .find_map(|log| {
+            let (write_address, write_data) = log.commits.get_mem_write().unwrap();
+            if write_address == end_pattern.memory_address && write_data == end_pattern.data {
+                Some(log.pc)
+            } else {
+                None
+            }
+        });
+    let Some(end_pc) = test_end_pc else {
+        return DiffMeta::failed("Can't find any end pattern from spike log");
+    };
+
+    for spike_event in spike_log.iter() {
+        if !is_reset {
+            if spike_event.pc == reset_vector_addr {
+                is_reset = true;
+            } else {
+                continue;
+            }
+        }
+
+        if spike_event.pc == end_pc {
+            break;
+        }
+
+        // ignore memory read write only commits
+        if !spike_event.has_reg_write_commit() {
+            continue;
+        }
+
+        let search_result = pokedex_log[pokedex_log_cursor..]
+            .iter()
+            .enumerate()
+            .find_map(|(i, event)| match event {
+                PokedexEvent::ArchState { pc, .. } => {
+                    if *pc == spike_event.pc {
+                        pokedex_log_cursor = i;
+                        Some(event)
+                    } else {
+                        None
+                    }
+                }
+                _ => None,
+            });
+
+        if search_result.is_none() {
+            return DiffMeta::failed(indoc::formatdoc! {"
+                At PC={:#010x} spike have following commit events that are not occur at simulator side:
+
+                {spike_event}
+                ", spike_event.pc
+            });
+        }
+
+        let search_result = search_result.unwrap();
+        let PokedexEvent::ArchState {
+            reg_idx, data, pc, ..
+        } = search_result
+        else {
+            unreachable!("internal error: we already filter emulator event type at above")
+        };
+
+        let match_event = spike_event
+            .get_register_type_commits()
+            .into_iter()
+            .find(|event| {
+                let (idx, value) = event.get_register().unwrap();
+                idx == *reg_idx && value == *data
+            });
+
+        if match_event.is_none() {
+            return DiffMeta::failed(indoc::formatdoc! {"
+                At PC={pc:#010x} simulator write {data:#010x} to register x{reg_idx},
+                but this action was not found at spike side.
+
+                ------------
+                |Event Dump|
+                ------------
+
+                We get simulator:
+                {search_result}
+
+                But have spike:
+                {spike_event}
+            "});
+        }
+    }
+
+    DiffMeta::passed()
+}
---
+++ pokedex/simulator/difftest/src/spike.rs
@@ -0,0 +1,552 @@
+use std::fmt::Display;
+use std::ops::{Deref, DerefMut};
+use std::path::PathBuf;
+
+use anyhow::Context;
+
+pub fn run_process(
+    args: &[String],
+    elf_path: impl AsRef<std::ffi::OsStr>,
+) -> anyhow::Result<SpikeLog> {
+    let spike_exec = which::which("spike").with_context(|| "spike exec not found")?;
+
+    let result = std::process::Command::new(&spike_exec)
+        .args(args)
+        .arg(&elf_path)
+        .output()
+        .with_context(|| "fail exeuting spike")?;
+
+    if !result.status.success() {
+        println!("{}", String::from_utf8_lossy(&result.stderr));
+        anyhow::bail!(
+            "fail to execute spike with args {} for elf {}",
+            args.join(" "),
+            elf_path.as_ref().to_str().unwrap()
+        );
+    }
+
+    let elf_path = PathBuf::from(&elf_path);
+    let spike_log_path = format!(
+        "./{}-spike-commits.log",
+        elf_path.file_name().unwrap().to_string_lossy()
+    );
+    std::fs::write(spike_log_path, &result.stderr)
+        .with_context(|| "fail storing spike commit log")?;
+    let stdout = String::from_utf8_lossy(&result.stderr);
+    let spike_log_ast = parse_spike_log(stdout);
+
+    Ok(spike_log_ast)
+}
+
+fn parse_spike_log(log: impl AsRef<str>) -> SpikeLog {
+    log.as_ref()
+        .lines()
+        .enumerate()
+        .map(|(line_number, line)| match SpikeLogSyntax::parse(line) {
+            Err(err) => {
+                panic!("fail parsing line at line {line_number}: {err}. Original line: '{line}'")
+            }
+            Ok(ast) => ast,
+        })
+        .collect()
+}
+
+#[derive(Debug, PartialEq)]
+pub struct SpikeLog(Vec<SpikeLogSyntax>);
+
+impl FromIterator<SpikeLogSyntax> for SpikeLog {
+    fn from_iter<T: IntoIterator<Item = SpikeLogSyntax>>(iter: T) -> Self {
+        Self(Vec::from_iter(iter))
+    }
+}
+
+impl Deref for SpikeLog {
+    type Target = Vec<SpikeLogSyntax>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for SpikeLog {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl SpikeLog {
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    // TODO: future usage
+    #[allow(dead_code)]
+    pub fn has_register_commits(&self) -> Vec<&SpikeLogSyntax> {
+        self.0
+            .iter()
+            .filter(|log| {
+                !log.commits.is_empty() && log.commits.iter().any(|c| c.is_register_write_commit())
+            })
+            .collect()
+    }
+
+    pub fn has_memory_write_commits(&self) -> Vec<&SpikeLogSyntax> {
+        self.0
+            .iter()
+            .filter(|log| !log.commits.is_empty() && log.commits.iter().any(|c| c.is_mem_write()))
+            .collect()
+    }
+}
+
+/// Describe all load store behavior occurs at Spike side
+#[derive(Debug, PartialEq)]
+pub enum LoadStoreType {
+    Register { index: u8, value: u64 },
+    MemoryRead { address: u64 },
+    MemoryWrite { address: u64, value: u64 },
+}
+
+impl Display for LoadStoreType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Register { index, value } => {
+                write!(f, "write register x{index} with {value:#010x}")
+            }
+            Self::MemoryWrite { address, value } => {
+                write!(f, "write memory {address:#010x} with {value:#010x}")
+            }
+            Self::MemoryRead { address } => write!(f, "read memory {address:#010x}"),
+        }
+    }
+}
+
+impl LoadStoreType {
+    pub fn is_register_write_commit(&self) -> bool {
+        match self {
+            Self::Register { .. } => true,
+            _ => false,
+        }
+    }
+
+    pub fn get_register(&self) -> Option<(u8, u64)> {
+        match self {
+            Self::Register { index, value } => Some((*index, *value)),
+            _ => None,
+        }
+    }
+
+    pub fn is_mem_write(&self) -> bool {
+        match self {
+            Self::MemoryWrite { .. } => true,
+            _ => false,
+        }
+    }
+
+    pub fn get_mem_write(&self) -> Option<(u64, u64)> {
+        match self {
+            Self::MemoryWrite { address, value } => Some((*address, *value)),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, Default, PartialEq)]
+pub struct LoadStoreCommits(Vec<LoadStoreType>);
+
+impl LoadStoreCommits {
+    pub fn get_mem_write(&self) -> Option<(u64, u64)> {
+        self.0.iter().find_map(|commit| commit.get_mem_write())
+    }
+
+    pub fn has_reg_write_commit(&self) -> bool {
+        self.0
+            .iter()
+            .any(|commit| commit.is_register_write_commit())
+    }
+}
+
+impl Deref for LoadStoreCommits {
+    type Target = Vec<LoadStoreType>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for LoadStoreCommits {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[derive(Debug, Default, PartialEq)]
+pub struct SpikeLogSyntax {
+    pub core: u8,
+    pub privilege: u8,
+    pub pc: u64,
+    pub instruction: u32,
+    pub commits: LoadStoreCommits,
+}
+
+impl Display for SpikeLogSyntax {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let SpikeLogSyntax {
+            core,
+            privilege,
+            pc,
+            instruction,
+            commits,
+        } = self;
+
+        let display_commit = commits.iter().fold(String::new(), |display, event| {
+            format!("{display}* {event}\n")
+        });
+
+        indoc::writedoc! {f, "
+            core: {core}
+            priviledge: {privilege}
+            pc: {pc:#010x}
+            instruction: {instruction:#010x}
+            commits:
+            {}
+        ", display_commit}
+    }
+}
+
+impl SpikeLogSyntax {
+    pub fn get_register_type_commits(&self) -> Vec<&LoadStoreType> {
+        self.commits
+            .iter()
+            .filter(|event| event.is_register_write_commit())
+            .collect()
+    }
+
+    pub fn has_reg_write_commit(&self) -> bool {
+        self.commits.has_reg_write_commit()
+    }
+}
+
+enum ParseCursor<'a> {
+    Core,
+    Priv,
+    Pc,
+    Insn,
+    RegParseBegin,
+    RegParseName(&'a str),
+    MemParseBegin,
+    MemParseRead(u64),
+    Error(String),
+}
+
+struct ParseContext<'a> {
+    cursor: ParseCursor<'a>,
+    state: SpikeLogSyntax,
+}
+
+impl Default for ParseContext<'_> {
+    fn default() -> Self {
+        Self {
+            cursor: ParseCursor::Core,
+            state: SpikeLogSyntax::default(),
+        }
+    }
+}
+
+impl ParseContext<'_> {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn to_spike_log(self) -> Result<SpikeLogSyntax, String> {
+        match self.cursor {
+            ParseCursor::Error(err) => Err(err),
+            _ => Ok(self.state),
+        }
+    }
+}
+
+impl SpikeLogSyntax {
+    fn parse<'a>(line: &'a str) -> Result<Self, String> {
+        fn to_error<'a>(expect: &str, actual: &str, err: impl Display) -> ParseCursor<'a> {
+            ParseCursor::Error(format!("expect {expect}, get '{actual}': {err}"))
+        }
+
+        let segments: Vec<&'a str> = line
+            .trim()
+            .split(" ")
+            .filter(|part| !part.is_empty())
+            .collect();
+        let total_segments = segments.len();
+        let ctx: ParseContext =
+            segments
+                .into_iter()
+                .enumerate()
+                .fold(ParseContext::new(), |mut ctx, (index, elem)| {
+                    match ctx.cursor {
+                        // skip literal "core"
+                        ParseCursor::Core if elem == "core" => ctx,
+                        // vec[0] is core id. core id always comes with ":", strip it here
+                        ParseCursor::Core => {
+                            if !elem.ends_with(":") {
+                                ctx.cursor = to_error(
+                                    "':' suffixed string",
+                                    elem,
+                                    "core_id not ends with ':'",
+                                );
+                                return ctx;
+                            }
+
+                            match (&elem[0..elem.len() - 1]).parse::<u8>() {
+                                Ok(v) => {
+                                    ctx.state.core = v;
+                                    ctx.cursor = ParseCursor::Priv;
+                                    ctx
+                                }
+                                Err(err) => {
+                                    ctx.cursor = to_error("u8 value core_id", elem, err);
+                                    ctx
+                                }
+                            }
+                        }
+                        ParseCursor::Priv => match elem.parse::<u8>() {
+                            Ok(priv_id) => {
+                                ctx.state.privilege = priv_id;
+                                ctx.cursor = ParseCursor::Pc;
+                                ctx
+                            }
+                            Err(err) => {
+                                ctx.cursor = to_error("u8 value priv_id", elem, err);
+                                ctx
+                            }
+                        },
+                        ParseCursor::Pc => {
+                            if !elem.starts_with("0x") {
+                                ctx.cursor =
+                                    to_error("hex string", elem, "pc value not prefixed with '0x'");
+                                return ctx;
+                            };
+
+                            match u64::from_str_radix(elem.trim_start_matches("0x"), 16) {
+                                Ok(pc) => {
+                                    ctx.state.pc = pc;
+                                    ctx.cursor = ParseCursor::Insn;
+                                    ctx
+                                }
+                                Err(err) => {
+                                    ctx.cursor = to_error("u64 value pc", elem, err);
+                                    ctx
+                                }
+                            }
+                        }
+                        // vec[3] is instruction decode, it always has surrounding parentheses
+                        ParseCursor::Insn => {
+                            if !elem.starts_with("(0x") {
+                                ctx.cursor = to_error(
+                                    "parentheses surrounding hex string",
+                                    elem,
+                                    "instruction not started with '(0x'",
+                                );
+                                return ctx;
+                            };
+
+                            if !elem.ends_with(")") {
+                                ctx.cursor = to_error(
+                                    "parentheses surrounding hex string",
+                                    elem,
+                                    "instruction not ends with ')'",
+                                );
+                                return ctx;
+                            };
+
+                            match u32::from_str_radix(&elem[3..elem.len() - 1], 16) {
+                                Ok(insn) => {
+                                    ctx.state.instruction = insn;
+                                    ctx.cursor = ParseCursor::RegParseBegin;
+                                    ctx
+                                }
+                                Err(err) => {
+                                    ctx.cursor = to_error("u32 value instruction", elem, err);
+                                    ctx
+                                }
+                            }
+                        }
+                        // then all other parts are register change, memory load and memory write
+                        // spike handle memory in undocumented way and we don't need to compare memory
+                        // behavior to spike, so trim memory here
+                        ParseCursor::RegParseBegin if elem != "mem" && !elem.starts_with("0x") => {
+                            const VALID: [char; 4] = ['x', 'f', 'v', 'c'];
+                            // TODO: handle CSR
+                            if VALID.contains(&elem.chars().next().unwrap()) {
+                                ctx.cursor = ParseCursor::RegParseName(elem);
+                            } else {
+                                ctx.cursor = to_error(
+                                    "register name",
+                                    elem,
+                                    "register name not prefixed with 'x', 'f', 'v' or 'c'",
+                                );
+                            }
+
+                            ctx
+                        }
+                        ParseCursor::RegParseBegin if elem == "mem" => {
+                            ctx.cursor = ParseCursor::MemParseBegin;
+                            ctx
+                        }
+                        ParseCursor::RegParseName(reg_name) => {
+                            if !elem.starts_with("0x") {
+                                ctx.cursor = to_error(
+                                    "hex string",
+                                    elem,
+                                    "register value not prefixed with '0x'",
+                                );
+                                return ctx;
+                            };
+
+                            let Ok(reg_idx): Result<u8, _> = reg_name[1..].parse() else {
+                                ctx.cursor = to_error(
+                                    "register index",
+                                    elem,
+                                    "register is not a valid digit",
+                                );
+                                return ctx;
+                            };
+
+                            match u64::from_str_radix(elem.trim_start_matches("0x"), 16) {
+                                Ok(reg_val) => {
+                                    ctx.cursor = ParseCursor::RegParseBegin;
+                                    ctx.state.commits.push(LoadStoreType::Register {
+                                        index: reg_idx,
+                                        value: reg_val,
+                                    });
+                                    ctx
+                                }
+                                Err(err) => {
+                                    ctx.cursor =
+                                        to_error("u64 value register_value", elem, err.to_string());
+                                    ctx
+                                }
+                            }
+                        }
+                        ParseCursor::MemParseBegin => {
+                            if !elem.starts_with("0x") {
+                                ctx.cursor = to_error(
+                                    "hex string",
+                                    elem,
+                                    "memory value not prefixed with '0x'",
+                                );
+                                return ctx;
+                            };
+
+                            match u64::from_str_radix(elem.trim_start_matches("0x"), 16) {
+                                Ok(address) => {
+                                    ctx.cursor = ParseCursor::MemParseRead(address);
+                                    let is_line_end = index == total_segments - 1;
+                                    if is_line_end {
+                                        ctx.state
+                                            .commits
+                                            .push(LoadStoreType::MemoryRead { address });
+                                    }
+                                    ctx
+                                }
+                                Err(err) => {
+                                    ctx.cursor = to_error("u64 memory address", elem, err);
+                                    ctx
+                                }
+                            }
+                        }
+                        ParseCursor::MemParseRead(address) if elem.starts_with("0x") => {
+                            match u64::from_str_radix(elem.trim_start_matches("0x"), 16) {
+                                Ok(write_val) => {
+                                    ctx.cursor = ParseCursor::RegParseBegin;
+                                    ctx.state.commits.push(LoadStoreType::MemoryWrite {
+                                        address,
+                                        value: write_val,
+                                    });
+                                    ctx
+                                }
+                                Err(err) => {
+                                    ctx.cursor =
+                                        to_error("u64 memory write value", elem, err.to_string());
+                                    ctx
+                                }
+                            }
+                        }
+                        ParseCursor::MemParseRead(address) => {
+                            ctx.cursor = ParseCursor::RegParseBegin;
+                            ctx.state
+                                .commits
+                                .push(LoadStoreType::MemoryRead { address });
+                            ctx
+                        }
+                        // passthrough error
+                        _ => ctx,
+                    }
+                });
+
+        ctx.to_spike_log()
+    }
+}
+
+#[test]
+fn test_parsing_spike_log_ast() {
+    let mut d = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("assets/example.spike.log");
+    let sample_log = std::fs::read(d).unwrap();
+    assert!(!sample_log.is_empty());
+    let raw = String::from_utf8_lossy(&sample_log);
+    let ast = parse_spike_log(&raw);
+    assert!(!ast.is_empty());
+
+    let expect = SpikeLog(vec![
+        SpikeLogSyntax {
+            core: 0,
+            privilege: 3,
+            pc: 4096,
+            instruction: 663,
+            commits: LoadStoreCommits(vec![LoadStoreType::Register {
+                index: 5,
+                value: 4096,
+            }]),
+        },
+        SpikeLogSyntax {
+            core: 0,
+            privilege: 3,
+            pc: 4100,
+            instruction: 33719699,
+            commits: LoadStoreCommits(vec![LoadStoreType::Register {
+                index: 11,
+                value: 4128,
+            }]),
+        },
+        SpikeLogSyntax {
+            core: 0,
+            privilege: 3,
+            pc: 4108,
+            instruction: 25342595,
+            commits: LoadStoreCommits(vec![
+                LoadStoreType::Register {
+                    index: 5,
+                    value: 2147483700,
+                },
+                LoadStoreType::MemoryRead { address: 4120 },
+            ]),
+        },
+        SpikeLogSyntax {
+            core: 0,
+            privilege: 3,
+            pc: 4112,
+            instruction: 163943,
+            commits: LoadStoreCommits(Vec::new()),
+        },
+        SpikeLogSyntax {
+            core: 0,
+            privilege: 3,
+            pc: 2147483860,
+            instruction: 1129507,
+            commits: LoadStoreCommits(vec![LoadStoreType::MemoryWrite {
+                address: 2684354552,
+                value: 2147483840,
+            }]),
+        },
+    ]);
+    assert_eq!(ast, expect);
+}
