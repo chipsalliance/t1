6250b8ea9391c591105d53691e34bf23ab223f5a
Avimitin <dev@avimit.in>
2025-07-29 17:01:00 +0800

    [pokedex-tests] integrate riscv-tests

--- pokedex/tests/default.nix
+++ pokedex/tests/default.nix
@@ -14,4 +14,16 @@ lib.makeScope newScope (scope: {
   ) { };
   pokedex-log = scope.callPackage ./pokedex-log.nix { };
   difftest-meta = scope.callPackage ./difftest-meta.nix { };
+  riscv-tests = scope.callPackage (
+    { fetchFromGitHub }:
+    fetchFromGitHub {
+      repo = "riscv-tests";
+      owner = "riscv-software-src";
+      rev = "b5ba87097c42aa41c56657e0ae049c2996e8d8d8";
+      hash = "sha256-VaaGzlEsOSjtUhDewxoM77wJUW9Yu646CYefWeggLys=";
+      postFetch = ''
+        rm -v $out/isa/rv32ui/ma_data.S
+      '';
+    }
+  ) { };
 })
--- pokedex/tests/package.nix
+++ pokedex/tests/package.nix
@@ -1,12 +1,28 @@
-{ rv32-stdenv }:
+{
+  lib,
+  rv32-stdenv,
+  riscv-tests,
+}:
 rv32-stdenv.mkDerivation {
   name = "pokedex-tests";
 
-  src = ./.;
+  src =
+    with lib.fileset;
+    toSource {
+      root = ./.;
+      fileset = unions [
+        ./makefile
+        ./riscv-tests-env
+        ./smoke
+        ./smoke_v
+        ./utils
+      ];
+    };
 
   makeFlags = [
     "RISCV_PREFIX=${rv32-stdenv.targetPlatform.config}"
     "PREFIX=${placeholder "out"}"
+    "RISCV_TESTS_SRC=${riscv-tests}"
   ];
 
   dontFixup = true;
