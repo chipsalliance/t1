a336233202e82fa80b08510a4a781a71f4f40ca9
Avimitin <dev@avimit.in>
2025-08-05 16:27:58 +0800

    [pokedex] unify command-line to config.kdl

--- pokedex/simulator/pokedex/src/lib.rs
+++ pokedex/simulator/pokedex/src/lib.rs
@@ -4,4 +4,6 @@ mod simulator;
 
 // We need to have granular control over what the emulator can use to prevent application
 // developers from messing around with ASL model states.
-pub use simulator::{SimulationException, Simulator, SimulatorParams};
+pub use simulator::{
+    AddressSpaceDescNode, BusInfo, SimulationException, Simulator, SimulatorParams,
+};
