aa808c342945816d455538abae76564c301ebb9a
Avimitin <dev@avimit.in>
2025-10-12 17:12:08 +0800

    [pokedex-tests] add riscv-vector-tests

--- pokedex/tests/package.nix
+++ pokedex/tests/package.nix
@@ -2,6 +2,7 @@
   lib,
   rv32-stdenv,
   riscv-tests,
+  riscv-vector-tests,
 }:
 rv32-stdenv.mkDerivation {
   name = "pokedex-tests";
@@ -13,6 +14,7 @@ rv32-stdenv.mkDerivation {
       fileset = unions [
         ./makefile
         ./riscv-tests-env
+        ./riscv-vector-tests
         ./smoke
         ./smoke_v
         ./utils
@@ -20,9 +22,12 @@ rv32-stdenv.mkDerivation {
     };
 
   makeFlags = [
+    # TODO: configurable
+    "VLEN=256"
     "RISCV_PREFIX=${rv32-stdenv.targetPlatform.config}"
     "PREFIX=${placeholder "out"}"
     "RISCV_TESTS_SRC=${riscv-tests}"
+    "CODEGEN_INSTALL_DIR=${riscv-vector-tests}"
   ];
 
   dontFixup = true;
