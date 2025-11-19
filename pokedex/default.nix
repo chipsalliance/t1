9bd6d0c476d3169a236788a643d0a71b4bda10b1
Avimitin <dev@avimit.in>
2025-11-19 16:42:59 +0800

    [pokedex] drop rvopcode-cli

--- pokedex/default.nix
+++ pokedex/default.nix
@@ -2,14 +2,9 @@
   lib,
   newScope,
   riscv-opcodes-src,
-  ivy-rvdecoderdb,
 }:
 lib.makeScope newScope (scope: {
   inherit riscv-opcodes-src;
-
-  rvopcode-cli = scope.callPackage ./rvopcode/package.nix {
-    inherit ivy-rvdecoderdb;
-  };
 
   model = scope.callPackage ./model/package.nix { };
 
