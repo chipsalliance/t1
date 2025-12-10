18ffc9922bac138d1d4a99488ca4f57b000e1325
Avimitin <dev@avimit.in>
2025-12-10 15:00:24 +0800

    [pokedex] use latest nixpkgs for minijinja and Typst

--- pokedex/default.nix
+++ pokedex/default.nix
@@ -2,6 +2,7 @@
   lib,
   newScope,
   riscv-opcodes-src,
+  fetchFromGitHub,
 }:
 let
   mkScope =
@@ -22,12 +23,23 @@ let
         )
       )
     );
+
+  pkgSrc = fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "677fbe97984e7af3175b6c121f3c39ee5c8d62c9"; # nixpkgs-unstable-small 2025-12-09
+    hash = "sha256-g2a4MhRKu4ymR4xwo+I+auTknXt/+j37Lnf0Mvfl1rE=";
+  };
+  lockedNixpkgs = import pkgSrc { system = "x86_64-linux"; };
 in
 lib.makeScope newScope (
   scope:
   {
     # Simulator needs no comptime ISA
     simulator = scope.callPackage ./simulator/package.nix { };
+
+    # I need latest minijinja-cli and typst but doesn't want to bump main Nixpkgs.
+    inherit (lockedNixpkgs) minijinja typst;
   }
   // mkScope scope.newScope (
     {
