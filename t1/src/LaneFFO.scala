d29c818d5cfd2d84af78c81df39dbc0304a6910a
root <root@xinpian.com>
2026-04-24 04:03:26 +0000

    [t1zaozi] migrate LaneFFO to zaozi

--- t1/src/LaneFFO.scala
+++ t1/src/LaneFFO.scala
@@ -4,66 +4,34 @@
 package org.chipsalliance.t1.rtl
 
 import chisel3._
-import chisel3.experimental.hierarchy.{instantiable, public}
-import chisel3.util._
+import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
+import chisel3.experimental.hierarchy.instantiable
+import chisel3.util.{Valid, ValidIO}
+
+object LaneFFOParam {
+  implicit def rw: upickle.default.ReadWriter[LaneFFOParam] = upickle.default.macroRW
+}
+
+case class LaneFFOParam(datapathWidth: Int) extends SerializableModuleParameter
+
+class LaneFFOInterface(parameter: LaneFFOParam) extends Bundle {
+  val clock:        Clock         = Input(Clock())
+  val reset:        Bool          = Input(Bool())
+  val src:          Vec[UInt]     = Input(Vec(4, UInt(parameter.datapathWidth.W)))
+  val resultSelect: UInt          = Input(UInt(2.W))
+  val complete:     Bool          = Input(Bool())
+  val maskType:     Bool          = Input(Bool())
+  val resp:         ValidIO[UInt] = Output(Valid(UInt(parameter.datapathWidth.W)))
+}
 
 @instantiable
-class LaneFFO(datapathWidth: Int) extends Module {
-  // Seq(mask, data, destination)
-  @public
-  val src:          Vec[UInt]     = IO(Input(Vec(4, UInt(datapathWidth.W))))
-  @public
-  val resultSelect: UInt          = IO(Input(UInt(2.W)))
-  @public
-  val resp:         ValidIO[UInt] = IO(Output(Valid(UInt(datapathWidth.W))))
-  @public
-  val complete:     Bool          = IO(Input(Bool()))
-  @public
-  val maskType:     Bool          = IO(Input(Bool()))
-
-  val truthMask: UInt = Mux(maskType, src.head, -1.S(datapathWidth.W).asUInt) & src(3)
-  val srcData:   UInt = truthMask & src(1)
-  val notZero:   Bool = srcData.orR
-  val lo:        UInt = scanLeftOr(srcData)
-  // set before(right or)
-  val ro:        UInt = (~lo).asUInt
-  // set including
-  val inc:       UInt = ro ## notZero
-  // 1H
-  val OH:        UInt = lo & inc
-  val index:     UInt = OHToUInt(OH)
-
-  // copy&paste from rocket-chip: src/main/scala/util/package.scala
-  // todo: upstream this to chisel3
-  private def OH1ToOH(x:   UInt): UInt = (((x << 1): UInt) | 1.U) & ~Cat(0.U(1.W), x)
-  private def OH1ToUInt(x: UInt): UInt = OHToUInt(OH1ToOH(x))
-  resp.valid := notZero
-  //
-  val selectOH = UIntToOH(resultSelect)
-  // find-first-set
-  val first:                      Bool = selectOH(0)
-  // set-before-first
-  val sbf:                        Bool = selectOH(1)
-  // set-only-first
-  val sof:                        Bool = selectOH(2)
-  // set-including-first
-  val sif:                        Bool = selectOH(3)
-
-  /** first: complete ? 0 : index sbf: complete ? 0 : notZero ? ro : -1 sof: complete ? 0 : OH sif: complete ? 0 :
-    * notZero ? inc : -1
-    */
-  val ffoResult:  UInt = Mux1H(
-    Seq(
-      complete,
-      !complete && first,
-      !complete && notZero && sbf,
-      !complete && sof,
-      !complete && notZero && sif,
-      !complete && !notZero && resultSelect(0)
-    ),
-    Seq(0.U, index, ro, OH, inc, -1.S.asUInt)
+class LaneFFO(val parameter: LaneFFOParam)
+    extends FixedIOExtModule(new LaneFFOInterface(parameter))
+    with SerializableModule[LaneFFOParam] {
+  val paramDir = java.nio.file.Paths.get("zaozi-params")
+  java.nio.file.Files.createDirectories(paramDir)
+  java.nio.file.Files.write(
+    paramDir.resolve("LaneFFO.json"),
+    upickle.default.write(parameter).getBytes(java.nio.charset.StandardCharsets.UTF_8)
   )
-  val resultMask: UInt = Mux(maskType && !first, src.head, -1.S(datapathWidth.W).asUInt) &
-    Mux(first, -1.S(datapathWidth.W).asUInt, src.last)
-  resp.bits := (ffoResult & resultMask) | (src(2) & (~resultMask).asUInt)
 }
