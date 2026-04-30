77f79aef4fe74bacea2e620ba3bdb7b65c534d0f
unlsycn <unlsycn@unlsycn.com>
2026-04-30 17:39:39 +0800

    [t1zaozi] migrate LanePopCount to zaozi
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- t1/src/LanePopCount.scala
+++ t1/src/LanePopCount.scala
@@ -4,14 +4,30 @@
 package org.chipsalliance.t1.rtl
 
 import chisel3._
-import chisel3.experimental.hierarchy.{instantiable, public}
-import chisel3.util.PopCount
+import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
+import chisel3.experimental.hierarchy.instantiable
+
+object LanePopCountParam {
+  implicit def rw: upickle.default.ReadWriter[LanePopCountParam] = upickle.default.macroRW
+}
+
+case class LanePopCountParam(datapathWidth: Int) extends SerializableModuleParameter
+
+class LanePopCountInterface(parameter: LanePopCountParam) extends Bundle {
+  val clock: Clock = Input(Clock())
+  val reset: Bool  = Input(Bool())
+  val src:   UInt  = Input(UInt(parameter.datapathWidth.W))
+  val resp:  UInt  = Output(UInt(parameter.datapathWidth.W))
+}
 
 @instantiable
-class LanePopCount(datapathWidth: Int) extends Module {
-  @public
-  val src:  UInt = IO(Input(UInt(datapathWidth.W)))
-  @public
-  val resp: UInt = IO(Output(UInt(datapathWidth.W)))
-  resp := PopCount(src)
+class LanePopCount(val parameter: LanePopCountParam)
+    extends FixedIOExtModule(new LanePopCountInterface(parameter))
+    with SerializableModule[LanePopCountParam] {
+  val paramDir = java.nio.file.Paths.get("zaozi-params")
+  java.nio.file.Files.createDirectories(paramDir)
+  java.nio.file.Files.write(
+    paramDir.resolve("LanePopCount.json"),
+    upickle.default.write(parameter).getBytes(java.nio.charset.StandardCharsets.UTF_8)
+  )
 }
---
+++ t1zaozi/src/LanePopCount.scala
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2026 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.rtl.zvma
+
+import me.jiuyang.zaozi.*
+import me.jiuyang.zaozi.default.{*, given}
+import me.jiuyang.zaozi.reftpe.*
+import me.jiuyang.zaozi.valuetpe.*
+import me.jiuyang.stdlib.*
+import me.jiuyang.stdlib.default.{*, given}
+import org.llvm.mlir.scalalib.capi.ir.{*, given}
+
+import java.lang.foreign.Arena
+
+case class LanePopCountParam(datapathWidth: Int) extends Parameter
+
+given upickle.default.ReadWriter[LanePopCountParam] = upickle.default.macroRW
+
+class LanePopCountInterface(parameter: LanePopCountParam) extends HWBundle(parameter):
+  val clock: BundleField[Clock] = Flipped(Clock())
+  val reset: BundleField[Reset] = Flipped(Reset())
+  val src:   BundleField[UInt]  = Flipped(UInt(parameter.datapathWidth))
+  val resp:  BundleField[UInt]  = Aligned(UInt(parameter.datapathWidth))
+
+class LanePopCountLayers(parameter: LanePopCountParam) extends LayerInterface(parameter):
+  def layers = Seq.empty
+
+class LanePopCountProbe(parameter: LanePopCountParam) extends DVBundle[LanePopCountParam, LanePopCountLayers](parameter)
+
+@generator
+object LanePopCount extends Generator[LanePopCountParam, LanePopCountLayers, LanePopCountInterface, LanePopCountProbe]:
+  override def moduleName(parameter: LanePopCountParam): String = "LanePopCount"
+
+  def architecture(parameter: LanePopCountParam) =
+    val io = summon[Interface[LanePopCountInterface]]
+
+    given Ref[Clock] = io.clock
+    given Ref[Reset] = io.reset
+
+    io.resp := popCount(io.src)
